9c18d754ec0622ff9a486c0dfc1e25d2e1f37e35
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-20 19:46:58 +0300

    Initial commit

---
+++ btrfs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# (╯°□°)╯︵ ┻━┻
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Color Templates       || #Patience                                                                 Mozart - Moonlight Sonata  #
+#                       ||                                                                             0:35 ━❍──────── -5:32    #
+#                       ||                                                                              ↻     ⊲  Ⅱ  ⊳     ↺     #
+#                       ||                                                                             VOLUME: ▁▂▃▄▅▆▇ 100%     #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+lsblk -o name,size ; echo -n "Device Name: " ; read sda ; sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$sda ; BT="/dev/${sda}"
+PB="$(ls /dev/* | grep -E "^${BT}p?1$")" ; PR="$(ls /dev/* | grep -E "^${BT}p?2$")" ; mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}"
+mount "${PR}" /mnt ; cd /mnt ; btrfs subvolume create @ ; btrfs subvolume create @home ; cd ; umount /mnt 
+mount -o noatime,compress=zstd,discard=async,subvol=@ "${PR}" /mnt ; mkdir /mnt/{boot,home}
+mount -o noatime,compress=zstd,discard=async,subvol=@home "${PR}" /mnt/home ; mount "${PB}" /mnt/boot
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
