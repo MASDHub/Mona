1d3a1295efb84015ff05bed6fc72a93d240d47f9
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-23 17:33:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ genfstab -U /mnt >> /mnt/etc/fstab ; ARC="arch-chroot /mnt" ; $ARC systemctl ena
 $ARC ln -sfv /usr/share/zoneinfo/$(curl -s https://ipapi.co/timezone) /etc/localtime ; $ARC hwclock --systohc ; locale-gen --purge en_US.UTF-8
 $ARC echo -e LANG="en_US.UTF-8" >> /etc/locale.conf ; $ARC echo "KEYMAP=us" >> /etc/vconsole.conf ; read -r -p "PC Name? " PN 
 $ARC echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${PN}.localdomain ${PN} \n" >> /etc/hosts ; echo "$PN" >> /etc/hostname
-echo "Enter Root Password: " ; $ARC passwd ; read -r -p "Enter User Name: " USN ; $ARC useradd -m -G wheel $USN ; $ARC passwd $USN ls 
-echo "$USN ALL=(ALL) ALL " >> /etc/sudoers.d/${USN} ;  su $USR ; $ARC grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-$ARC grub-mkconfig -o /boot/grub/grub.cfg #; arch-chroot /home/${USR} ./installMo.sh arch-chroot -u $USR chroot-dir /home/${USR} 
+echo "Enter Root Password: " ; $ARC passwd ; read -r -p "Enter User Name: " USN ; $ARC useradd -m -G wheel $USN ; $ARC passwd $USN 
+echo "$USN ALL=(ALL) ALL " >> /etc/sudoers.d/${USN} ; $ARC grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+$ARC grub-mkconfig -o /boot/grub/grub.cfg ; arch-chroot /home/${USR} ./installMo.sh #su $USR ;  arch-chroot -u $USR chroot-dir /home/${USR} 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
