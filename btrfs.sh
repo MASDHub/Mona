d12c60a5c8ff07e959b78a3173aa027815c812da
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-23 21:31:01 +0300

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -8,7 +8,7 @@ setfont ter-122n
 #                       ||                                                                            ↻     ⊲  Ⅱ  ⊳     ↺     #
 #                       ||                                                                            VOLUME: ▁▂▃▄▅▆▇ 100%   #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-lsblk -o name,size -e 7,11 ; read -r -p "Device Name: ? " sda ; wipefs -aq /dev/$sda ; BT="/dev/$sda"
+lsblk -o name,size -e 7,11 ; read -r -p "Device Name: " sda ; wipefs -a -q /dev/$sda ; BT="/dev/$sda"
 sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$sda ; PB="$(ls /dev/* | grep -E "^${BT}p?1$")" ; PR="$(ls /dev/* | grep -E "^${BT}p?2$")"
 mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" ; mount "${PR}" /mnt ; cd /mnt ; btrfs subvolume create @ ; btrfs subvolume create @home                          
 cd  ; umount /mnt ; mount -o noatime,compress=zstd,discard=async,subvol=@ "${PR}" /mnt ; mkdir /mnt/{boot,home}          
