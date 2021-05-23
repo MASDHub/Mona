0b5909c2c14350f4ae068277cf88e446555e4042
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-23 06:59:25 +0300

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/env bash
-set -euo pipefail
-# (╯°□°)╯︵ ┻━┻
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Color Templates       || #Patience                                                                 Mozart - Moonlight Sonata  #
-#                       ||                                                                             0:35 ━❍──────── -5:32    #
-#                       ||                                                                              ↻     ⊲  Ⅱ  ⊳     ↺     #
-#                       ||                                                                             VOLUME: ▁▂▃▄▅▆▇ 100%     #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-lsblk -o name,size ; echo -n "Device Name: " ; read sda ; sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$sda ; BT="/dev/${sda}"
-PB="$(ls /dev/* | grep -E "^${BT}p?1$")" ; PR="$(ls /dev/* | grep -E "^${BT}p?2$")" ; mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}"
-mount "${PR}" /mnt ; cd /mnt ; btrfs subvolume create @ ; btrfs subvolume create @home ; cd ; umount /mnt 
-mount -o noatime,compress=zstd,discard=async,subvol=@ "${PR}" /mnt ; mkdir /mnt/{boot,home}
-mount -o noatime,compress=zstd,discard=async,subvol=@home "${PR}" /mnt/home ; mount "${PB}" /mnt/boot
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#!/bin/bash  
+set -euo pipefail 
+setfont ter-122n 
+# 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                       ||                                                                         Mozart - Moonlight Sonata   #
+#                       ||                                                                           0:35 ━❍──────── -5:32    #
+#                       ||                                                                            ↻     ⊲  Ⅱ  ⊳     ↺     #
+#                       ||                                                                            VOLUME: ▁▂▃▄▅▆▇ 100%   #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+lsblk -o name,size -e 7,11 ; echo -n "Device Name: " ; read sda ; sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$sda           
+BT="/dev/${sda}" ; PB="$(ls /dev/* | grep -E "^${BT}p?1$")" ; PR="$(ls /dev/* | grep -E "^${BT}p?2$")"                         
+mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" ; mount "${PR}" /mnt ; cd /mnt ; btrfs subvolume create @                           
+btrfs subvolume create @home ; cd  ; umount /mnt ; mount -o noatime,compress=zstd,discard=async,subvol=@ "${PR}" /mnt           
+mkdir /mnt/{boot,home} ; mount -o noatime,compress=zstd,discard=async,subvol=@home "${PR}" /mnt/home ; mount "${PB}" /mnt/boot 
+#curl -sL https://git.io/Js167 --output install.sh ; curl -sL https://git.io/JsyGF --output installMo.bash ;
+#chmod +x install.sh ; chmod +x installMo.bash ./install.sh
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
