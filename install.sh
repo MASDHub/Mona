84ecdc3521e47ee240c0999f588e91d11d42d6b1
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-23 21:59:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ set -euo pipefail
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --verbose --save /etc/pacman.d/mirrorlist 
 gpg --list-keys ; pacman-key --init ; pacman-key --populate archlinux >/dev/null
-pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub-btrfs vim git 2>/dev/null
-read -r -p "Enter User Name: " USN ; usermod -m -d /mnt/home/${USN} ${USN} ; echo "${USN} ALL=(ALL) ALL " >> /mnt/etc/sudoers.d/${USN}
-genfstab -U /mnt >> /mnt/etc/fstab ; passwd $USN -R /mnt ; echo "Enter Root Password: " ; passwd -R /mnt
-echo -e LANG="en_US.UTF-8" >> /etc/locale.conf ; echo "KEYMAP=us" >> /etc/vconsole.conf ; read -r -p "PC Name? " PN 
+pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub-btrfs vim git 2>/dev/null ; genfstab -U /mnt >> /mnt/etc/fstab
+echo "Enter Root Password: " ; passwd -R /mnt ; read -r -p "Enter User Name: " USN ; ; useradd -m -G wheel -R /mnt $USN ; passwd -R /mnt $USN
+locale-gen --purge en_US.UTF-8 ; echo "${USN} ALL=(ALL) ALL " >> /mnt/etc/sudoers.d/${USN} ; read -r -p "PC Name? " PN ; ARC="arch-chroot /mnt"
+echo -e LANG="en_US.UTF-8" >> /etc/locale.conf ; echo "KEYMAP=us" >> /etc/vconsole.conf ; 
 echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${PN}.localdomain ${PN} \n" >> /etc/hosts ; echo "$PN" >> /etc/hostname
-ln -sfv /usr/share/zoneinfo/$(curl -s https://ipapi.co/timezone) /etc/localtime ; hwclock --systohc ; locale-gen --purge en_US.UTF-8
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg
-#ARC="chroot -u $USR /mnt /bin/bash";
+ln -sfv /usr/share/zoneinfo/$(curl -s https://ipapi.co/timezone) /etc/localtime ; hwclock --systohc
+$ARC grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; $ARC grub-mkconfig -o /boot/grub/grub.cfg
+#ARC="chroot -u $USN /mnt /bin/bash";
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
