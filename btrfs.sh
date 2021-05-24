b98295042a299a36c6408c005ddaaaa0e375b585
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-25 01:30:28 +0300

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -1,17 +1,16 @@
 #!/bin/bash  
 set -euo pipefail 
-setfont ter-122n 
-# 
+setfont ter-120b 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #                       ||                                                                         Mozart - Moonlight Sonata   #
 #                       ||                                                                           0:35 ━❍──────── -5:32    #
 #                       ||                                                                            ↻     ⊲  Ⅱ  ⊳     ↺     #
 #                       ||                                                                            VOLUME: ▁▂▃▄▅▆▇ 100%   #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-lsblk -o name,size -e 7,11 ; read -r -p "Device Name: " sda ; wipefs -a -q /dev/$sda ; BT="/dev/$sda"
-sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$sda ; PB="$(ls /dev/* | grep -E "^${BT}p?1$")" ; PR="$(ls /dev/* | grep -E "^${BT}p?2$")"
-mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" ; mount "${PR}" /mnt ; cd /mnt ; btrfs subvolume create @ ; btrfs subvolume create @home                          
-cd  ; umount /mnt ; mount -o noatime,compress=zstd,discard=async,subvol=@ "${PR}" /mnt ; mkdir /mnt/{boot,home}          
-mount -o noatime,compress=zstd,discard=async,subvol=@home "${PR}" /mnt/home ; mount "${PB}" /mnt/boot 
-curl -sL https://git.io/Jsde3 -o install.sh ; chmod +x install.sh ; ./install.sh
+lsblk -do name,size -e 7,11 ; read -r -p "$(tput setaf 6)$(tput bold)Device name: $(tput sgr0)" sda ; BT="/dev/${sda}"           
+sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$sda ; PB="$(ls /dev/* | grep -E "^${BT}p?1$")" ; PR="$(ls /dev/* | grep -E "^${BT}p?2$")"                         
+mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" ; mount "${PR}" /mnt ; cd /mnt ; btrfs subvolume create @                           
+btrfs subvolume create @home ; cd  ; umount /mnt ; mount -o noatime,compress=zstd,discard=async,subvol=@ "${PR}" /mnt           
+mkdir /mnt/{boot,home} ; mount -o noatime,compress=zstd,discard=async,subvol=@home "${PR}" /mnt/home ; mount "${PB}" /mnt/boot 
+curl -sL https://git.io/Jsde3 --output install.sh ; chmod +x install.sh ; lsblk -o name,size,type,mountpoint -e 7,11 ; ./install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
