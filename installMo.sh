3b7e5b487cf3a20913c20b8d482f975a3bd1825d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-25 16:23:51 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -3,19 +3,18 @@ set -euo pipefail
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || #B4AFB6 #716966 #677254 #170D1D #75422C #643D33 #4C312D #DCBC62 #7D5016 #5B3424                      ʕ•̼͛͡•ʕ-̺͛͡•ʔ•̮͛͡•ʔ #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-git clone https://aur.archlinux.org/pikaur.git ; cd /pikaur ; makepkg -fsri ;
+git clone https://aur.archlinux.org/pikaur.git ; cd /pikaur ; makepkg -fsri ;PKR="pikaur -S --noconfirm"  
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔʕ•̫͡•ʔ❣ʕ-̼͡-ʔ◟ʕ´∀`ʔ◞ʕ*̫͡*ʕ•͡•ʔ-̫͡-ʕ•̼͡•٩(̾●̮̮̃̾•̃̾)۶ʕ-̺͡•ʔ•̮͡•ʔʕ•͡ω•ʔʕ•͡-•ʔ=͟͟͞͞ʕ•̫͡•ʔ✧ƪ(•̃͡•̃͡ ƪʕ•͕͡•ʔʕ•̫͡•ʔ♡*       (҂⌣̀_⌣́)(❛ัॢᵕ❛ั ॢ)✩*  #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
 # xorg openbox xterm >/dev/null
 #pacman -S alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc htop gufw
-#${PCM} xdg-user-dirs xdg-utils   || ${PCM} xf86-video-${PCC} || ${PCM} xf86-video-${PCC}gpu 2>&1>/dev/null
-#cd /pikaur ; makepkg --fsri ; PKR="pikaur -S --noconfirm"                                                                             #
-#${PKR} nm-tray-git all-repository-fonts volumeicon-git tint2-git picom-git wpgtk-git profanity-python2-git obkey-git                 #
-#${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components nemo-seahorse nemo-share          #
-#${PKR} firefox-developer-edition obmenu-generator  alacrity-git vim-youcompleteme-git sddm-stellar-theme                              #
-#exit ; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg               #
-#systemctl enable NetworkManager ; systemctl enable avahi-daemon ; systemctl enable libvirtd ; systemctl enable sshd                  #
-#systemctl enable sddm ; wpga-install.sh ; echo "openbox-session" >> $HOME/.xinitrc ; printf "\e[1;31mDone!\e[0m"                     #
-#echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi                            #
+#${PCM} xdg-user-dirs xdg-utils   || ${PCM} xf86-video-${PCC} || ${PCM} xf86-video-${PCC}gpu 2>&1>/dev/null                                                                      
+#${PKR} nm-applet nm-connection-editor all-repository-fonts volumeicon tint2 picom wpgtk profanity-python2 obkey                 
+#${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components nemo-seahorse nemo-share          
+#${PKR} firefox-developer-edition obmenu-generator  alacrity-git vim-youcompleteme-git sddm-stellar-theme                              
+#exit ; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg               
+#systemctl enable NetworkManager ; systemctl enable avahi-daemon ; systemctl enable libvirtd ; systemctl enable sshd                  
+#systemctl enable sddm ; wpga-install.sh ; echo "openbox-session" >> $HOME/.xinitrc ; printf "\e[1;31mDone!\e[0m"                     
+#echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi                           
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
