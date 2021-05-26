e77e3d708cfc212a844a97ece36ea8fb50c0834a
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-26 06:13:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ PMC='sed -i 's/#Color/Color/'' ; PML='echo -e  \n[multilib]\nInclude = /etc/pacm
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub-btrfs vim xorg sddm git openbox  2>/dev/null
 genfstab -U /mnt >> /mnt/etc/fstab ; $PML >> /mnt/etc/pacman.conf $PMC /mnt/etc/pacman.conf ; printf "${LRD}Enter Root Password: ${NC}\n" ; $ARC passwd ; printf "${LRD}Enter User Name: ${NC}"
 read -r USN ; $ARC useradd -m -g users -G wheel -s /bin/bash $(USN) ; $ARC passwd ${USN} ; echo "${USN} ALL=(ALL) ALL " >> /mnt/etc/sudoers.d/${USN}
-$ARC ln -sf /usr/share/zoneinfo/$(curl -s https://ipapi.co/timezone) /etc/localtime ; $ARC hwclock --systohc 
-$ARC locale-gen --purge en_US.UTF-8 ; $ARC echo -e LANG="en_US.UTF-8" >> /etc/locale.conf ; $ARC echo "KEYMAP=us" >> /etc/vconsole.conf
-$ARC echo "${USN}pc" >> /etc/hostname ; $ARC echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${USN}pc.localdomain ${USN}pc \n" >> /etc/hosts 
+$ARC ln -sf /mnt/usr/share/zoneinfo/$(curl -s https://ipapi.co/timezone) /mnt/etc/localtime ; $ARC hwclock --systohc 
+$ARC locale-gen --purge en_US.UTF-8 ; $ARC echo -e LANG="en_US.UTF-8" >> /mnt/etc/locale.conf ; $ARC echo "KEYMAP=us" >> /mnt/etc/vconsole.conf
+$ARC echo "${USN}pc" >> /mnt/etc/hostname ; $ARC echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${USN}pc.localdomain ${USN}pc \n" >> /mnt/etc/hosts 
 printf "${LRD}NETWORK ENABLED${NC}\n" ; $ARC systemctl enable NetworkManager ; printf "${LRD}DISPLAY MANAGER ENABLED${NC}\n" ; $ARC systemctl enable sddm 
 printf "${LRD}GRUB INSTALL ${NC}\n" ; $ARC grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; $ARC grub-mkconfig -o /boot/grub/grub.cfg
-su -l $USN
+$ARC su -l $USN
 #arch-chroot -u $USN /mnt/home /usr/bin/bash ; curl -sL https://git.io/JsyGF -o installMO.sh && chmod +x installMO.sh && ./installMO.sh
 #while [[ $? -ne 0 ]]; do ;passwd $USN ;
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
