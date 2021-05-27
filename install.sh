b904caa6893643b581f0cdec21d70bc3a8139c44
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-27 20:33:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 # Color Templates || #6F674B #837B5E #998F71 #A89E81 #B5AB8A #C8BE9A #e7e3d5 #9a978a #6F674B #837B5E #998F71 #A89E81 #B5AB8A #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 LBL='\033[1;31m' ; NC='\033[0m' ; printf "${LBL}Enter Root Password: ${NC}\n" ; passwd ; printf "${LBL}Enter User Name: ${NC}" 
-read -r USN ; useradd -m -G wheel ${USN} ; passwd ${USN} ; sed -i 's/# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers #SU="sudo -u ${USN}"
+read -r USN ; useradd -m -G wheel ${USN} ; passwd ${USN} ; sed -i 's/# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers 
 ln -sf /share/zoneinfo/$(curl -s https://ipapi.co/timezone) /etc/localtime ; hwclock --systohc ; locale-gen --purge en_US.UTF-8 
 echo -e LANG="en_US.UTF-8" >> /etc/locale.conf ; echo "KEYMAP=us" >> /etc/vconsole.conf ; echo "${USN}pc" >> /etc/hostname  
 echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${USN}pc.localdomain ${USN}pc \n" >> /etc/hosts 
-printf "${LBL}NETWORK ENABLED${NC}\n" ; systemctl enable NetworkManager ; printf "${LBL}GRUB INSTALL ${NC}\n" #printf "${RD}DISPLAY MANAGER ENABLED${NC}\n" ; systemctl enable sddm
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg #${SU} -H sh -c "cd ~${USN}; svn update" ; ${SU} curl -sL https://git.io/Jspfl -o installMO.sh ; ${SU} chmod +x installMO.sh ; ${SU} ./installMO
+printf "${LBL}NETWORK ENABLED${NC}\n" ; systemctl enable NetworkManager ; printf "${LBL}GRUB INSTALL ${NC}\n" 
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
