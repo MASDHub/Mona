6f9715a78ffc54bccde9ace4420cfc5287805b7c
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-28 09:09:05 +0300

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -9,7 +9,7 @@ setfont ter-120b
 lsblk -do name,size -e 7,11 ; LBL='\033[1;31m' ; NC='\033[0m'; printf "${LBL}Device name: ${NC}" ; read -r SDA
 BT="/dev/$SDA" ; sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$SDA ; PB="$(ls /dev/* | grep -E "^${BT}p?1$")"
 PR="$(ls /dev/* | grep -E "^${BT}p?2$")" ; mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" ; mount "${PR}" /mnt ; cd /mnt 
-BC="btrfs subvolume create @" ; ${BC} ; ${BC}home ; cd ; curl -sL https://git.io/Jsde3 -o install.sh
+BC="btrfs su cr @" ; ${BC} ; ${BC}home ; cd ; curl -sL https://git.io/Jsde3 -o install.sh
 umount /mnt ; mount -o noatime,compress=zstd,discard=async,subvol=@ "${PR}" /mnt ; mkdir /mnt/{boot,home}
 mount -o noatime,compress=zstd,discard=async,subvol=@home "${PR}" /mnt/home ; mount "${PB}" /mnt/boot
 lsblk -o name,size,type,mountpoint -e 7,11 ; cp install.sh /mnt/install.sh ; chmod +x /mnt/install.sh
