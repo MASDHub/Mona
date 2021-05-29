eb46523cea1f3eedad35414323ee4bb350057efd
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-29 23:43:28 +0300

    Update btrfs.sh

--- btrfs.sh
+++ btrfs.sh
@@ -1,20 +1,22 @@
 #!/bin/bash  
 set -euo pipefail 
 setfont ter-120b 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Mozart - Moonlight Sonata  ||                                                                                           #
-#  0:35 ━❍──────── -5:32                                                                                                 #
-#   VOLUME: ▁▂▃▄▅▆▇ 100%     ||                                                                                           #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-lsblk -do name,size -e 7,11 ; LBL='\033[1;31m' ; NC='\033[0m'; printf "${LBL}Device name: ${NC}" ; read -r SDA
-BT="/dev/$SDA" ; sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$SDA ; PB="$(ls /dev/* | grep -E "^${BT}p?1$")"
-PR="$(ls /dev/* | grep -E "^${BT}p?2$")" ; BC="btrfs su cr @" ; MU="mount -o noatime,compress=zstd,discard=async,subvol=@"  
-mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" ; mount "${PR}" /mnt ; cd /mnt ; ${BC} ; ${BC}home ; cd 
-umount /mnt ; ${MU} "${PR}" /mnt ; mkdir /mnt/{boot,home} ; ${MU}home "${PR}" /mnt/home ; mount "${PB}" /mnt/boot
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Mozart - Moonlight Sonata  ||                                                                                                                                   #
+#  0:35 ━❍──────── -5:32                                                                                                                                           #
+#   VOLUME: ▁▂▃▄▅▆▇ 100%    ||                                                                                                                                 #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+lsblk -do name,size -e 7,11 ; BLU='\033[1;31m' ; NOC='\033[0m'; printf "${BLU}Device name: ${NOC}" ; read SDA 
+sgdisk -o -n 1::+512M -t 1:EF00 -n -i -p /dev/$SDA ; BT="/dev/$SDA" ; BC="btrfs su cr @" 
+PB="$(ls /dev/* | grep -E "^${BT}p?1$")" ; PR="$(ls /dev/* | grep -E "^${BT}p?2$")" 
+MU="mount -o noatime,compress=zstd,discard=async,subvol=@" ; mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" 
+mount "${PR}" /mnt ; cd /mnt ; ${BC} ; ${BC}home ; ${BC}snapshots ; cd ; umount /mnt ; ${MU} "${PR}" /mnt  
+mkdir /mnt/{boot,home,.snapshots} ; ${MU}home "${PR}" /mnt/home ; ${MU}home "${PR}" /mnt/.snapshots 
+mount "${PB}" /mnt/boot ; lsblk -o name,size,type,mountpoint -e 7,11 ; sleep 5
 curl -sL https://git.io/Jsde3 > install.sh ; cp install.sh /mnt/install.sh ; chmod +x /mnt/install.sh 
-sed -i 's/#Color/Color/' /etc/pacman.conf  ; sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
 reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/pacman.d/mirrorlist
-gpg --list-keys ; pacman-key --init ; pacman-key --populate archlinux >/dev/null ; printf "${LBL}Enter to Accpet${NC}\n"
-pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub-btrfs vim git 2>/dev/null 
-genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt ./install.sh
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+sed -i 's/#Color/Color/' /etc/pacman.conf ; sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
+gpg -k ; pacman-key --init ; pacman-key --populate archlinux >/dev/null
+pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub-btrfs vim snap-pac
+genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt sh ./install.sh
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
