099fb20e4f486a24477974dc9c0687913114c703
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-30 18:24:32 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -4,12 +4,11 @@ set -euo pipefail
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || #B4AFB6 #716966 #677254 #170D1D #75422C #643D33 #4C312D #DCBC62 #7D5016 #5B3424                      ʕ•̼͛͡•ʕ-̺͛͡•ʔ•̮͛͡•ʔ #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-cd ; git clone https://aur.archlinux.org/pikaur.git ; cd /pikaur ; makepkg -fsri ; PKR="pikaur -S "             #(҂⌣̀_⌣́)(❛ัॢᵕ❛ั ॢ)✩*                           #
+cd ; git clone https://aur.archlinux.org/pikaur.git ; cd ~/pikaur/ ; sudo -S makepkg -fsri ; PKR="pikaur -S"             #(҂⌣̀_⌣́)(❛ัॢᵕ❛ั ॢ)✩*   # 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔʕ•̫͡•ʔ❣ʕ-̼͡-ʔ◟ʕ´∀`ʔ◞ʕ*̫͡*ʕ•͡•ʔ-̫͡-ʕ•̼͡•٩(̾●̮̮̃̾•̃̾)۶ʕ-̺͡•ʔ•̮͡•ʔʕ•͡ω•ʔʕ•͡-•ʔ=͟͟͞͞ʕ•̫͡•ʔ✧ƪ(•̃͡•̃͡ ƪʕ•͕͡•ʔʕ•̫͡•ʔ♡*       
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
-#pacman -S rsync snap-pac-grub sddm snapper-gui alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc htop gufw openbox xorg sddm
-#${PCM} xdg-user-dirs xdg-utils  || ${PCM} xf86-video-${PCC} || ${PCM} xf86-video-${PCC}gpu 2>&1>/dev/null                                                                      
+pacman -S rsync snap-pac-grub sddm snapper-gui alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc htop gufw openbox xorg sddm
 #${PKR} nm-applet nm-connection-editor all-repository-fonts volumeicon tint2 picom wpgtk profanity-python2 obkey obmenu-generator                
 #${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components nemo-seahorse nemo-share          
 #${PKR} firefox-developer-edition alacrity-git vim-youcompleteme-git sddm-stellar-theme sddm-config-editor-git 
