61c5a3502d8c3323c5df04d586d4d94b4e495f97
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-30 23:04:52 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -8,7 +8,7 @@ cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; pushd pikaur ; makepkg -
 # Color Templates || ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔʕ•̫͡•ʔ❣ʕ-̼͡-ʔ◟ʕ´∀`ʔ◞ʕ*̫͡*ʕ•͡•ʔ-̫͡-ʕ•̼͡•٩(̾●̮̮̃̾•̃̾)۶ʕ-̺͡•ʔ•̮͡•ʔʕ•͡ω•ʔʕ•͡-•ʔ=͟͟͞͞ʕ•̫͡•ʔ✧ƪ(•̃͡•̃͡ ƪʕ•͕͡•ʔʕ•̫͡•ʔ♡*    (҂⌣̀_⌣́)(❛ัॢᵕ❛ั ॢ)✩  
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
 ${PKR} rsync alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc 
-${PKR} nm-applet nm-connection-editor volumeicon xorg sddm-stellar-theme htop gufw obkey obmenu-generator openbox
+${PKR} network-manager-applet nm-connection-editor volumeicon xorg sddm-stellar-theme htop gufw obkey obmenu-generator openbox
 ${PKR} firefox-developer-edition otf-fira-sans tint2 picom profanity-python2 wpgtk alacrity vim-youcompleteme           
 ${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components timeshift
 wpga-install.sh ; echo "openbox-session" >> ~/.xinitrc ; printf "\e[1;31mDone!\e[0m"                     
