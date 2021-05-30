78aac979ad9e23717d823ad1a5781d38cb9bc3a5
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-30 22:48:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,15 +25,9 @@ ln -sf /share/zoneinfo/$(curl -s https://ipapi.co/timezone) /etc/localtime ; hwc
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment ; echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf ; locale-gen en_US.UTF-8 ; echo "${USN}pc" >> /etc/hostname
 echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${USN}pc.localdomain ${USN}pc \n" >> /etc/hosts 
-printf "${RED}NETWORK ENABLED${NOC}\n" ; systemctl enable NetworkManager
-umount /.snapshots/ ; rm -rf /.snapshots/ ; snapper --no-dbus -v list-configs ; snapper --no-dbus -v create-config / 
-btrfs subvolume delete /.snapshots ; mkdir /.snapshots ; mount -a ; chmod 750 /.snapshots 
-printf "${RED}SNAPSHOT ENABLED ${NOC}\n" ; systemctl enable snapper-timeline.timer ; systemctl enable snapper-cleanup.timer
-printf "${RED}GRUB INSTALL ${NOC}\n" ; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 
-systemctl enable grub-btrfs.path ; curl -sL https://git.io/Jspfl -o /home/${USN}/installMO.sh
+printf "${RED}NETWORK ENABLED${NOC}\n" ; systemctl enable NetworkManager ; printf "${RED}GRUB INSTALL ${NOC}\n"
 GFX='GRUB_GFXMODE=' ; sed -i 's/'${GFX}'auto/'${GFX}'1920x1080,1024x768x32,auto/' /etc/default/grub
-SED='ALLOW_USERS=' ; sed -i 's/'${SED}'""/'${SED}'"'${USN}'"/' /etc/snapper/configs/root 
-snapper --no-dbus create ; grub-mkconfig -o /boot/grub/grub.cfg 
-printf "${RED}SYSTEM CLEANUP ${NOC}\n" ; pacman -Sc ; cd / ; chown root:root /home ; chmod 755 /home 
-runuser --login ${USN} --session-command 'sh ~/installMO.sh' 
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg 
+printf "${RED}SYSTEM CLEANUP ${NOC}\n" ; pacman -Sc ; curl -sL https://git.io/Jspfl -o /home/${USN}/installMO.sh
+cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${USN} --session-command 'sh ~/installMO.sh' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
