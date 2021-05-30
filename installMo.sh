5e8063c8d2d79b4587be9978158e3c5352b04225
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-30 18:41:08 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -8,10 +8,10 @@ cd ; git clone https://aur.archlinux.org/pikaur.git ; cd ~/pikaur/ ; sudo -S mak
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔʕ•̫͡•ʔ❣ʕ-̼͡-ʔ◟ʕ´∀`ʔ◞ʕ*̫͡*ʕ•͡•ʔ-̫͡-ʕ•̼͡•٩(̾●̮̮̃̾•̃̾)۶ʕ-̺͡•ʔ•̮͡•ʔʕ•͡ω•ʔʕ•͡-•ʔ=͟͟͞͞ʕ•̫͡•ʔ✧ƪ(•̃͡•̃͡ ƪʕ•͕͡•ʔʕ•̫͡•ʔ♡*       
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
-pacman -S rsync snap-pac-grub sddm snapper-gui alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc htop gufw openbox xorg sddm
-#${PKR} nm-applet nm-connection-editor all-repository-fonts volumeicon tint2 picom wpgtk profanity-python2 obkey obmenu-generator                
-#${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components nemo-seahorse nemo-share          
-#${PKR} firefox-developer-edition alacrity-git vim-youcompleteme-git sddm-stellar-theme sddm-config-editor-git 
-#exit ;wpga-install.sh ; echo "openbox-session" >> $HOME/.xinitrc ; printf "\e[1;31mDone!\e[0m"                     
+${PKR} rsync snap-pac-grub snapper-gui alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc 
+${PKR} nm-applet nm-connection-editor volumeicon xorg sddm-stellar-theme sddm-config-editor-git  htop gufw obkey obmenu-generator openbox
+${PKR} firefox-developer-edition otf-fira-sans tint2 picom profanity-python2 wpgtk alacrity vim-youcompleteme           
+${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components
+${PKR} openbox ; wpga-install.sh ; echo "openbox-session" >> ~/.xinitrc ; printf "\e[1;31mDone!\e[0m"                     
 #echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
