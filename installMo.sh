45029bb2e84eb5a0f657f39c745b359d480d1169
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-31 00:17:45 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -7,10 +7,12 @@ cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; pushd pikaur ; makepkg -
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔʕ•̫͡•ʔ❣ʕ-̼͡-ʔ◟ʕ´∀`ʔ◞ʕ*̫͡*ʕ•͡•ʔ-̫͡-ʕ•̼͡•٩(̾●̮̮̃̾•̃̾)۶ʕ-̺͡•ʔ•̮͡•ʔʕ•͡ω•ʔʕ•͡-•ʔ=͟͟͞͞ʕ•̫͡•ʔ✧ƪ(•̃͡•̃͡ ƪʕ•͕͡•ʔʕ•̫͡•ʔ♡*    (҂⌣̀_⌣́)(❛ัॢᵕ❛ั ॢ)✩  
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
-${PKR} rsync alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc 
-${PKR} network-manager-applet nm-connection-editor volumeicon xorg sddm-stellar-theme htop gufw obkey obmenu-generator openbox
-${PKR} firefox-developer-edition otf-fira-sans tint2 picom profanity-python2 wpgtk alacrity vim-youcompleteme           
+${PKR} rsync alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc xfce4-mpc-plugin
+${PKR} network-manager-applet nm-connection-editor xfce4-datetime-plugin xorg sddm-stellar-theme gufw obkey obmenu-generator openbox
+${PKR} firefox-developer-edition otf-fira-sans picom ristretto alacritty bash-completion 
+${PKR} xfconf xfce4-whiskermenu-plugin xfce4-taskmanager xfce4-settings xfce4-screenshooter xfce4-notes-plugin xfce4-appfinder
 ${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components timeshift
+if [[ "$(cat /sys/class/dmi/id/chassis_type)" == '8' '9' '10' '11' '12' '13' '14'  ]] ; then ${PKR} xfce4-power-manager xfce4-battery-plugin ; else ; VGA="amd-ucode xf86-video-amdgpu" ; fi  
 wpga-install.sh ; echo "openbox-session" >> ~/.xinitrc ; printf "\e[1;31mDone!\e[0m"                     
 #echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    #set -m
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
