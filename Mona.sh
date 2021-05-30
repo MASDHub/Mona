589ab61887d7f10a2110c9f55f481b5d35da7568
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-30 22:07:54 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -14,9 +14,12 @@ mount "${PR}" /mnt ; cd /mnt ; ${BC} ; ${BC}home ; ${BC}snapshots ; cd ; umount 
 mkdir /mnt/{boot,home,.snapshots} ; ${MU}home "${PR}" /mnt/home ; ${MU}snapshots "${PR}" /mnt/.snapshots 
 mount "${PB}" /mnt/boot ; lsblk -o name,size,type,mountpoint -e 7,11 ; sleep 5
 curl -sL https://git.io/Jsde3 > install.sh ; cp install.sh /mnt/install.sh ; chmod +x /mnt/install.sh 
+MOD='MODULES=' ; MKC='/etc/mkinitcpio.conf' ; if [[ "$(lscpu | grep -Eo "Intel" | sort -u)" == Intel ]]
+then VGA="intel-ucode xf86-video-intel" && sed -i 's/'${MOD}'()/'${MOD}'(i915 btrfs)/' ${MKC} ; else
+VGA="amd-ucode xf86-video-amdgpu" && sed -i 's/'${MOD}'()/'${MOD}'(amdgpu btrfs)/' ${MKC} ; fi 
 reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/pacman.d/mirrorlist
 sed -i 's/#Color/Color/' /etc/pacman.conf ; sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
 gpg -k ; pacman-key --init ; pacman-key --populate archlinux >/dev/null
-pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub-btrfs vim snap-pac git
+pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub-btrfs vim snap-pac git ${VGA}
 genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt sh ./install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
