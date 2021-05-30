2fb57b88b71e292e1303edc19e8733f99c1e9455
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-30 22:59:51 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -10,7 +10,7 @@ cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; pushd pikaur ; makepkg -
 ${PKR} rsync alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc 
 ${PKR} nm-applet nm-connection-editor volumeicon xorg sddm-stellar-theme htop gufw obkey obmenu-generator openbox
 ${PKR} firefox-developer-edition otf-fira-sans tint2 picom profanity-python2 wpgtk alacrity vim-youcompleteme           
-${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components
-${PKR} openbox ; wpga-install.sh ; echo "openbox-session" >> ~/.xinitrc ; printf "\e[1;31mDone!\e[0m"                     
+${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components timeshift
+wpga-install.sh ; echo "openbox-session" >> ~/.xinitrc ; printf "\e[1;31mDone!\e[0m"                     
 #echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    #set -m
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
