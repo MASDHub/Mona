151a1c4fedec4906c190f333643ac4cbac5c4af5
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-30 19:56:26 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-#set -m
-set -euo pipefail
+set -euo pipefail 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || #B4AFB6 #716966 #677254 #170D1D #75422C #643D33 #4C312D #DCBC62 #7D5016 #5B3424                      ʕ•̼͛͡•ʕ-̺͛͡•ʔ•̮͛͡•ʔ #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -13,5 +12,5 @@ ${PKR} nm-applet nm-connection-editor volumeicon xorg sddm-stellar-theme sddm-co
 ${PKR} firefox-developer-edition otf-fira-sans tint2 picom profanity-python2 wpgtk alacrity vim-youcompleteme           
 ${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components
 ${PKR} openbox ; wpga-install.sh ; echo "openbox-session" >> ~/.xinitrc ; printf "\e[1;31mDone!\e[0m"                     
-#echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    
+#echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    #set -m
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
