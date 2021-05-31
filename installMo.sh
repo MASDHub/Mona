2f85201567a03dcf19d8bf29611f855b39e37a92
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-31 21:15:00 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -12,10 +12,10 @@ echo -e "XDG_${DS}${DIR}${Dk}\nXDG_${DW}${DIR}${Dn}\nXDG_${MU}${DIR}${Me}" > /ho
 # Color Templates || ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔʕ•̫͡•ʔ❣ʕ-̼͡-ʔ◟ʕ´∀`ʔ◞ʕ*̫͡*ʕ•͡•ʔ-̫͡-ʕ•̼͡•٩(̾●̮̮̃̾•̃̾)۶ʕ-̺͡•ʔ•̮͡•ʔʕ•͡ω•ʔʕ•͡-•ʔ=͟͟͞͞ʕ•̫͡•ʔ✧ƪ(•̃͡•̃͡ ƪʕ•͕͡•ʔʕ•̫͡•ʔ♡*    (҂⌣̀_⌣́)(❛ัॢᵕ❛ั ॢ)✩  
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
 X='xfce4' ; P="pikaur -S" ; F='firefox-developer-edition-i18n-' ; O='otf-fira-'
-${P} xorg rsync openbox ${X}-settings archlinux-xdg-menu xfconf network-manager-applet nm-connection-editor gufw xterm 
-&& alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc picom  
-&& nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components obkey ristretto timeshift 
-&& ${F}en-us ${F}en-gb ${F}en-ca  ${F}fr ${F}de ${F}it ${F}ja ${F}zh-cn ${F}zh-tw ${O}sans ${O}mono
+${P} xorg rsync xfce-polkit openbox ${X}-settings archlinux-xdg-menu xfconf network-manager-applet nm-connection-editor gufw xterm
+&& alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse vlc picom xorg-xprop gvfs
+&& nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components obkey ristretto menulibre timeshift 
+&& ${F}en-us ${F}en-gb ${F}en-ca  ${F}fr ${F}de ${F}it ${F}ja ${F}zh-cn ${F}zh-tw ${O}sans ${O}mono xcursor-breeze
 && ${X}-whiskermenu-plugin ${X}-taskmanager ${X}-screenshooter ${X}-notes-plugin ${X}-appfinder ${X}-datetime-plugin ${X}-mpc-plugin
 if [[ -n "$(grep -E '8|9|10|11|12|13|14' /sys/class/dmi/id/chassis_type)" ]]; then ${P} ${X}-power-manager ${X}-battery-plugin ; fi
 A='alacritty' ; U='/usr/local/' ; C='completion' ; git clone https://github.com/${A}/${A}.git ; cd ${A} 
@@ -28,6 +28,11 @@ cp extra/${C}s/${A}.bash ~/.bash_${C}/${A} ; echo "source ~/.bash_${C}/${A}" >> 
 ${P} sddm-stellar-theme ; printf "${R}DISPLAY MANAGER ENABLED${B}" ; systemctl enable sddm
 echo "openbox-session" >> /home/${USN}/.xinitrc ;  #sed -i "s/#openbox/openbox/" /etc/update-menus.conf ; update-menus ; 
 #echo "<menu id="xdg-menu" label="XDG Menu" execute="cat /var/cache/xdg-menu/openbox/menu.xml"/>" >> /home/${USN}/.config/openbox/menu.xml  
-printf "\e[1;31mDone!\e[0m" ; EXIT         
+#xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa xfsettingsd
+#xfconf-query -c xfce4-session -p /sessions/Failsafe/Client1_Command -t string -sa openbox-session
+#rm ~/.cache/sessions/* && chmod 500 ~/.cache/sessions
+#gconftool-2 --type boolean --set /desktop/gnome/interface/buttons_have_icons true
+#gconftool-2 --type boolean --set /desktop/gnome/interface/menus_have_icons true
+printf "\e[1;31mDone!\e[0m" ; #rm -rf ~/.cache/session ; reboot    
 #echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    #set -m
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
