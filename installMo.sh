52a5c552c4a67f474ca27095c0d99271c18b1d0d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-31 16:18:23 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -13,7 +13,8 @@ ${PKR} firefox-developer-edition otf-fira-sans picom ristretto alacritty bash-co
 ${PKR} xfconf xfce4-whiskermenu-plugin xfce4-taskmanager xfce4-settings xfce4-screenshooter xfce4-notes-plugin xfce4-appfinder xfce4-screensaver 
 ${PKR} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components timeshift
 if [[ -n "$(grep -E '8|9|10|11|12|13|14' /sys/class/dmi/id/chassis_type)" ]]; then ${PKR} xfce4-power-manager xfce4-battery-plugin ; fi
-sudo systemctl enable sddm ;
-wpga-install.sh ; printf "\e[1;31mDone!\e[0m"                     
+DS='DESKTOP' ; Dk='Desktop"' ; DW='DOWNLOAD' ; Dn='Downloads"' ; MU='MUSIC' ; Me='Media"' ; DIR='_DIR="$HOME/' 
+echo -e "XDG_${DS}${DIR}${Dk}\nXDG_${DW}${DIR}${Dn}\nXDG_${MU}${DIR}${Me}" > /home/${USN}/.config/user-dirs.dirs
+echo "openbox-session" >> /home/${USN}/.xinitrc ; sudo systemctl enable sddm ; printf "\e[1;31mDone!\e[0m"                     
 #echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    #set -m
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
