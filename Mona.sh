d1f631d1ab3139136d2177f68d831e374391065e
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-31 17:54:46 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -6,13 +6,13 @@ setfont ter-122b
 #  0:35 ━❍──────── -5:32                                                                                                                                           #
 #   VOLUME: ▁▂▃▄▅▆▇ 100%    ||                                                                                                                                 #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-R='\e[1;31m' ; N='\e[0m'  ; lsblk -do name,size -e 7,11 ; printf "${R}Device name: ${N}" ; read SDA 
+R='\e[1;31m' ; N='\e[0m'  ; DV='name,size -e 7,11' ; lsblk -do ${DV} ; printf "${R}Device name: ${N}" ; read SDA 
 BT="/dev/$SDA" ; PB="$(ls /dev/* | grep -E "^${BT}p?1$")" ; PR="$(ls /dev/* | grep -E "^${BT}p?2$")" 
 sgdisk -Z  -o -n 1::+512M -t 1:EF00 -n -i -v -p /dev/$SDA  ; mkfs.vfat "${PB}" ; mkfs.btrfs -fq "${PR}" 
 BC="btrfs su cr @" ; MU="mount -o noatime,compress=zstd,discard=async,subvol=@"
 mount "${PR}" /mnt ; cd /mnt ; ${BC} ; ${BC}home ; cd ; umount /mnt ; ${MU} "${PR}" /mnt  
 mkdir /mnt/{boot,home} ; ${MU}home "${PR}" /mnt/home ; mount "${PB}" /mnt/boot 
-lsblk -o name,size,type,mountpoint -e 7,11 ; sleep 5
+lsblk -o ${DV} ; sleep 5
 curl -sL https://git.io/Jsde3 > install.sh ; cp install.sh /mnt/install.sh ; chmod +x /mnt/install.sh 
 MOD='MODULES=' ; MKC='/etc/mkinitcpio.conf' ; if [[ "$(lscpu | grep -Eo "Intel" | sort -u)" == Intel ]]
 then VGA="intel-ucode xf86-video-intel" && sed -i 's/'${MOD}'()/'${MOD}'(i915 btrfs)/' ${MKC} 
