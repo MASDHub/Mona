58126917b91a2994a98aab3c8860d18420cc4184
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-05-31 16:18:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,19 +18,16 @@ head -n 17 install.sh
 # Color Templates || #6F674B #837B5E #998F71 #A89E81 #B5AB8A #C8BE9A #e7e3d5 #9a978a #6F674B #837B5E    #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 sed -i 's/#Color/Color/' /etc/pacman.conf ; sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
-RED='\033[1;31m' ; NOC='\033[0m' ; printf "${RED}Enter Root Password: ${NOC}\n" ; passwd #; if [[ $? -ne 0 ]] ; then passwd ; fi 
-printf "${RED}Enter User Name: ${NOC}" ; read -r USN ; useradd -m -G wheel ${USN} ; mkdir /home/${USN}/.config/
-sed -i 's/# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers ; passwd "${USN}" #; if [[ $? -ne 0 ]] ; then passwd ${USN} ; fi
-DS='DESKTOP' ; Dk='Desktop"' ; DW='DOWNLOAD' ; Dn='Downloads"' ; MU='MUSIC' ; Me='Media"' ; DIR='_DIR="$HOME/' 
-echo -e "XDG_${DS}${DIR}${Dk}\nXDG_${DW}${DIR}${Dn}\nXDG_${MU}${DIR}${Me}" > /home/${USN}/.config/user-dirs.dirs
-echo "openbox-session" >> /home/${USN}/.xinitrc 
+R='\e[1;31m' ; NC='\e[0m' ; printf "${R}Enter Root Password: ${NC}\n" ; passwd #; if [[ $? -ne 0 ]] ; then passwd ; fi 
+printf "${R}Enter User Name: ${NC}" ; read -r USN ; useradd -m -G wheel ${USN}
+sed -i 's/# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers ; passwd "${USN}" #; if [[ $? -ne 0 ]] ; then passwd ${USN} ; 
 ln -sf /share/zoneinfo/$(curl -s https://ipapi.co/timezone) /etc/localtime ; hwclock --systohc
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment ; echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf ; locale-gen en_US.UTF-8 ; echo "${USN}pc" >> /etc/hostname
 echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${USN}pc.localdomain ${USN}pc \n" >> /etc/hosts 
-printf "${RED}NETWORK ENABLED${NOC}\n" ; systemctl enable NetworkManager ; printf "${RED}GRUB INSTALL ${NOC}\n"
+printf "${R}NETWORK ENABLED${NC}\n" ; systemctl enable NetworkManager ; printf "${R}GRUB INSTALL ${NC}\n"
 GFX='GRUB_GFXMODE=' ; sed -i 's/'${GFX}'auto/'${GFX}'1920x1080,1024x768x32,auto/' /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg 
-printf "${RED}SYSTEM CLEANUP ${NOC}\n" ; pacman -Sc ; curl -sL https://git.io/Jspfl -o /home/${USN}/installMO.sh
+printf "${R}SYSTEM CLEANUP ${NC}\n" ; pacman -Sc ; curl -sL https://git.io/Jspfl -o /home/${USN}/installMO.sh
 cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${USN} --session-command 'sh ~/installMO.sh' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
