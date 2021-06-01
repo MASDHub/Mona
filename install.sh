9cfe9d84a538693ddae2acfa7bb13f4a38f31e8f
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 08:14:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,13 +21,13 @@ A='\e[1;31m' ; B='\e[0m' ; printf "${A}Enter Root Password: ${B}\n" ; passwd
 printf "${A}Enter User Name: ${B}" ; read -r U ; useradd -m -G wheel ${U} 
 sed -i 's/# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers ; passwd "${U}"
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; hwclock --systohc
-E='en_US.UTF-8' ; echo "LC_ALL=e${E}" >> /etc/environment ; echo "${E} UTF-8" >> /etc/locale.gen
+E='en_US.UTF-8' ; F='localhost' ; G='GRUB_GFXMODE=' ; H='/etc/pacman.conf' ; I='TotalDownload'
+echo "LC_ALL=e${E}" >> /etc/environment ; echo "${E} UTF-8" >> /etc/locale.gen
 echo "LANG=${E}" >> /etc/locale.conf ; locale-gen ${E} ; echo "${U}pc" >> /etc/hostname
-echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> /etc/hosts 
+echo -e "127.0.0.1 ${F} \n::1 ${F} \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> /etc/hosts 
 printf "${A}NETWORK ENABLED${B}\n" ; systemctl enable NetworkManager ; printf "${A}GRUB INSTALL ${B}\n"
-GFX='GRUB_GFXMODE=' ; sed -i 's/'${GFX}'auto/'${GFX}'1920x1080,1024x768x32,auto/' /etc/default/grub
+sed -i 's/#Color/Color/' ${H} ; sed -i "s/#${I}/${I}/" ${H} ; sed -i 's/'${G}'auto/'${G}'1920x1080,auto/' /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg 
-sed -i 's/#Color/Color/' /etc/pacman.conf ; sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
-curl -sL https://git.io/Jspfl -o /home/${U}/installMO.sh ; cd / ; chown root:root /home ; chmod 755 /home 
+curl -sL https://git.io/Jspfl > /home/${U}/installMO.sh ; cd / ; chown root:root /home ; chmod 755 /home 
 runuser --login ${U} --session-command 'sh ~/installMO.sh' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
