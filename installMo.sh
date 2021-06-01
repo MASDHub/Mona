67e5b247424d44be0ea30d259b6f617f1a7b9be0
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 11:31:00 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -10,16 +10,21 @@ cd ~ ; ${G} ${F}aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔ🤘❣ʕ-̼͡-ʔ◟ʕ´∀`ʔ◞ʕ*̫͡*ʕ•͡•ʔ-̫͡-ʕ•̼͡•٩(̾●̮̮̃̾•̃̾)۶ʕ-̺͡•ʔ•̮͡•ʔʕ•͡ω•ʔʕ•͡-•ʔ=͟͟͞͞ʕ•̫͡•ʔ✧ƪ(•̃͡•̃͡ ƪʕ•͕͡•ʔʕ•̫͡•ʔ♡*🔆🔅 (҂⌣̀_⌣́)(❛ัॢᵕ❛ั ॢ)✨  
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
-P="pikaur -S" ; X='xfce4' ; W='pipewire' ; F='firefox-developer-edition-i18n-' ; O='otf-fira-' 
+P="pikaur -S" ; X='xfce4' ; W='pipewire' ; R='firefox-developer-edition-i18n-' ; O='otf-fira-' 
 ${P} xorg rsync xfce-polkit openbox ${X}-settings archlinux-xdg-menu xfconf network-manager-applet nm-connection-editor gufw xterm \
 && alsa-utils ${W} ${W}-alsa ${W}-pulse ${W}-jack gst-plugin-${W} libpulse vlc picom xorg-xprop gvfs xcursor-breeze \
 && nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components obkey ristretto menulibre timeshift \
-&& ${F}en-us ${F}en-gb ${F}en-ca  ${F}fr ${F}de ${F}it ${F}ja ${F}zh-cn ${F}zh-tw ${F}$ru {F}he ${F}pt-br ${O}sans ${O}mono ttf-ms-fonts \
+&& ${R}en-us ${R}en-gb ${R}en-ca  ${R}fr ${R}de ${R}it ${R}ja ${R}zh-cn ${R}zh-tw ${R}$ru {R}he ${R}pt-br ${O}sans ${O}mono ttf-ms-fonts \
 && ${X}-whiskermenu-plugin ${X}-taskmanager ${X}-screenshooter ${X}-notes-plugin ${X}-appfinder ${X}-datetime-plugin ${X}-mpc-plugin 
 if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]]; then ${P} ${X}-power-manager ${X}-battery-plugin ; fi
 echo "openbox-session" >> ~/.xinitrc 
-${P} sddm-stellar-theme ; printf "${B}DISPLAY MANAGER ENABLED${C}" ; sudo systemctl enable sddm
-printf "${B}SYSTEM CLEANUP ${C}\n" ; pacman -Sc ; rm ~/.cache/sessions/* && chmod 500 ~/.cache/sessions
+sudo cp target/release/alacritty /usr/local/bin && cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop && sudo update-desktop-database
+G='xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t' ; sudo ${G}string -sa xfsettingsd && ${G}string -sa openbox-session
+sudo sed -i "s/#openbox/openbox/" /etc/update-menus.conf && update-menus ; 
+sudo echo "<menu id="xdg-menu" label="XDG Menu" execute="cat /var/cache/xdg-menu/openbox/menu.xml"/>" >> /home/${USN}/.config/openbox/menu.xml  
+printf "${B}DISPLAY MANAGER ENABLED${C}" ; ${P} sddm-stellar-theme  ; sudo systemctl enable sddm
+printf "${B}SYSTEM CLEANUP ${C}\n" ; sudo pacman -Sc && rm ~/.cache/sessions/* && chmod 500 ~/.cache/sessions
 printf "\e[1;31mDone!\e[0m" ; #rm -rf ~/.cache/session ; reboot    
 #echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    #set -m
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
