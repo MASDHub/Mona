b3d2839cad95bc4f9b40622f3b7ca2ff8b2852d0
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 11:07:45 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -14,9 +14,9 @@ E1="$(ls /dev/* | grep -E "^${E}p?1$")" ; E2="$(ls /dev/* | grep -E "^${E}p?2$")
 F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@'
 mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt ; mkdir /mnt/{boot,home}
 ${G}home ${E2} /mnt/home ; mount ${E1} /mnt/boot ; lsblk -o ${C} ; curl -sL https://git.io/Jsde3 > /mnt/install.sh
-H='pacman' ; I='TotalDownload' ; ${H}-key --list-keys ; ${H}-key --init ; ${H}-key --populate archlinux >/dev/null
+H='pacman' ; I='TotalDownload' ; sed -i 's/#Color/Color/' /etc/${H}.conf && sed -i "s/#${I}/${I}/" /etc/${H}.conf 
 reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/${H}.d/mirrorlist
-sed -i 's/#Color/Color/' /etc/${H}.conf && sed -i "s/#${I}/${I}/" /etc/${H}.conf 
+${H}-key --list-keys && ${H}-key --init && ${H}-key --populate archlinux &1>/dev/null
 J='MODULES=' ; K='/etc/mkinitcpio.conf' ; L='xf86-video-' ; M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)"
 if [[ "${M}" == "Intel" ]] ; then N="intel-ucode ${L}intel" && sed -i "s/${J}()/${J}(i915 btrfs)/" ${K} 
 elif [[ "${M}" == "AMD" ]] ; then N="amd-ucode ${L}amdgpu" && sed -i "s/${J}()/${J}(amdgpu btrfs)/" ${K} ; fi
