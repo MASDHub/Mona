9b9d79b46cb516dd0076a22926ab3dd636866c9d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 07:42:27 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/bash
 set -euo pipefail 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Color Templates || #B4AFB6 #716966 #677254 #170D1D #75422C #643D33 #4C312D #DCBC62 #7D5016 #5B3424                                                ʕ•̼͛͡•ʕ-̺͛͡•ʔ•̮͛͡•ʔ #
+# Color Templates || A='alacritty' | B='\e[1;31m' | D='/usr/local/' | E="~/.config/pikaur.conf"                                           ʕ•̼͛͡•ʕ-̺͛͡•ʔ•̮͛͡•ʔ #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-pacman -S --needed git cmake freetype2 fontconfig pkg-config make libxcb 
-A='alacritty' ; U='/usr/local/' ; C='completion' ; git clone https://github.com/${A}/${A}.git ; cd ${A} 
+#pacman -S --needed git cmake freetype2 fontconfig pkg-config make libxcb 
+A='alacritty' ; B='\e[1;31m' ;  C='\e[0m' ; D='/usr/local/' ; C='completion' 
+cd ~ ;  git clone https://github.com/${A}/${A}.git ; cd ${A} 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh ; rustup override set stable ; rustup update stable
-cargo build --release ; cp target/release/${A} ${U}bin ; cp extra/logo/${A}-term.svg /usr/share/pixmaps/${A}.svg
-desktop-file-install extra/linux/Alacritty.desktop ; update-desktop-database ; mkdir -p ${U}share/man/man1
-gzip -c extra/${A}.man | tee ${U}share/man/man1/${A}.1.gz ; mkdir -p ~/.bash_${C}
+cargo build --release ; cp target/release/${A} ${D}bin ; cp extra/logo/${A}-term.svg /usr/share/pixmaps/${A}.svg
+desktop-file-install extra/linux/Alacritty.desktop ; update-desktop-database ; mkdir -p ${D}share/man/man1
+gzip -c extra/${A}.man | tee ${D}share/man/man1/${A}.1.gz ; mkdir -p ~/.bash_${C}
 cp extra/${C}s/${A}.bash ~/.bash_${C}/${A} ; echo "source ~/.bash_${C}/${A}" >> ~/.bashrc
-cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; pushd pikaur ; makepkg -fsri ; R='\e[1;31m' ; B='\e[0m'
-D="~/.config/pikaur.conf" K1='keepbuilddir =' ; K2='keepbuilddeps =' ; N1='noedit =' ; N2='nodiff ='
-sed -i "s/${K1} no/${K1} yes/" ${D} ;  sed -i 's/${K2} no/${K2} yes/' ${D} ; sed -i 's/${N1} no/${N1} yes/' ${D} ;  sed -i 's/${N2} no/${N2} yes/' ${D}
-DS='DESKTOP' ; Dk='Desktop"' ; DW='DOWNLOAD' ; Dn='Downloads"' ; MU='MUSIC' ; Me='Media"' ; DIR='_DIR="$HOME/' 
+cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; pushd pikaur ; makepkg -fsri 
+E="$HOME/.config/pikaur.conf" ; E1='keepbuilddir =' ; E2='keepbuilddeps =' ; E3='noedit =' ; E4='nodiff ='
+sed -i "s/${E1} no/${E1} yes/" ${E} ;  sed -i "s/${E2} no/${E2} yes/" ${E} ; sed -i "s/${E3} no/${E3} yes/" ${E} ;  sed -i "s/${E4} no/${E4} yes/" ${E}
+DS='DESKTOP' ; Dk='Desktop' ; DW='DOWNLOAD' ; Dn='Downloads"' ; MU='MUSIC' ; Me='Media"' ; DIR='_DIR="$HOME/' 
 echo -e "XDG_${DS}${DIR}${Dk}\nXDG_${DW}${DIR}${Dn}\nXDG_${MU}${DIR}${Me}" > /home/${USN}/.config/user-dirs.dirs
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔ🤘❣ʕ-̼͡-ʔ◟ʕ´∀`ʔ◞ʕ*̫͡*ʕ•͡•ʔ-̫͡-ʕ•̼͡•٩(̾●̮̮̃̾•̃̾)۶ʕ-̺͡•ʔ•̮͡•ʔʕ•͡ω•ʔʕ•͡-•ʔ=͟͟͞͞ʕ•̫͡•ʔ✧ƪ(•̃͡•̃͡ ƪʕ•͕͡•ʔʕ•̫͡•ʔ♡*🔆🔅 (҂⌣̀_⌣́)(❛ัॢᵕ❛ั ॢ)✨  
@@ -25,7 +26,7 @@ ${P} xorg rsync xfce-polkit openbox ${X}-settings archlinux-xdg-menu xfconf netw
 && ${F}en-us ${F}en-gb ${F}en-ca  ${F}fr ${F}de ${F}it ${F}ja ${F}zh-cn ${F}zh-tw ${F}$ru {F}he ${F}pt-br ${O}sans ${O}mono ttf-ms-fonts \
 && ${X}-whiskermenu-plugin ${X}-taskmanager ${X}-screenshooter ${X}-notes-plugin ${X}-appfinder ${X}-datetime-plugin ${X}-mpc-plugin 
 if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]]; then ${P} ${X}-power-manager ${X}-battery-plugin ; fi
-${P} sddm-stellar-theme ; printf "${R}DISPLAY MANAGER ENABLED${B}" ; systemctl enable sddm
+${P} sddm-stellar-theme ; printf "${B}DISPLAY MANAGER ENABLED${C}" ; systemctl enable sddm
 echo "openbox-session" >> /home/${USN}/.xinitrc 
 #sed -i "s/#openbox/openbox/" /etc/update-menus.conf ; update-menus ; 
 #echo "<menu id="xdg-menu" label="XDG Menu" execute="cat /var/cache/xdg-menu/openbox/menu.xml"/>" >> /home/${USN}/.config/openbox/menu.xml  
@@ -34,6 +35,6 @@ echo "openbox-session" >> /home/${USN}/.xinitrc
 #rm ~/.cache/sessions/* && chmod 500 ~/.cache/sessions
 #gconftool-2 --type boolean --set /desktop/gnome/interface/buttons_have_icons true
 #gconftool-2 --type boolean --set /desktop/gnome/interface/menus_have_icons true
-printf "${R}SYSTEM CLEANUP ${NC}\n" ; pacman -Sc ; printf "\e[1;31mDone!\e[0m" ; #rm -rf ~/.cache/session ; reboot    
+printf "${B}SYSTEM CLEANUP ${C}\n" ; pacman -Sc ; printf "\e[1;31mDone!\e[0m" ; #rm -rf ~/.cache/session ; reboot    
 #echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    #set -m
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
