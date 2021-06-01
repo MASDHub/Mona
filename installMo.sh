3a90540c2b41da29c51f4a6714f000130f4d4339
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 09:56:41 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/bash
 set -euo pipefail 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='alacritty' ; B='\e[1;31m' ;  C='\e[0m' ; D='.bash' ; E='completion' 
-F="$HOME/.config/pikaur.conf" ; F1='keepbuilddir =' ; F2='keepbuilddeps =' ; F3='noedit =' ; F4='nodiff ='                                                         # ʕ•̼͛͡•ʕ-̺͛͡•ʔ•̮͛͡•ʔ 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-cd ~ ;  git clone https://github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
-source $HOME/.cargo/env ; rustup override set stable ; cargo build --release 
-mkdir -p ~/${D}_${E} ; cp extra/${E}s/${A}${D} ~/${D}_${E}/${A} ; echo "source ~/${D}_${E}/${A}" >> ~/${D}rc
-cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri 
-sed -i "s/${F1} no/${F2} yes/" ${F} ;  sed -i "s/${F2} no/${F2} yes/" ${F} ; sed -i "s/${F3} no/${F3} yes/" ${F} ;  sed -i "s/${F4} no/${F4} yes/" ${F}
-echo -e "XDG_DESKTOP_DIR="$HOME/Desktop"\nXDG_DOWNLOAD_DIR="$HOME/Downloads"\n" >> /home/${USN}/.config/user-dirs.dirs
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+A='alacritty' ; B='\e[1;31m' ;  C='\e[0m' ; D='.bash' ; E='completion' F='https://' G='git clone' # ʕ•̼͛͡•ʕ-̺͛͡•ʔ•̮͛͡•ʔ 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+cd ~ ;  ${G} ${F}github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tlsv1.2 -sSf ${F}sh.rustup.rs | sh 
+source $HOME/.cargo/env ; rustup override set stable ; cargo build --release ; mkdir -p ~/${D}_${E} 
+cp extra/${E}s/${A}${D} ~/${D}_${E}/${A} ; echo "source ~/${D}_${E}/${A}" >> ~/${D}rc
+cd ~ ; ${G} ${F}aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔ🤘❣ʕ-̼͡-ʔ◟ʕ´∀`ʔ◞ʕ*̫͡*ʕ•͡•ʔ-̫͡-ʕ•̼͡•٩(̾●̮̮̃̾•̃̾)۶ʕ-̺͡•ʔ•̮͡•ʔʕ•͡ω•ʔʕ•͡-•ʔ=͟͟͞͞ʕ•̫͡•ʔ✧ƪ(•̃͡•̃͡ ƪʕ•͕͡•ʔʕ•̫͡•ʔ♡*🔆🔅 (҂⌣̀_⌣́)(❛ัॢᵕ❛ั ॢ)✨  
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
@@ -22,6 +19,8 @@ ${P} xorg rsync xfce-polkit openbox ${X}-settings archlinux-xdg-menu xfconf netw
 if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]]; then ${P} ${X}-power-manager ${X}-battery-plugin ; fi
 ${P} sddm-stellar-theme ; printf "${B}DISPLAY MANAGER ENABLED${C}" ; systemctl enable sddm
 echo "openbox-session" >> /home/${USN}/.xinitrc 
+
+#sed -i "s/${F1}dir no/${F1}dir yes/" ${F} ;  sed -i "s/${F1}deps no/${F1}deps yes/" ${F} ; sed -i "s/${F3} no/${F3} yes/" ${F} ;  sed -i "s/${F4} no/${F4} yes/" ${F}
 #sed -i "s/#openbox/openbox/" /etc/update-menus.conf ; update-menus ; 
 #echo "<menu id="xdg-menu" label="XDG Menu" execute="cat /var/cache/xdg-menu/openbox/menu.xml"/>" >> /home/${USN}/.config/openbox/menu.xml  
 #xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa xfsettingsd
