79347aa32628a35f42174d3b70082a0b603b012c
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 06:40:30 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -3,6 +3,13 @@ set -euo pipefail
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Color Templates || #B4AFB6 #716966 #677254 #170D1D #75422C #643D33 #4C312D #DCBC62 #7D5016 #5B3424                                                ʕ•̼͛͡•ʕ-̺͛͡•ʔ•̮͛͡•ʔ #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+pacman -S --needed git cmake freetype2 fontconfig pkg-config make libxcb 
+A='alacritty' ; U='/usr/local/' ; C='completion' ; git clone https://github.com/${A}/${A}.git ; cd ${A} 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh ; rustup override set stable ; rustup update stable
+cargo build --release ; cp target/release/${A} ${U}bin ; cp extra/logo/${A}-term.svg /usr/share/pixmaps/${A}.svg
+desktop-file-install extra/linux/Alacritty.desktop ; update-desktop-database ; mkdir -p ${U}share/man/man1
+gzip -c extra/${A}.man | tee ${U}share/man/man1/${A}.1.gz ; mkdir -p ~/.bash_${C}
+cp extra/${C}s/${A}.bash ~/.bash_${C}/${A} ; echo "source ~/.bash_${C}/${A}" >> ~/.bashrc
 cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; pushd pikaur ; makepkg -fsri ; R='\e[1;31m' ; B='\e[0m'
 D="~/.config/pikaur.conf" K1='keepbuilddir =' ; K2='keepbuilddeps =' ; N1='noedit =' ; N2='nodiff ='
 sed -i "s/${K1} no/${K1} yes/" ${D} ;  sed -i 's/${K2} no/${K2} yes/' ${D} ; sed -i 's/${N1} no/${N1} yes/' ${D} ;  sed -i 's/${N2} no/${N2} yes/' ${D}
@@ -18,13 +25,6 @@ ${P} xorg rsync xfce-polkit openbox ${X}-settings archlinux-xdg-menu xfconf netw
 && ${F}en-us ${F}en-gb ${F}en-ca  ${F}fr ${F}de ${F}it ${F}ja ${F}zh-cn ${F}zh-tw ${F}$ru {F}he ${F}pt-br ${O}sans ${O}mono ttf-ms-fonts \
 && ${X}-whiskermenu-plugin ${X}-taskmanager ${X}-screenshooter ${X}-notes-plugin ${X}-appfinder ${X}-datetime-plugin ${X}-mpc-plugin 
 if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]]; then ${P} ${X}-power-manager ${X}-battery-plugin ; fi
-A='alacritty' ; U='/usr/local/' ; C='completion' ; git clone https://github.com/${A}/${A}.git ; cd ${A} 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh ; rustup override set stable ; rustup update stable
-pacman -S --needed cmake freetype2 fontconfig pkg-config make libxcb ; cargo build --release
-cp target/release/${A} ${U}bin ; cp extra/logo/${A}-term.svg /usr/share/pixmaps/${A}.svg
-desktop-file-install extra/linux/Alacritty.desktop ; update-desktop-database ; mkdir -p ${U}share/man/man1
-gzip -c extra/${A}.man | tee ${U}share/man/man1/${A}.1.gz ; mkdir -p ~/.bash_${C}
-cp extra/${C}s/${A}.bash ~/.bash_${C}/${A} ; echo "source ~/.bash_${C}/${A}" >> ~/.bashrc
 ${P} sddm-stellar-theme ; printf "${R}DISPLAY MANAGER ENABLED${B}" ; systemctl enable sddm
 echo "openbox-session" >> /home/${USN}/.xinitrc 
 #sed -i "s/#openbox/openbox/" /etc/update-menus.conf ; update-menus ; 
