e66804f6c5908790ede60aeb00cc6a6f38608c57
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 09:30:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/bin/bash                                                                                                                
-set -euo pipefail            
-head -n 17 install.sh
-#8'         8888
+#!/bin/bash   
+set -euo pipefail 
+head 15 file.txt | tail -14
+ #8'         8888
 #d8.-=. ,==-.:888b 
 #>8 `~` :`~' d8888                 
 #88         ,88888                 
@@ -15,19 +15,15 @@ head -n 17 install.sh
 #/88:.__ ,       _%-' ---  -       
  #  '''::===..-'   =  --.  `                 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Color Templates || #6F674B #837B5E #998F71 #A89E81 #B5AB8A #C8BE9A #e7e3d5 #9a978a #6F674B #837B5E    #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='\e[1;31m' ; B='\e[0m' ; C='printf' ; D='en_US.UTF-8'  ; E='GRUB_GFXMODE=' ; F='/etc/pacman.conf' ; G='TotalDownload'
-${C} "${A}Enter Root Password: ${B}\n" ; passwd ; ${C} "${A}Enter User Name: ${B}" ; read -r U 
-useradd -m -G wheel ${U} ; sed -i 's/# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers ; passwd "${U}"
+A='\e[1;31m' ; B='\e[0m' ; C='printf' ; D='en_US.UTF-8'  ; E='GRUB_GFXMODE=' ; F='/etc/pacman.conf' G='TotalDownload' 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
+sed -i 's/#Color/Color/' ${F} ; sed -i "s/#${G}/${G}/" ${F} ; pacman -S --needed git cmake freetype2 fontconfig pkg-config  
+${C} "${A}Enter Root Password: ${B}\n" ; passwd ; ${C} "${A}Enter User Name: ${B}" ; read -r U ; useradd -m -G wheel ${U} ; passwd "${U}"
+sed -i 's/# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers ; curl -sL https://git.io/Jspfl > /home/${U}/installMO.sh 
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; hwclock --systohc
-echo "LC_ALL=e${D}" >> /etc/environment ; echo "${D} UTF-8" >> /etc/locale.gen
-echo "LANG=${D}" >> /etc/locale.conf ; locale-gen ${D} ; echo "${U}pc" >> /etc/hostname
-echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> /etc/hosts 
-${C} "${A}NETWORK ENABLED${B}\n" ; systemctl enable NetworkManager ; ${C} "${A}GRUB INSTALL ${B}\n"
-sed -i 's/#Color/Color/' ${F} ; sed -i "s/#${G}/${G}/" ${F} ; sed -i 's/'${E}'auto/'${E}'1920x1080,auto/' /etc/default/grub
-pacman -S --needed git cmake freetype2 fontconfig pkg-config make libxcb
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg 
-curl -sL https://git.io/Jspfl > /home/${U}/installMO.sh ; cd / ; chown root:root /home ; chmod 755 /home 
-runuser --login ${U} --session-command 'sh ~/installMO.sh' 
+echo "LC_ALL=e${D}" >> /etc/environment ; echo "${D} UTF-8" >> /etc/locale.gen ; echo "LANG=${D}" >> /etc/locale.conf ; locale-gen ${D} 
+echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> /etc/hosts ; echo "${U}pc" >> /etc/hostname
+${C} "${A}NETWORK ENABLED${B}\n" ; systemctl enable NetworkManager ${C} "${A}GRUB INSTALL ${B}\n" 
+sed -i 's/'${E}'auto/'${E}'1920x1080,auto/' /etc/default/grub ; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 
+grub-mkconfig -o /boot/grub/grub.cfg ; cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${U} --session-command 'sh ~/installMO.sh' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
