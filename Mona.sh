517d3e75eeca70e0216e6a7dba81f18ce60470ba
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 03:18:36 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -6,20 +6,20 @@ setfont ter-124b
 #  0:35 ━❍──────── -5:32                                                                                                                                           #
 #   VOLUME: ▁▂▃▄▅▆▇ 100%    ||                                                                                                                                   #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-R='\e[1;31m' ; N='\e[0m' ; DV="name,size -e 7,11" ; lsblk -do ${DV} ; printf "${R}Device name: ${N}"
-read -e D ; B="/dev/${D}" ; D1="$(ls /dev/* | grep -E "^${B}p?1$")" ; D2="$(ls /dev/* | grep -E "^${B}p?2$")"
-sgdisk /dev/${D} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p ; mkfs.vfat ${D1} ; mkfs.btrfs -fq ${D2}
-MU='mount -o noatime,compress=zstd,discard=async,subvol=@' ; BC="btrfs su cr @"
-mount ${D2} /mnt ; cd /mnt ; ${BC} ; ${BC}home ; cd ; umount /mnt ; ${MU} ${D2} /mnt
-mkdir /mnt/{boot,home} ; ${MU}home ${D2} /mnt/home ; mount ${D1} /mnt/boot ; lsblk -o ${DV}
-P='pacman' ; T='TotalDownload' ; gpg -k ; ${P}-key --init ; ${P}-key --populate archlinux >/dev/null
-sed -i 's/#Color/Color/' /etc/${P}.conf ; sed -i 's/#{T}/{T}/' /etc/${P}.conf
-MO='MODULES=' ; MK='/etc/mkinitcpio.conf' ; XV='xf86-video-' ; L="lscpu | grep -Eo" ; S="| sort -u"
-if [[ "$(${L} "Intel" ${S})" == "Intel" ]] ; then V="intel-ucode ${XV}intel" ; elif [[ "$(${L} "AMD" ${S})" == "AMD" ]] 
-then V="amd-ucode ${XV}amdgpu" ; elif [[ "${V}" == "intel-ucode ${XV}intel" ]] ; then sed -i "s/${MO}()/${MO}(i915 btrfs)/" ${MK}
-elif [[ "${V}" == "amd-ucode ${XV}amdgpu" ]] ; then sed -i "s/${MO}()/${MO}(amdgpu btrfs)/" ${MK} ; fi
-reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/${P}.d/mirrorlist
+A='\e[1;31m' ; B='\e[0m' ; C="name,size -e 7,11" ; lsblk -do ${C} ; printf "${A}Device name: ${B}"
+read C && D="/dev/${C}" && D1="$(ls /dev/* | grep -E "^${D}p?1$")" && D2="$(ls /dev/* | grep -E "^${D}p?2$")"
+sgdisk "${D}" -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p ; mkfs.vfat ${D1} ; mkfs.btrfs -fq ${D2}
+E='mount -o noatime,compress=zstd,discard=async,subvol=@' ; F="btrfs su cr @"
+mount ${D2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${E} ${D2} /mnt
+mkdir /mnt/{boot,home} ; ${E}home ${D2} /mnt/home ; mount ${D1} /mnt/boot ; lsblk -o ${C}
+G='pacman' ; H='TotalDownload' ; gpg -k ; ${G}-key --init ; ${G}-key --populate archlinux >/dev/null
+sed -i 's/#Color/Color/' /etc/${G}.conf && sed -i 's/#{H}/{H}/' /etc/${G}.conf
+I='MODULES=' ; J='/etc/mkinitcpio.conf' ; K='xf86-video-' 
+if [[ "$(lspu | grep 'Intel' | sort -u)" == "Intel" ]] ; then N="intel-ucode ${K}intel" \
+&& sed -i "s/${I}()/${I}(i915 btrfs)/" ${J} ; elif [[ "$(lspu | grep 'AMD' | sort -u)" == "AMD" ]] 
+then N="amd-ucode ${K}amdgpu" && sed -i "s/${I}()/${I}(amdgpu btrfs)/" ${J} ; fi
+reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/${G}.d/mirrorlist
 curl -sL https://git.io/Jsde3 > install.sh ; cp install.sh /mnt/install.sh ; chmod +x /mnt/install.sh
-pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub-btrfs vim git ${V}
+pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub-btrfs vim git ${N}
 genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt sh ./install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
