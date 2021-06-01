2afa46d7e1babc39e39fdbd8dd2e0bb4e9ec1d64
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 12:58:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,11 +15,11 @@ head -n 15 install.sh | tail -n 13
  #  '''::===..-'   =  --.  `                 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 A='\e[1;31m' ; B='\e[0m' ; C='printf' ; D='en_US.UTF-8' ; E='GRUB_GFXMODE=' ; F='/etc/pacman.conf' 
-G='TotalDownload' ; H='/etc/locale.' ; I='/etc/host' ; J='do echo "Try again"' ; W='%wheel ALL=(ALL) ALL' ; P='passwd' 
+G='TotalDownload' ; H='/etc/locale.' ; I='/etc/host' ; J="echo "Try again"" ; W='%wheel ALL=(ALL) ALL' ; P='passwd' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
 sed -i 's/#Color/Color/' ${F} ; sed -i "s/#${G}/${G}/" ${F} ; pacman -S --needed git cmake freetype2 fontconfig pkg-config  
-${C} "${A}Enter Root Password: ${B}\n" ; ${P} ; until ${P}  ; ${J} ; done ; ${C} "${A}Enter User Name: ${B}" ;  read -r U 
-useradd -m -G wheel ${U} ; ${P}  "${U}" ; until ${P} "${U}" ; ${J} ; done
+${C} "${A}Enter Root Password: ${B}\n" ; ${P} ; until ${P}  ; do ${J} ; done ; ${C} "${A}Enter User Name: ${B}" ;  read -r U 
+useradd -m -G wheel ${U} ; ${P}  "${U}" ; until ${P} "${U}" ; do ${J} ; done
 sed -i "s/# ${W}/ ${W}/" /etc/sudoers ; curl -sL https://git.io/Jspfl > /home/${U}/installMO.sh 
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; hwclock --systohc
 echo "LC_ALL=${D}" >> /etc/environment ; echo "${D} UTF-8" >> ${H}gen ; echo "LANG=${D}" >> ${H}conf ; locale-gen ${D} 
