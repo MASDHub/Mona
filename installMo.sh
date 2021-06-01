6c205064d0cc16ae780fc89c91bf9d8a57fd05a8
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 10:58:41 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -17,21 +17,9 @@ ${P} xorg rsync xfce-polkit openbox ${X}-settings archlinux-xdg-menu xfconf netw
 && ${F}en-us ${F}en-gb ${F}en-ca  ${F}fr ${F}de ${F}it ${F}ja ${F}zh-cn ${F}zh-tw ${F}$ru {F}he ${F}pt-br ${O}sans ${O}mono ttf-ms-fonts \
 && ${X}-whiskermenu-plugin ${X}-taskmanager ${X}-screenshooter ${X}-notes-plugin ${X}-appfinder ${X}-datetime-plugin ${X}-mpc-plugin 
 if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]]; then ${P} ${X}-power-manager ${X}-battery-plugin ; fi
-${P} sddm-stellar-theme ; printf "${B}DISPLAY MANAGER ENABLED${C}" ; systemctl enable sddm
-echo "openbox-session" >> /home/${USN}/.xinitrc 
-
-#sed -i "s/${F1}dir no/${F1}dir yes/" ${F} ;  sed -i "s/${F1}deps no/${F1}deps yes/" ${F} ; sed -i "s/${F3} no/${F3} yes/" ${F} ;  sed -i "s/${F4} no/${F4} yes/" ${F}
-#sed -i "s/#openbox/openbox/" /etc/update-menus.conf ; update-menus ; 
-#echo "<menu id="xdg-menu" label="XDG Menu" execute="cat /var/cache/xdg-menu/openbox/menu.xml"/>" >> /home/${USN}/.config/openbox/menu.xml  
-#xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -sa xfsettingsd
-#xfconf-query -c xfce4-session -p /sessions/Failsafe/Client1_Command -t string -sa openbox-session
-#rm ~/.cache/sessions/* && chmod 500 ~/.cache/sessions
-#gconftool-2 --type boolean --set /desktop/gnome/interface/buttons_have_icons true
-#gconftool-2 --type boolean --set /desktop/gnome/interface/menus_have_icons true
-cp target/release/${A} ${D}bin ; cp extra/logo/${A}-term.svg /usr/share/pixmaps/${A}.svg
-desktop-file-install extra/linux/Alacritty.desktop ; update-desktop-database ; mkdir -p ${D}share/man/man1
-gzip -c extra/${A}.man | tee ${D}share/man/man1/${A}.1.gz ; 
-
-printf "${B}SYSTEM CLEANUP ${C}\n" ; pacman -Sc ; printf "\e[1;31mDone!\e[0m" ; #rm -rf ~/.cache/session ; reboot    
+echo "openbox-session" >> ~/.xinitrc 
+${P} sddm-stellar-theme ; printf "${B}DISPLAY MANAGER ENABLED${C}" ; sudo systemctl enable sddm
+printf "${B}SYSTEM CLEANUP ${C}\n" ; pacman -Sc ; rm ~/.cache/sessions/* && chmod 500 ~/.cache/sessions
+printf "\e[1;31mDone!\e[0m" ; #rm -rf ~/.cache/session ; reboot    
 #echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    #set -m
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
