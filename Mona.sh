b983e396c940f78c8185ae20184acb9e182ed3a5
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 04:36:38 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -7,10 +7,10 @@ setfont ter-124b
 #   VOLUME: ▁▂▃▄▅▆▇ 100%    ||                                                                                                                                   #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 A='\e[1;31m' ; B='\e[0m' ; C="name,size -e 7,11" ; lsblk -do ${C} ; printf "${A}Device name: ${B}"
-read C && D="/dev/${C}" && D1="$(ls /dev/* | grep -E "^${D}p?1$")" && D2="$(ls /dev/* | grep -E "^${D}p?2$")"
-sgdisk "${D}" -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p ; mkfs.vfat ${D1} ; mkfs.btrfs -fq ${D2}
-E='mount -o noatime,compress=zstd,discard=async,subvol=@' ; F="btrfs su cr @"
-mount ${D2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${E} ${D2} /mnt
+read C ; D="/dev/${C}" ; sgdisk "${D}" -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p
+D1="$(ls /dev/* | grep -E "^${D}p?1$")" ; D2="$(ls /dev/* | grep -E "^${D}p?2$")"
+mkfs.vfat ${D1} ; mkfs.btrfs -fq ${D2} ; E='mount -o noatime,compress=zstd,discard=async,subvol=@' 
+F="btrfs su cr @" ; mount ${D2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${E} ${D2} /mnt
 mkdir /mnt/{boot,home} ; ${E}home ${D2} /mnt/home ; mount ${D1} /mnt/boot ; lsblk -o ${C}
 G='pacman' ; H='TotalDownload' ; gpg -k ; ${G}-key --init ; ${G}-key --populate archlinux >/dev/null
 sed -i 's/#Color/Color/' /etc/${G}.conf && sed -i 's/#{H}/{H}/' /etc/${G}.conf
