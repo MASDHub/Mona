f893dd2c0a286646eb692d71262a13d689ffbab2
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 11:39:08 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 setfont ter-124b 
 A='\e[1;31m' ; B='\e[0m' 
 C="name,size -e 7,11" ; lsblk -do ${C}
+O='2>&1>/dev/null'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Mozart - Moonlight Sonata  ||  A='\e[1;31m' | B='\e[0m' | C="name,size -e 7,11" | read -r D | E="/dev/${D}"
 #  0:35 ━❍──────── -5:32     ||  F='btrfs su cr @' | G='mount -o noatime,compress=zstd,discard=async,subvol=@'
@@ -11,12 +12,12 @@ C="name,size -e 7,11" ; lsblk -do ${C}
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 printf "${A}Device name: ${B}" ; read -r D ; E="/dev/${D}" ; sgdisk ${E} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p 
 E1="$(ls /dev/* | grep -E "^${E}p?1$")" ; E2="$(ls /dev/* | grep -E "^${E}p?2$")" ; mkfs.vfat ${E1} ; mkfs.btrfs -fq ${E2}
-F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@'
+F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@' 
 mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt ; mkdir /mnt/{boot,home}
 ${G}home ${E2} /mnt/home ; mount ${E1} /mnt/boot ; lsblk -o ${C} ; curl -sL https://git.io/Jsde3 > /mnt/install.sh
 H='pacman' ; I='TotalDownload' ; sed -i 's/#Color/Color/' /etc/${H}.conf && sed -i "s/#${I}/${I}/" /etc/${H}.conf 
 reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/${H}.d/mirrorlist
-${H}-key --list-keys && ${H}-key --init && ${H}-key --populate archlinux 2>&1>/dev/null
+${H}-key --list-keys ${O} ; ${H}-key --init ${O} ; ${H}-key --populate archlinux ${O}
 J='MODULES=' ; K='/etc/mkinitcpio.conf' ; L='xf86-video-' ; M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)"
 if [[ "${M}" == "Intel" ]] ; then N="intel-ucode ${L}intel" && sed -i "s/${J}()/${J}(i915 btrfs)/" ${K} 
 elif [[ "${M}" == "AMD" ]] ; then N="amd-ucode ${L}amdgpu" && sed -i "s/${J}()/${J}(amdgpu btrfs)/" ${K} ; fi
