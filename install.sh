479b82aab746ba6329f952fb3a117e8c0f22597d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 06:17:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,10 +18,10 @@ head -n 17 install.sh
 # Color Templates || #6F674B #837B5E #998F71 #A89E81 #B5AB8A #C8BE9A #e7e3d5 #9a978a #6F674B #837B5E    #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 sed -i 's/#Color/Color/' /etc/pacman.conf ; sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
-R='\e[1;31m' ; NC='\e[0m' ; printf "${R}Enter Root Password: ${NC}\n" ; passwd #; if [[ $? -ne 0 ]] ; then passwd ; fi 
-printf "${R}Enter User Name: ${NC}" ; read -r ${USN} ; useradd -m -G wheel ${USN}
-sed -i 's/# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers ; passwd "${USN}" #; if [[ $? -ne 0 ]] ; then passwd ${USN} ; 
-ln -sf /share/zoneinfo/$(curl -s https://ipapi.co/timezone) /etc/localtime ; hwclock --systohc
+R='\e[1;31m' ; NC='\e[0m' ; printf "${R}Enter Root Password: ${NC}\n" ; passwd 
+printf "${R}Enter User Name: ${NC}" ; read -r USN ; useradd -m -G wheel ${USN} 
+sed -i 's/# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers ; passwd "${USN}"
+ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; hwclock --systohc
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment ; echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf ; locale-gen en_US.UTF-8 ; echo "${USN}pc" >> /etc/hostname
 echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${USN}pc.localdomain ${USN}pc \n" >> /etc/hosts 
