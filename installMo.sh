ed40fd22fba9bf33ae43772bc9e8a8081fb6ec5e
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 12:03:10 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -17,13 +17,14 @@ ${P} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-
 ${P} ${R}en-us ${R}en-gb ${R}en-ca  ${R}fr ${R}de ${R}it ${R}ja ${R}zh-cn ${R}zh-tw ${R}$ru {R}he ${R}pt-br ${O}sans ${O}mono ttf-ms-fonts 
 ${P} ${X}-whiskermenu-plugin ${X}-taskmanager ${X}-screenshooter ${X}-notes-plugin ${X}-appfinder ${X}-datetime-plugin ${X}-mpc-plugin 
 if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]]; then ${P} ${X}-power-manager ${X}-battery-plugin ; fi
-echo "openbox-session" >> ~/.xinitrc 
+echo "openbox-session" >> ~/.xinitrc ; #sudo su
 #sudo cp target/release/alacritty /usr/local/bin && cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 #sudo desktop-file-install extra/linux/Alacritty.desktop && sudo update-desktop-database
 #G='xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t' ; sudo ${G}string -sa xfsettingsd && ${G}string -sa openbox-session
 #sudo sed -i "s/#openbox/openbox/" /etc/update-menus.conf && update-menus ; 
 #sudo echo "<menu id="xdg-menu" label="XDG Menu" execute="cat /var/cache/xdg-menu/openbox/menu.xml"/>" >> /home/${USN}/.config/openbox/menu.xml  
-printf "${B}DISPLAY MANAGER ENABLED${C}" ; ${P} sddm-stellar-theme  ; sudo systemctl enable sddm
+printf "${B}DISPLAY MANAGER ENABLED${C}" ; ${P} sddm-stellar-theme ; sudo systemctl enable sddm
+printf "${B}NETWORK ENABLED${C}\n" ; sudo systemctl enable NetworkManager 
 printf "${B}SYSTEM CLEANUP ${C}\n" ; sudo pacman -Sc && rm ~/.cache/sessions/* && chmod 500 ~/.cache/sessions
 printf "\e[1;31mDone!\e[0m" ; #rm -rf ~/.cache/session ; reboot    
 #echo -n "reboot? (y/n) " ; read ANS ; if [ "$ANS" != "${ANS#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    #set -m
