ce23816286cbd68bbd9e8a993124e701f4eb6c5d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-01 06:23:17 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -5,15 +5,15 @@ A='\e[1;31m' ; B='\e[0m'
 C="name,size -e 7,11" ; lsblk -do ${C}
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Mozart - Moonlight Sonata  ||  A='\e[1;31m' | B='\e[0m' | C="name,size -e 7,11" | read -r D | E="/dev/${D}"
-#  0:35 ━❍──────── -5:32     ||  F='mount -o noatime,compress=zstd,discard=async,subvol=@' | G='btrfs su cr @'
+#  0:35 ━❍──────── -5:32     ||  F='btrfs su cr @' | G='mount -o noatime,compress=zstd,discard=async,subvol=@'
 #       ↻     ⊲  Ⅱ  ⊳     ↺       ||  H='pacman' | I='TotalDownload' | J='MODULES=' | K='/etc/mkinitcpio.conf'
 #    VOLUME: ▁▂▃▄▅▆▇ 100%   ||  L='xf86-video-' | M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)" | N="intel/amd-ucode"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 printf "${A}Device name: ${B}" ; read -r D ; E="/dev/${D}" ; sgdisk ${E} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p 
 E1="$(ls /dev/* | grep -E "^${E}p?1$")" ; E2="$(ls /dev/* | grep -E "^${E}p?2$")" ; mkfs.vfat ${E1} ; mkfs.btrfs -fq ${E2} 
-F='mount -o noatime,compress=zstd,discard=async,subvol=@' ; G='btrfs su cr @' ; H='pacman' ; I='TotalDownload' 
-mount ${E2} /mnt ; cd /mnt ; ${G} ; ${G}home ; cd ; umount /mnt ; ${F} ${E2} /mnt ; mkdir /mnt/{boot,home} 
-${F}home ${E2} /mnt/home ; mount ${E1} /mnt/boot ; lsblk -o ${C}
+F='btrfs su cr @' ; mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt 
+G='mount -o noatime,compress=zstd,discard=async,subvol=@' ; H='pacman' ; I='TotalDownload' 
+${G} ${E2} /mnt ; mkdir /mnt/{boot,home} ; ${G}home ${E2} /mnt/home ; mount ${E1} /mnt/boot ; lsblk -o ${C}
 J='MODULES=' ; K='/etc/mkinitcpio.conf' ; L='xf86-video-'  ; M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)"
 if [[ "${M}" == "Intel" ]] ; then N="intel-ucode ${L}intel" && sed -i "s/${J}()/${J}(i915 btrfs)/" ${K} 
 elif [[ "${M}" == "AMD" ]] ; then N="amd-ucode ${L}amdgpu" && sed -i "s/${J}()/${J}(amdgpu btrfs)/" ${K} ; fi
