921505aca673b2e974def87dc4ee5c93df1596f7
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-02 20:50:30 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -9,7 +9,7 @@ cp extra/${E}s/${A}${D} ~/${D}_${E}/${A} ; echo "source ~/${D}_${E}/${A}" >> ~/$
 cd ~ ; ${G} ${F}aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 P="pikaur -S --needed" ; X='xfce4' ; W='pipewire' ; R='firefox-developer-edition-i18n-' ; O='otf-fira-' H='openbox' I='.config' # ʕु-̫͡-ʔु”♬ ʕ•̭͡•ʕ•̯ͦ͡•ʕ•̻̀•́ʔ❣ 
-J='<item label="' ; K='"><action name="Execute"><command>' L='</command></action></item>' M="sudo tee -a ~/${I}/${H}/menu.xml"
+J='<item label="' ; K='"><action name="Execute"><command>' L='</command></action></item>' M="~/${I}/${H}/menu.xml"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
 ${P} xorg rsync xfce-polkit openbox ${X}-settings archlinux-xdg-menu xfconf network-manager-applet nm-connection-editor gufw xterm 
 ${P} alsa-utils ${W} ${W}-alsa ${W}-pulse ${W}-jack gst-plugin-${W} libpulse vlc picom xorg-xprop gvfs xcursor-breeze geany geany-plugins
@@ -18,11 +18,12 @@ ${P} ${R}en-us ${R}en-gb ${R}en-ca ${R}fr ${R}de ${R}it ${R}ja ${R}zh-cn ${R}zh-
 ${P} ${X}-whiskermenu-plugin ${X}-taskmanager ${X}-screenshooter ${X}-notes-plugin ${X}-appfinder ${X}-datetime-plugin ${X}-mpc-plugin 
 ${P} galculator
 if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then ${P} ${X}-power-manager ${X}-battery-plugin ; fi
-P1='reversesearchsorting =' P2='noedit =' ; P3="~/.config/pikaur.conf" ; sudo -i "s/${P1} no/${P1} yes/" ${P3} ; sudo -i "s/${P2} no/${P2} yes/" ${P3}
+P1='reversesearchsorting =' P2='noedit =' ; P3="~/${I}/pikaur.conf" ; sudo -i "s/${P1} no/${P1} yes/" ${P3} ; sudo -i "s/${P2} no/${P2} yes/" ${P3}
 sudo cp ~/${A}/target/release/${A} /usr/local/bin ; sudo mkdir -p ~/${I}/${H} ; sudo cp -a /etc/xdg/${H}/ ~/${I}/
 sed -i '5,$d' ~/${I}/${H}/menu.xml ; echo "<menu id="root-menu" label="Openbox 3">" | ${M} 
-echo -e "${J}Internet${K}firefox-developer-edition${L}\n${J}File Manager${K}nemo${L}\n${J}Terminal${K}${A}${L}\n${J}Calculator${K}galculator${L}" | ${M} 
-echo -e "${J}Text Editor${K}geany${L}\n${J}Settings${K}xfce4-settings-manager${L}\n${J}Turn Off${K}sudo shutdown${L}</menu>\n</openbox_menu>" | ${M} 
+echo -e "${J}Internet${K}firefox-developer-edition${L}\n${J}File Manager${K}nemo${L}\n${J}Terminal${K}${A}${L}\n" | sudo tee -a ${M} 
+echo -e "${J}Text Editor${K}geany${L}\n${J}Settings${K}xfce4-settings-manager${L}\n${J}Turn Off${K}sudo shutdown${L}\n" | sudo tee -a ${M} 
+echo -e "${J}Calculator${K}galculator${L}\n</menu>\n</openbox_menu>\n" | sudo tee -a ${M} 
 echo -e "xfce-mcs-manager &\n\nxfce4-panel &\n" | sudo tee ~/${I}/${H}/autostart ; echo "exec ${H}-session" | sudo tee ~/.xinitrc 
 printf "${B}DISPLAY MANAGER ENABLED${C}" ; ${P} sddm-stellar-theme ; sudo systemctl enable sddm
 printf "${B}NETWORK ENABLED${C}\n" ; sudo systemctl enable NetworkManager 
