b7521cea1be60fc9c7b157e8ae0fc513e56a2dc0
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-03 00:17:09 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -16,17 +16,4 @@ ${I} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-
 ${I} ${L}en-us ${L}en-gb ${L}en-ca ${L}fr ${L}de ${L}it ${L}ja ${L}zh-cn ${L}zh-tw ${L}ru ${L}he ${L}pt-br ${M}sans ${M}mono ttf-ms-fonts 
 ${I} ${J}-whiskermenu-${O} ${J}-task${N} ${J}-screenshooter ${J}-notes-${O} ${J}-appfinder ${J}-datetime-${O} ${J}-mpc-${O} 
 ${I} galculator ; if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then ${H} ${J}-power-${N} ${J}-battery-${H} ; fi
-I2='reversesearchsorting =' I3='noedit =' ; sudo -i "s/${I2} no/${I2} yes/" ~/${I}/${H}.conf ; sudo -i "s/${I3} no/${I3} yes/" ~/${I}/${H}.conf
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
- Q='openbox' R='.config' ; S='<item label="' ; T='"><action name="Execute"><command>' U='</command></action></item>' ; V='menu.xml' 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
-sudo cp ~/${A}/target/release/${A} /usr/local/bin ; sudo mkdir -p ~/${R}/${Q} ; sudo cp -a /etc/xdg/${Q}/ ~/${R}/
-sudo sed -i '5,$d' ~/${R}/${Q}/menu.xml ; echo -e '<menu id="root-menu" label="Openbox 3">' | sudo tee -a ~/${R}/${Q}/${V}
-echo -e "${S}Internet${T}firefox-developer-edition${T}\n${S}File Manager${T}nemo${U}\n${S}Terminal${T}${A}${T}\n" | sudo tee -a ~/${R}/${Q}/${V}
-echo -e "${S}Text Editor${T}geany${T}\n${S}Settings${T}xfce4-settings-${N}${T}\n${S}Turn Off${T}sudo shutdown now${T}\n" | sudo tee -a ~/${R}/${Q}/${V}
-echo -e "${S}Calculator${T}galculator${T}\n</menu>\n</openbox_menu>\n" | sudo tee -a ~/${R}/${Q}/${V}
-echo -e "xfce-mcs-${N} &\n\nxfce4-panel &\n" | sudo tee ~/${R}/${Q}/autostart ; echo "exec ${Q}-session" | sudo tee ~/.xinitrc 
-printf "${B}DISPLAY MANAGER ENABLED${C}" ; ${P} sddm-stellar-theme ; sudo systemctl enable sddm
-printf "${B}NETWORK ENABLED${C}\n" ; sudo systemctl enable NetworkManager ; printf "${B}SYSTEM CLEANUP${C}\n" ; sudo pacman -Sc
- printf "${B}Done!${C}\n" ; read -r "reboot? (y/n) "-p W ; if [ "$W" != "${W#[Yy]}" ] ; then exit ; umount -a ; reboot ; fi    
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+curl -sL hhttps://git.io/JGVg5 > ~/installna.sh ; su --login root --session-command 'sh ~/installMO.sh'
