e32902cf58159d97a445b497b3f81b1fbb99ce3c
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-02 13:18:09 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 setfont ter-124b 
 A='\e[1;31m' ; B='\e[0m' 
 C="name,size -e 7,11" ; lsblk -do ${C}
-O='2>&1>/dev/null'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Mozart - Moonlight Sonata  ||  A='\e[1;31m' | B='\e[0m' | C="name,size -e 7,11" | read -r D | E="/dev/${D}"
 #  0:35 ━❍──────── -5:32     ||  F='btrfs su cr @' | G='mount -o noatime,compress=zstd,discard=async,subvol=@'
@@ -12,10 +11,10 @@ O='2>&1>/dev/null'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 printf "${A}Device name: ${B}" ; read -r D ; E="/dev/${D}" ; sgdisk ${E} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p 
 E1="$(ls /dev/* | grep -E "^${E}p?1$")" ; E2="$(ls /dev/* | grep -E "^${E}p?2$")" ; mkfs.vfat ${E1} ; mkfs.btrfs -fq ${E2}
-F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@' 
-mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt ; mkdir /mnt/{boot,home}
-${G}home ${E2} /mnt/home ; mount ${E1} /mnt/boot ; lsblk -o ${C} ; curl -sL https://git.io/Jsde3 > /mnt/install.sh
-H='pacman' ; I='TotalDownload' ; sed -i 's/#Color/Color/' /etc/${H}.conf && sed -i "s/#${I}/${I}/" /etc/${H}.conf 
+F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@' ; mount ${E2} /mnt ; cd /mnt ; ${F} 
+${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt ; mkdir /mnt/{boot,home} ; ${G}home ${E2} /mnt/home 
+mount ${E1} /mnt/boot ; lsblk -o ${C} ; curl -sL https://git.io/Jsde3 > /mnt/install.sh
+H='pacman' ; I='TotalDownload' ; sed -i 's/#Color/Color/' /etc/${H}.conf && sed -i s"/#${I}/${I}/" /etc/${H}.conf 
 gpg -k ; ${H}-key --init ; ${H}-key --populate archlinux >/dev/null
 reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/${H}.d/mirrorlist
 J='MODULES=' ; K='/etc/mkinitcpio.conf' ; L='xf86-video-' ; M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)"
