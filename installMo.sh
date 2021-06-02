a2aed3773525ec65766999a8e0c0d5cd3da1e367
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-03 01:18:11 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 set -euo pipefail 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='alacritty' ; B='\e[1;31m' ;  C='\e[0m' ; D='.bash' ; E='completion' ; F='https://' ; G='git clone' ; H='pikaur' ; I="pikaur -S --needed"
-J='xfce4' ; K='pipewire' ; L='firefox-developer-edition-i18n-' ; M='otf-fira-' ; N='manager' ; O='plugin' 
+A='alacritty' ; B='.bash' ; C='completion' ; D='https://' ; E='git clone' ; F='pikaur' ; G="pikaur -S --needed" ; H='xfce4' ; I='pipewire' 
+J='firefox-developer-edition-i18n-' ; K='otf-fira-' ; L='manager' ; M='plugin' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-cd ~ ;  ${G} ${F}github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tlsv1.2 -sSf ${F}sh.rustup.rs | sh ; source "$HOME"/.cargo/env ; 
-cargo build --release ; mkdir -p ~/${D}_${E} ; cp extra/${E}s/${A}${D} ~/${D}_${E}/${A} ; echo "source ~/${D}_${E}/${A}" >> ~/${D}rc
-cd ~ ; ${G} ${F}aur.archlinux.org/${H}.git ; cd ${H} ; makepkg -fsri
-${I} xorg rsync xfce-polkit openbox ${J}-settings archlinux-xdg-menu xfconf nm-connection-editor gufw xterm sddm-stellar-theme
-${I} alsa-utils ${K} ${K}-alsa ${K}-pulse ${K}-jack gst-${O}-${K} libpulse vlc picom xorg-xprop gvfs xcursor-breeze geany geany-${O}s
-${I} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components obkey ristretto menulibre timeshift 
-${I} ${L}en-us ${L}en-gb ${L}en-ca ${L}fr ${L}de ${L}it ${L}ja ${L}zh-cn ${L}zh-tw ${L}ru ${L}he ${L}pt-br ${M}sans ${M}mono ttf-ms-fonts 
-${I} ${J}-whiskermenu-${O} ${J}-task${N} ${J}-screenshooter ${J}-notes-${O} ${J}-appfinder ${J}-datetime-${O} ${J}-mpc-${O} 
-${I} galculator ; if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then ${H} ${J}-power-${N} ${J}-battery-${H} ; fi
+cd ~ ;  ${E} ${D}github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tlsv1.2 -sSf ${D}sh.rustup.rs | sh ; source "$HOME"/.cargo/env ; 
+cargo build --release ; mkdir -p ~/${B}_${C} ; cp extra/${C}s/${A}${B} ~/${B}_${C}/${A} ; echo "source ~/${B}_${C}/${A}" >> ~/${B}rc
+cd ~ ; ${E} ${D}aur.archlinux.org/${F}.git ; cd ${F} ; makepkg -fsri
+${G} xorg rsync xfce-polkit openbox ${H}-settings archlinux-xdg-menu xfconf nm-connection-editor gufw xterm sddm-stellar-theme
+${G} alsa-utils ${I} ${I}-alsa ${I}-pulse ${I}-jack gst-${M}-${I} libpulse vlc picom xorg-xprop gvfs xcursor-breeze geany geany-${M}s
+${G} nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components obkey ristretto menulibre timeshift 
+${G} ${J}en-us ${J}en-gb ${J}en-ca ${J}fr ${J}de ${J}it ${J}ja ${J}zh-cn ${J}zh-tw ${J}ru ${J}he ${J}pt-br ${K}sans ${K}mono ttf-ms-fonts 
+${G} ${H}-whiskermenu-${M} ${H}-task${L} ${H}-screenshooter ${H}-notes-${M} ${H}-appfinder ${H}-datetime-${M} ${H}-mpc-${M} 
+${G} galculator ; if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then ${F} ${H}-power-${L} ${H}-battery-${F} ; fi
 curl -sL hhttps://git.io/JGVg5 > ~/installna.sh ; sudo -i root -c 'sh ~/installna.sh'
