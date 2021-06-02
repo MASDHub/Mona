82bf240419cce8bbd67a5c442ac984bd7844154b
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-03 00:58:36 +0300

    Update installna.sh

--- installna.sh
+++ installna.sh
@@ -3,7 +3,7 @@ A='pikaur' = B='\e[1;31m' ; C='\e[0m' ;  D='.config' ; E='openbox' ; F='reverses
 J='<item label="' ; K='"><action name="Execute"><command>' L='</command></action></item>' ; M='manager'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
 sed -i "s/${F} no/${F} yes/" ~/${E}/${A}.conf ; sed -i "s/${G} no/${G} yes/" ~/${E}/${A}.conf
-cp ~/${H}/target/release/${H} /usr/local/bin ; mkdir -p ~/${D}/${E} ; sudo cp -a /etc/xdg/${E}/ ~/${D}/
+cp ~/${H}/target/release/${H} /usr/local/bin ; mkdir -p ~/${D}/${E} ; cp -a /etc/xdg/${E}/ ~/${D}/
 sed -i '5,$d' ~/${D}/${E}/menu.xml ; echo -e '<menu id="root-menu" label="Openbox 3">' >> ~/${D}/${E}/${I}
 echo -e "${J}Internet${K}firefox-developer-edition${L}\n${J}File Manager${K}nemo${L}\n${J}Terminal${K}${A}${L}\n" >> ~/${D}/${E}/${I}
 echo -e "${J}Text Editor${K}geany${L}\n${J}Settings${K}xfce4-settings-${M}${L}\n${J}Turn Off${K}sudo shutdown now${L}\n" >> ~/${D}/${E}/${I}
