c48568e835288cf8787d5222e9cf8f19426bd47d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-04 23:35:18 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -11,8 +11,8 @@ C="name,size -e 7,11" ; lsblk -do ${C}
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 printf "${A}Device name: ${B}" ; read -r D ; E="/dev/${D}" ; sgdisk ${E} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p 
 E1="$(ls /dev/* | grep -E "^${E}p?1$")" ; E2="$(ls /dev/* | grep -E "^${E}p?2$")" ; mkfs.vfat ${E1} ; mkfs.btrfs -fq ${E2}
-F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@' ; mount ${E2} /mnt ; cd /mnt ; ${F} 
-${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt ; mkdir /mnt/{boot,home} ; ${G}home ${E2} /mnt/home 
+F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@' ; mount ${E2} /mnt ; cd /mnt 
+${F} ; ${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt ; mkdir /mnt/{boot,home} ; ${G}home ${E2} /mnt/home 
 mount ${E1} /mnt/boot ; lsblk -o ${C} ; curl -sL https://git.io/Jsde3 > /mnt/install.sh
 H='pacman' ; I='TotalDownload' ; sed -i 's/#Color/Color/' /etc/${H}.conf && sed -i s"/#${I}/${I}/" /etc/${H}.conf 
 gpg -k ; ${H}-key --init ; ${H}-key --populate archlinux >/dev/null
