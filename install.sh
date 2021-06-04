5c190664e3acd10fadc5e1508722ea34c3a79ccd
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-04 06:12:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,14 +13,14 @@ head -n 15 install.sh | tail -n 13
 #d88%            %%%8--'-.        
 #/88:.__ ,       _%-' ---  -       
  #  '''::===..-'   =  --.  `                 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='\e[1;31m' ; B='\e[0m' ; C='printf' ; D='en_US.UTF-8' ; E='/etc/pacman.conf' 
-F='TotalDownload' ; G='/etc/locale.' ; H='/etc/host' ; I='echo "Try again"' ; W='%wheel ALL=(ALL) ALL' ; P='passwd' 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+A='\e[1;31m' ; B='\e[0m' ; C='printf' ; D='en_US.UTF-8' ; E='/etc/pacman.conf' ; F='TotalDownload' ; G='/etc/locale.'
+H='/etc/host' ; I='echo "Try again"' ; W='%wheel ALL=(ALL) ALL' ; P='passwd' 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
 sed -i 's/#Color/Color/' ${E} ; sed -i "s/#${F}/${F}/" ${E} ; pacman -S --needed git cmake freetype2 fontconfig pkg-config  
 ${C} "${A}Enter Root Password: ${B}\n" ; until ${P}  ; do ${I} ; done ; ${C} "${A}Enter User Name: ${B}" ;  read -r U 
-useradd -m -G wheel ${U} ; until ${P} ${U} ; do ${I} ; done ; sed -i "s/# ${W}/ ${W}/" /etc/sudoers 
-echo "${U}" >> u.txt ; chmod +x u.txt ; curl -sL https://git.io/Jspfl > /home/${U}/installMO.sh 
+useradd -m -G wheel ${U} ; until ${P} ${U} ; do ${I} ; done ; sed -i "s/# ${W}/ ${W}/" /etc/sudoers ; echo "${U}" >> u.txt 
+chmod +x u.txt ; curl -sL https://git.io/Jspfl > /home/${U}/installMO.sh 
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; hwclock --systohc
 echo "LC_ALL=${D}" >> /etc/environment ; echo "${D} UTF-8" >> ${G}gen ; echo "LANG=${D}" >> ${G}conf ; locale-gen ${D} 
 echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> ${H}s ; echo "${U}pc" >> ${H}name
