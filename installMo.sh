f60578daeaee8c50b23e65be4bbf886b5f43ac31
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-04 04:41:30 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 A='alacritty' ; B='.bash' ; C='completion' ; D='https://' ; E='git clone' ; F='pikaur' ; G="pikaur -S --needed" ; H='xfce4' ; I='pipewire' 
 J='firefox-developer-edition-i18n-' ; K='otf-fira-' ; L='manager' ; M='plugin' ; N=$USER
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-cd ~ ;  ${E} ${D}github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tlsv1.2 -sSf ${D}sh.rustup.rs | sh ; source "$HOME/.cargo/env"
+cd ~ ;  ${E} ${D}github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tlsv1.2 -sSf ${D}sh.rustup.rs | sh ; source "/home/${N}/.cargo/env"
 cargo build --release ; mkdir -p ~/${B}_${C} ; cp extra/${C}s/${A}${B} ~/${B}_${C}/${A} ; echo "source ~/${B}_${C}/${A}" >> ~/${B}rc
 cd ~ ; ${E} ${D}aur.archlinux.org/${F}.git ; cd ${F} ; makepkg -fsri
 ${G} xorg rsync xfce-polkit openbox ${H}-settings archlinux-xdg-menu xfconf nm-connection-editor gufw xterm sddm-stellar-theme
