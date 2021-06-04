c52c926acdc013567b1fe924ef83a55c3bbf3b64
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-04 19:20:40 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -8,7 +8,7 @@ cd ~ ;  ${E} ${D}github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tl
 source /home/${N}/.cargo/env ; cargo build --release ; mkdir -p ~/${B}_${C} ; mkdir -p ~/.config/${A} ; mkdir ~/.config/openbox
 cp extra/${C}s/${A}${B} ~/${B}_${C}/${A} ; cp ${A}.yml ~/.config/${A}/${A}.yml ; echo "source ~/${B}_${C}/${A}" >> ~/${B}rc
 cd ~ ; ${E} ${D}aur.archlinux.org/${F}.git ; cd ${F} ; makepkg -fsri ; curl -sL https://git.io/JGVg5 > ~/installna.sh 
-${G} xorg rsync xfce-polkit openbox ${H}-settings obconf nm-connection-editor gufw xterm sddm-stellar-theme \
+${G} xorg rsync xfce-polkit openbox obconf openbox-themes nm-connection-editor gufw xterm sddm-stellar-theme \
 alsa-utils ${I} ${I}-alsa ${I}-pulse ${I}-jack gst-${M}-${I} libpulse vlc picom xorg-xprop gvfs capitaine-cursors geany geany-${M}s \
 nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components obkey ristretto menulibre timeshift \
 ${J}en-us ${J}en-gb ${J}en-ca ${J}fr ${J}de ${J}it ${J}ja ${J}zh-cn ${J}zh-tw ${J}ru ${J}he ${J}pt-br ${K}sans ${K}mono ttf-ms-fonts \
