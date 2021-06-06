6d40d5940d80905868fdf80f4acb74ad856cb3bf
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-07 02:36:27 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -6,18 +6,10 @@ I='.config/openbox/menu.xml' ; J='<item label="' ; K='"><action name="Execute"><
 cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri ; curl -sL https://git.io/JGVg5 > ~/installna.sh
 pikaur -S --needed oranchelo-icon-theme obkey ttf-ms-fonts rofi-theme-menu-1080p timeshift 
 mkdir -p ~/.config/openbox ; cp -a /etc/xdg/openbox/ ~/.config/ ; sed -i '5,$d' ~/.config/openbox/menu.xml
-{ echo '<menu id="root-menu" label="Openbox 3">' 
-echo "${J}File Manager${K}nemo${L}"
-echo "${J}Search${K}rofi -show drun${L}"
-echo "${J}Internet${K}firefox-developer-edition${L}"
-echo "${J}Terminal${K}alacritty${L}"
-echo "${J}Text Editor${K}geany${L}"
-echo "${J}Settings${K}nemo${L}"
-echo "${J}Calculator${K}galculator${L}"
-echo "${J}Refresh${K}openbox --reconfigure${L}"
-echo "${J}Reboot${K}reboot${L}"
-echo "${J}Lock Screen${K}${L}"
-echo "${J}Power Off${K}poweroff now${L}" ; } >> ~/.config/openbox/menu.xml
+{ echo '<menu id="root-menu" label="Openbox 3">' ; echo "${J}File Manager${K}nemo${L}" ; echo "${J}Search${K}rofi -show drun${L}" 
+echo "${J}Internet${K}firefox-developer-edition${L}" ; echo "${J}Terminal${K}alacritty${L}" ; echo "${J}Text Editor${K}geany${L}" 
+echo "${J}Settings${K}nemo${L}" ; echo "${J}Calculator${K}galculator${L}" ; echo "${J}Refresh${K}openbox --reconfigure${L}" 
+echo "${J}Reboot${K}reboot${L}" ; echo "${J}Lock Screen${K}${L}" ; echo "${J}Power Off${K}poweroff now${L}" ; } >> ~/.config/openbox/menu.xml
 #sed -i '250,260d' ~/.config/openbox/rc.xml ; sed -e '248s/$/<keybind key="A-Tab"><action name="Execute">\
 #<command>rofi -dpi 96 -show window</command></action></keybind>/' ~/.config/openbox/rc.xml
 { echo -e "picom &\nlxqt-policykit &\n(sleep 2s && trayer --monitor primary --height 40 --align right --iconspacing 10 --transparent true --tint 0x716966) &\n"
