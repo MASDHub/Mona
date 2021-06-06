89d24a2f14b9b8bd84444a073d208b447305fe78
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-06 22:14:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ A='\e[1;31m' ; B='\e[0m' ; C='printf' ; D='en_US.UTF-8' ; E='/etc/pacman.conf' ;
 H='/etc/host' ; W='%wheel ALL=(ALL) ALL'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
 printf "${A}Enter Root Password: ${B}\n" ; until passwd ; do printf "\n" ; done 
-printf "${A}Enter User Name: ${B}" ; read -r U ; until [[ "${U}" -gt 4 ]] && [[ "${U}" == [a-z] ]]
-do printf "Try Again\n" && read -r U ; done 
+printf "${A}Enter User Name: ${B}" ; read -r U1 ; U="${U1,,}" ; until [[ ${#U} -gt 4 ]] && [[ "${U}" =~ [a-z] ]]
+do printf "Try Again\n" && read -r U1 && U="${U1,,}" ; done 
 useradd -m -G wheel ${U} ; sed -i "s/# ${W}/ ${W}/" /etc/sudoers ; until passwd ${U} ; do printf "\n" ; done 
 echo "${U}" >> /etc/u.txt ; chmod +x /etc/u.txt ; curl -sL https://git.io/Jspfl > /home/${U}/installMO.sh 
 sed -i 's/#Color/Color/' ${E} ; sed -i "s/#${F}/${F}/" ${E} ; pacman -S --needed git cmake freetype2 fontconfig pkg-config  
