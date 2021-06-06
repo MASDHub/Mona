890c1e6e054d2592ed7343be7a93d92f6fc55ff2
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-07 01:26:02 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/bash
 set -euo pipefail 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='alacritty' ; B='completion' ; C='git clone' ; D="pikaur -Syyu" ; E='pipewire' ; F='firefox-developer-edition-i18n-' ; G='otf-fira-' 
-U=$USER
+mkdir -p ~/.config/openbox ; cp -a /etc/xdg/openbox/ ~/.config/ 
+cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri ; curl -sL https://git.io/JGVg5 > ~/installna.sh 
+pikaur -S --needed oranchelo-icon-theme obkey ttf-ms-fonts rofi-theme-menu-1080p timeshift ; su --login root -c "sh /home/$USER/installna.sh"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-mkdir -p ~/.config/openbox ; cp -a /etc/xdg/openbox/ ~/.config/
-cd ~ ; ${C} https://aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri ; curl -sL https://git.io/JGVg5 > ~/installna.sh 
-${D} xorg rsync lxqt-policykit openbox obconf openbox-themes oranchelo-icon-theme nm-connection-editor network-manager-applet gufw \
-alsa-utils ${E} ${E}-alsa ${E}-pulse ${E}-jack gst-plugin-${E} libpulse vlc volumeicon picom xorg-xprop capitaine-cursors geany geany-plugins \
-nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components obkey arandr gvfs gvfs-mtp gvfs-afc \
-${F}en-us ${F}en-gb ${F}en-ca ${F}fr ${F}de ${F}it ${F}ja ${F}zh-cn ${F}zh-tw ${F}ru ${F}he ${F}pt-br ${G}sans ${G}mono ttf-ms-fonts \
-trayer rofi-theme-menu-1080p plank galculator xterm sddm timeshift alacritty 
-if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then ${D} tlp acpi cbatticon ; fi
-su --login root -c "sh /home/${U}/installna.sh"
