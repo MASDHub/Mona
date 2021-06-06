4319ff6977178a6776556400e2360ade5facace0
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-06 20:26:37 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -8,10 +8,10 @@ cd ~ ; ${E} ${D}github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tls
 source /home/${N}/.cargo/env ; cargo build --release ; mkdir -p ~/${B}_${C} ; mkdir -p ~/.config/${A} ; mkdir ~/.config/openbox
 cp extra/${C}s/${A}${B} ~/${B}_${C}/${A} ; cp ${A}.yml ~/.config/${A}/${A}.yml ; echo "source ~/${B}_${C}/${A}" >> ~/${B}rc
 cd ~ ; ${E} ${D}aur.archlinux.org/${F}.git ; cd ${F} ; makepkg -fsri ; curl -sL https://git.io/JGVg5 > ~/installna.sh 
-${G} xorg rsync lxqt-policykit openbox obconf openbox-themes nm-connection-editor network-manager-applet gufw xterm sddm-stellar-theme timeshift \
+${G} xorg rsync lxqt-policykit openbox obconf openbox-themes oranchelo-icon-theme nm-connection-editor network-manager-applet gufw 
 alsa-utils ${I} ${I}-alsa ${I}-pulse ${I}-jack gst-${M}-${I} libpulse vlc volumeicon picom xorg-xprop capitaine-cursors geany geany-${M}s \
 nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components obkey arandr gvfs gvfs-mtp gvfs-afc \
 ${J}en-us ${J}en-gb ${J}en-ca ${J}fr ${J}de ${J}it ${J}ja ${J}zh-cn ${J}zh-tw ${J}ru ${J}he ${J}pt-br ${K}sans ${K}mono ttf-ms-fonts \
-trayer ${H}-menu-1080p ${H}-android-1080p  ${H}-applet-1080p ${H}-launcher-1080p galculator
+trayer ${H}-menu-1080p ${H}-android-1080p  ${H}-applet-1080p ${H}-launcher-1080p galculator xterm sddm-stellar-theme timeshift \
 if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then ${F} tlp acpi cbatticon ; fi
 su --login root -c "sh /home/${N}/installna.sh"
