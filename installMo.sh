081f30c8908333bdc500c5c69b098419800e6744
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-06 23:24:20 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 set -euo pipefail 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='alacritty' ; B='.bash' ; C='completion' ; D='https://' ; E='git clone' ; F='pikaur' ; G="pikaur -Syyu" 
-H='rofi-theme' ; I='pipewire' ; J='firefox-developer-edition-i18n-' ; K='otf-fira-' ; L='manager' ; M='plugin' ; N=$USER
+A='alacritty' ; B='completion' ; C='git clone' ; D="pikaur -Syyu" ; E='pipewire' ; F='firefox-developer-edition-i18n-' ; G='otf-fira-' 
+N=$USER
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-cd ~ ; ${E} ${D}github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tlsv1.2 -sSf ${D}sh.rustup.rs | sh 
-source /home/${N}/.cargo/env ; cargo build --release ; mkdir -p ~/${B}_${C} ; mkdir -p ~/.config/${A} ; mkdir ~/.config/openbox
-cp extra/${C}s/${A}${B} ~/${B}_${C}/${A} ; cp ${A}.yml ~/.config/${A}/${A}.yml ; echo "source ~/${B}_${C}/${A}" >> ~/${B}rc
-cd ~ ; ${E} ${D}aur.archlinux.org/${F}.git ; cd ${F} ; makepkg -fsri ; curl -sL https://git.io/JGVg5 > ~/installna.sh 
-${G} xorg rsync lxqt-policykit openbox obconf openbox-themes oranchelo-icon-theme nm-connection-editor network-manager-applet gufw \
-alsa-utils ${I} ${I}-alsa ${I}-pulse ${I}-jack gst-${M}-${I} libpulse vlc volumeicon picom xorg-xprop capitaine-cursors geany geany-${M}s \
+cd ~ ; ${C} https://github.com/${A}/${A}.git ; cd ${A} ; curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
+source /home/${N}/.cargo/env ; cargo build --release ; mkdir -p ~/.bash_${B} ; mkdir -p ~/.config/${A} ; mkdir ~/.config/openbox
+cp extra/${B}s/${A}.bash ~/.bash_${B}/${A} ; cp ${A}.yml ~/.config/${A}/${A}.yml ; echo "source ~/.bash_${B}/${A}" >> ~/.bashrc
+cd ~ ; ${C} https://aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri ; curl -sL https://git.io/JGVg5 > ~/installna.sh 
+${D} xorg rsync lxqt-policykit openbox obconf openbox-themes oranchelo-icon-theme nm-connection-editor network-manager-applet gufw \
+alsa-utils ${E} ${E}-alsa ${E}-pulse ${E}-jack gst-plugin-${E} libpulse vlc volumeicon picom xorg-xprop capitaine-cursors geany geany-plugins \
 nemo nemo-fileroller nemo-image-converter nemo-preview nemo-python nemo-qt-components obkey arandr gvfs gvfs-mtp gvfs-afc \
-${J}en-us ${J}en-gb ${J}en-ca ${J}fr ${J}de ${J}it ${J}ja ${J}zh-cn ${J}zh-tw ${J}ru ${J}he ${J}pt-br ${K}sans ${K}mono ttf-ms-fonts \
-trayer ${H}-menu-1080p plank galculator xterm sddm-stellar-theme timeshift 
-if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then ${F} tlp acpi cbatticon ; fi
+${F}en-us ${F}en-gb ${F}en-ca ${F}fr ${F}de ${F}it ${F}ja ${F}zh-cn ${F}zh-tw ${F}ru ${F}he ${F}pt-br ${G}sans ${G}mono ttf-ms-fonts \
+trayer rofi-theme-menu-1080p plank galculator xterm sddm timeshift 
+if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then ${D} tlp acpi cbatticon ; fi
 su --login root -c "sh /home/${N}/installna.sh"
