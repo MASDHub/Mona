a01b80550ff3153a72d702e9512e2f8c76febce4
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-07 04:14:34 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -7,7 +7,7 @@ cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsr
 pikaur -S --needed oranchelo-icon-theme obkey ttf-ms-fonts rofi-theme-menu-1080p timeshift 
 mkdir -p ~/${A} ; cp -a /etc/xdg/openbox/ ~/.config/ ; sed -i '5,$d' ~/${A}/menu.xml
 { echo '<menu id="root-menu" label="Openbox 3">' ; echo "${B}File Manager${C}nemo${D}" ; echo "${B}Search${C}rofi -show drun${D}" 
-echo "${B}Internet${C}firefox-developer-edition${D}" ; echo "${B}Terminal${C}alacritty${D}" ; echo "${B}Text Editor${K}geany${D}" 
+echo "${B}Internet${C}firefox-developer-edition${D}" ; echo "${B}Terminal${C}alacritty${D}" ; echo "${B}Text Editor${C}geany${D}" 
 echo "${B}Settings${C}obconf${D}" ; echo "${B}Calculator${C}galculator${D}" ; echo "${B}Refresh${C}openbox --reconfigure${D}" 
 echo "${B}Reboot${C}reboot${D}" ; echo "${B}Lock Screen${C}${D}" ; echo "${B}Power Off${C}poweroff now${D}" ; } >> ~/${A}/menu.xml
 #sed -i '250,260d' ~/.config/openbox/rc.xml ; sed -e '248s/$/<keybind key="A-Tab"><action name="Execute">\
