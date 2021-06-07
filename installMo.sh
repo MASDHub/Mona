1e9d7c1a3125400ab975e93a48f2420028667726
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-07 20:50:11 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 A='.config/openbox' ; B='<item label="' ; C='"><action name="Execute"><command>' ; D='</command></action></item>' E="~/.config/plank"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri ; curl -sL https://git.io/JGVg5 > ~/installna.sh
-pikaur -S --needed picom-git oranchelo-icon-theme obkey ttf-ms-fonts timeshift 
+pikaur -S --needed picom-git rofi-dmenu oranchelo-icon-theme obkey ttf-ms-fonts timeshift 
 mkdir -p ~/${A} ; cp -a /etc/xdg/openbox/ ~/.config/ ; sed -i '5,$d' ~/${A}/menu.xml ; sed -i '5,$d' ~/${A}/autostart
 { echo '<menu id="root-menu" label="Openbox 3">' ; echo "${B}File Manager${C}nemo${D}" ; echo "${B}Search${C}rofi -show drun${D}" 
 echo "${B}Internet${C}firefox-developer-edition${D}" ; echo "${B}Terminal${C}alacritty${D}" ; echo "${B}Text Editor${C}geany${D}" 
