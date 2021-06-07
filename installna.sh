34cc278f31fcbe184ebe26498c5d44dda5bf7957
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-08 02:13:04 +0300

    Update installna.sh

--- installna.sh
+++ installna.sh
@@ -4,8 +4,8 @@ set -euo pipefail ; U=$(cat /etc/u.txt)
 A="/home/${U}/.config/pikaur.conf" ; B='\e[1;31m' ; C='\e[0m' ; F='noedit =' ; G='reversesearchsorting =' ; X='/usr/share/sddm/scripts/Xsetup' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
 sed -i "s/${G} no/${G} yes/" ${A} ; sed -i "s/${F} no/${F} yes/" ${A} ; update-desktop-database 
-printf "${B}DISPLAY MANAGER ENABLED${C}" ; systemctl enable sddm ; printf "${B}NETWORK ENABLED${C}\n" ; systemctl enable NetworkManager
-if [[ "$( pacman -Qd | grep -Ec tpl )" == [1-9] ]] ; then systemctl enable tlp && systemctl enable acpid ; fi
+printf "${B}DISPLAY MANAGER ENABLED${C}\n" ; systemctl enable sddm ; printf "${B}NETWORK ENABLED${C}\n" ; systemctl enable NetworkManager
+if [[ "$( pacman -Qd | grep -Ec tpl )" == [1-9] ]] ; then printf "${B}NETWORK ENABLED${C}\n" && systemctl enable tlp && systemctl enable acpid ; fi
 { echo 'M="$( xrandr | grep -Ec '"'HDMI-1|HDMI1|eDP1|eDP-1|VGA1|VGA-1|DVI1|DVI-1'"' )"' ; echo 'M1="$( xrandr | grep -Eo '"'eDP1|eDP-1'"' )"' 
 echo 'M2="$( xrandr | grep -Eo '"'HDMI-1|HDMI1|DVI-1|DVI1|VGA1|VGA-1'"' )"' ; echo 'if [[ "${M}" == "2" ]]'
 echo 'then xrandr --output "${M1}" --mode 1920x1080 --pos 0x0 --rotate normal --output "${M2}" --primary --auto --pos 1920x0 --rotate normal'
