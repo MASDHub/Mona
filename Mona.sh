a644b32e2f9a2a0e3984ce1207c6012ed52ac7ea
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-07 05:08:23 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -23,8 +23,8 @@ else M2="intel-ucode ${L}intel" && sed -i "s/${J}()/${J}(i915 btrfs)/" ${K} ; fi
 if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then Q="tlp acpi cbatticon" ; fi
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub vim ${M2} \
 xorg lxqt-policykit obconf nm-connection-editor network-manager-applet gufw picom xorg-xprop sddm xterm alacritty \
-alsa-utils ${N} ${N}-alsa ${N}-jack gst-plugin-${N} libpulse vlc volumeicon geany geany-plugins capitaine-cursors \
+alsa-utils ${N} ${N}-alsa ${N}-jack gst-plugin-${N} libpulse vlc volumeicon geany-plugins capitaine-cursors \
 nemo-fileroller nemo-image-converter nemo-preview arandr gvfs-mtp gvfs-afc trayer plank galculator \
-${P}en-us ${P}en-gb ${P}en-ca ${P}fr ${P}de ${P}it ${P}ja ${P}zh-cn ${P}zh-tw ${P}ru ${P}he ${P}pt-br ${O}sans ${O}mono ${Q}
+${P}en-us ${P}fr ${P}de ${P}it ${P}ja ${P}zh-cn ${P}ru ${P}he ${P}pt-br ${O}sans ${O}mono firefox-ublock-origin ${Q}
 genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt sh ./install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
