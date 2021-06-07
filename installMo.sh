4c1ef193e8c17f7cf07bdc96718e52f141fcfbd2
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-07 05:51:45 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -9,7 +9,7 @@ mkdir -p ~/${A} ; cp -a /etc/xdg/openbox/ ~/.config/ ; sed -i '5,$d' ~/${A}/menu
 { echo '<menu id="root-menu" label="Openbox 3">' ; echo "${B}File Manager${C}nemo${D}" ; echo "${B}Search${C}rofi -show drun${D}" 
 echo "${B}Internet${C}firefox-developer-edition${D}" ; echo "${B}Terminal${C}alacritty${D}" ; echo "${B}Text Editor${C}geany${D}" 
 echo "${B}Settings${C}obconf${D}" ; echo "${B}Calculator${C}galculator${D}" ; echo "${B}Refresh${C}openbox --reconfigure${D}" 
-echo "${B}Reboot${C}reboot${D}" ; echo "${B}Lock Screen${C}${D}" ; echo "${B}Power Off${C}poweroff now${D}" ; } >> ~/${A}/menu.xml
+echo "${B}Reboot${C}reboot${D}" ; echo "${B}Lock Screen${C}${D}" ; echo "${B}Power Off${C}poweroff${D}" ; } >> ~/${A}/menu.xml
 #sed -i '250,260d' ~/.config/openbox/rc.xml ; sed -e '248s/$/<keybind key="A-Tab"><action name="Execute">\
 #<command>rofi -dpi 96 -show window</command></action></keybind>/' ~/.config/openbox/rc.xml
 { echo -e "picom &\nlxqt-policykit &\n(sleep 2s && trayer --monitor primary --height 40 --align right --iconspacing 10 --transparent true --tint 0x716966) &"
