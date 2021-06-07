8251611af85184a2416ef604be6be0b29ea34ec3
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-07 20:27:06 +0300

    Update installna.sh

--- installna.sh
+++ installna.sh
@@ -6,10 +6,10 @@ A="/home/${U}/.config/pikaur.conf" ; B='\e[1;31m' ; C='\e[0m' ; F='noedit =' ; G
 sed -i "s/${G} no/${G} yes/" ${A} ; sed -i "s/${F} no/${F} yes/" ${A} ; update-desktop-database 
 printf "${B}DISPLAY MANAGER ENABLED${C}" ; systemctl enable sddm ; printf "${B}NETWORK ENABLED${C}\n" ; systemctl enable NetworkManager
 if [[ "$( pacman -Qd | grep -Ec tpl )" == [1-9] ]] ; then systemctl enable tlp && systemctl enable acpid ; fi
-{ echo 'M="$( xrandr | grep -Ec ''HDMI-1|HDMI1|eDP1|eDP-1|VGA1|VGA-1|DVI1|DVI-1'' )"'
-echo 'M1="$( xrandr | grep -Eo ''eDP1|eDP-1'' )"' ; echo 'M2="$( xrandr | grep -Eo ''HDMI-1|HDMI1|DVI-1|DVI1|VGA1|VGA-1'' )"' ; echo 'if [[ "${M}" == "2" ]]'
-echo 'then xrandr --output "${M1}" --mode 1920x1080 --pos 0x0 --rotate normal --output "${M2}" --primary --auto --pos 1920x0 --rotate normal ; fi' ; } >> ${X}
-cat /home/${U}/plank/docks.ini | dconf load /net/launchpad/plank/docks/ ; rm /home/${U}/plank/docks.ini
+{ echo 'M="$( xrandr | grep -Ec ''HDMI-1|HDMI1|eDP1|eDP-1|VGA1|VGA-1|DVI1|DVI-1'' )"' ; echo 'M1="$( xrandr | grep -Eo ''eDP1|eDP-1'' )"' 
+echo 'M2="$( xrandr | grep -Eo ''HDMI-1|HDMI1|DVI-1|DVI1|VGA1|VGA-1'' )"' ; echo 'if [[ "${M}" == "2" ]]'
+echo 'then xrandr --output "${M1}" --mode 1920x1080 --pos 0x0 --rotate normal --output "${M2}" --primary --auto --pos 1920x0 --rotate normal'
+echo 'fi' ; } >> ${X} ; cat /home/${U}/plank/docks.ini | dconf load /net/launchpad/plank/docks/ ; rm /home/${U}/plank/docks.ini
 printf "${B}Done! ( Type: 'exit' and then 'reboot' )${C}\n" ; rm /etc/u.txt ; rm /home/${U}/installMO.sh ; rm -- "$0"
 #sed -i '0,10d' >> .gtkrc-2.0 ; { echo -e 'gtk-theme-name="Oranchelo"\gtk-icon-theme-name="Oranchelo-Light"\ngtk-font-name="Fira Sans Compressed Book 12"'
 #echo -e 'gtk-cursor-theme-name="capitaine-cursors"\ngtk-cursor-theme-size=36" >> .gtkrc-2.0 
