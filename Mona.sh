3a258996ada03eb8e91809d83ac28c1c75089ad3
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-08 22:19:37 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -7,17 +7,22 @@ H='pacman' ; I='TotalDownload' ; J='MODULES=' ; K='/etc/mkinitcpio.conf'       #
 L='xf86-video-' ; M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)" ; N='pipewire' #    ↻     ⊲  Ⅱ  ⊳     ↺     
 O='otf-fira-' ; P='firefox-developer-edition-i18n-' ; Q='libreoffice-still-'   #    VOLUME: ▁▂▃▄▅▆▇ 100%   
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-lsblk -do ${C} ; printf "${A}Choose Device name: ${B}" ; read -r D ; until [[ "${D}" == +(nvme0n1|sda|sdb|hda|hdb|hdc|hdd|mmcblk0) ]] 
-do printf "Try Again\n" && read -r D ; done ; D1="/dev/${D}" ; sgdisk ${D1} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p
-E1="$(ls /dev/* | grep -E "^${D1}p?1$")" ; E2="$(ls /dev/* | grep -E "^${D1}p?2$")" ; mkfs.vfat ${E1} ; mkfs.btrfs -fq ${E2} 
-mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt ; mkdir /mnt/{boot,home} ; ${G}home ${E2} /mnt/home 
-mount ${E1} /mnt/boot ; lsblk -o ${C} ; curl -sL https://git.io/Jsde3 > /mnt/install.sh ; gpg -k ; ${H}-key --init 
-sed -i 's/#Color/Color/' /etc/${H}.conf ; sed -i s"/#${I}/${I}/" /etc/${H}.conf ; ${H}-key --populate archlinux >/dev/null
+lsblk -do ${C} ; printf "${A}Choose Device name: ${B}" ; read -r D 
+until [[ "${D}" == +(nvme0n1|sda|sdb|hda|hdb|hdc|hdd|mmcblk0) ]] 
+do printf "Try Again\n" && read -r D ; done ; D1="/dev/${D}" 
+sgdisk ${D1} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p
+E1="$(ls /dev/* | grep -E "^${D1}p?1$")" ; mkfs.vfat ${E1}
+E2="$(ls /dev/* | grep -E "^${D1}p?2$")" ; mkfs.btrfs -fq ${E2} 
+mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt 
+mkdir /mnt/{boot,home} ; ${G}home ${E2} /mnt/home ; mount ${E1} /mnt/boot ; lsblk -o ${C} 
+${H}-key --populate archlinux >/dev/null ; gpg -k ; ${H}-key --init 
+sed -i 's/#Color/Color/' /etc/${H}.conf ; sed -i s"/#${I}/${I}/" /etc/${H}.conf
 reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/${H}.d/mirrorlist
-if [[ "${M}" == "AMD" ]] || [[ "${M}" == "Intel" ]] ; then M1="${M}"; fi ; if [[ "${M1}" == "AMD" ]] 
-then M2="amd-ucode ${L}amdgpu" && sed -i "s/${J}()/${J}(amdgpu btrfs)/" ${K}
+if [[ "${M}" == "AMD" ]] || [[ "${M}" == "Intel" ]] ; then M1="${M}"; fi 
+if [[ "${M1}" == "AMD" ]] ; then M2="amd-ucode ${L}amdgpu" && sed -i "s/${J}()/${J}(amdgpu btrfs)/" ${K}
 else M2="intel-ucode ${L}intel" && sed -i "s/${J}()/${J}(i915 btrfs)/" ${K} ; fi
-if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then R="tlp acpid cbatticon" ; fi
+if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then R="tlp acpid cbatticon"
+fi ; curl -sL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/install.sh
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub vim ${M2} \
 xorg lxqt-policykit obconf nm-connection-editor network-manager-applet gufw xorg-xprop sddm xterm alacritty \
 alsa-utils ${N} ${N}-alsa ${N}-jack gst-plugin-${N} libpulse vlc volumeicon geany-plugins capitaine-cursors \
