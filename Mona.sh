2e50a7ad758151de2d3224561b1c6c66387ae96d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-08 22:25:03 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -15,8 +15,8 @@ E1="$(ls /dev/* | grep -E "^${D1}p?1$")" ; mkfs.vfat ${E1}
 E2="$(ls /dev/* | grep -E "^${D1}p?2$")" ; mkfs.btrfs -fq ${E2} 
 mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt 
 mkdir /mnt/{boot,home} ; ${G}home ${E2} /mnt/home ; mount ${E1} /mnt/boot ; lsblk -o ${C} 
-${H}-key --populate archlinux >/dev/null ; gpg -k ; ${H}-key --init 
-sed -i 's/#Color/Color/' /etc/${H}.conf ; sed -i s"/#${I}/${I}/" /etc/${H}.conf
+${H}-key --populate archlinux>/dev/null ; gpg -k ; ${H}-key --init 
+sed -i 's/#Color/Color/' /etc/${H}.conf ; sed -i "s/#${I}/${I}/" /etc/${H}.conf
 reflector -p https -c "$(curl -s https://ipapi.co/country_name)" -f 2 --save /etc/${H}.d/mirrorlist
 if [[ "${M}" == "AMD" ]] || [[ "${M}" == "Intel" ]] ; then M1="${M}"; fi 
 if [[ "${M1}" == "AMD" ]] ; then M2="amd-ucode ${L}amdgpu" && sed -i "s/${J}()/${J}(amdgpu btrfs)/" ${K}
