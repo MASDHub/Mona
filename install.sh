e56cd49fb5f20af6453d86af5bb408d78b9adb21
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-09 02:37:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,18 +14,20 @@ head -n 15 install.sh | tail -n 13
 #/88:.__ ,       _%-' ---  -       
  #  '''::===..-'   =  --.  `                 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='\e[1;31m' ; B='\e[0m' ; C='printf' ; D='en_US.UTF-8' ; E='/etc/pacman.conf' ; F='TotalDownload' ; G='/etc/locale.'
-H='/etc/host' ; W='%wheel ALL=(ALL) ALL'
+A='\e[1;31m' ; B='\e[0m' ; C='en_US.UTF-8' ; D='/etc/pacman.conf' ; E='TotalDownload' ; F='/etc/locale'
+G='/etc/host' ; W='%wheel ALL=(ALL) ALL'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
-printf "${A}Enter Root Password: ${B}\n" ; until passwd ; do printf "\n" ; done 
-printf "${A}Enter User Name: ${B}" ; read -r U1 ; U="${U1,,}" ; until [[ ${#U} -gt 4 ]] && [[ "${U}" =~ [a-z] ]]
-do printf "Try Again\n" && read -r U1 && U="${U1,,}" ; done ; useradd -m -G wheel ${U} 
-sed -i "s/# ${W}/ ${W}/" /etc/sudoers ; until passwd ${U} ; do printf "\n" ; done 
-echo "${U}" >> /etc/u.txt ; chmod +x /etc/u.txt ; curl -sL https://git.io/Jspfl > /home/${U}/installMO.sh 
-sed -i 's/#Color/Color/' ${E} ; sed -i "s/#${F}/${F}/" ${E} ; pacman -Syyyu --needed git cmake pkg-config  
+printf "${A}Enter Root Password: ${B}\n" ; until passwd ; do printf "\n"
+done ; printf "${A}Enter User Name: ${B}" ; read -r U1 ; U="${U1,,}" 
+until [[ ${#U} -gt 4 ]] && [[ "${U}" =~ [a-z] ]]
+do printf "Try Again\n" && read -r U1 && U="${U1,,}" 
+done ; useradd -m -G wheel ${U} ; sed -i "s/# ${W}/ ${W}/" /etc/sudoers 
+until passwd ${U} ; do printf "\n" ; done ; echo "${U}" >> /etc/u.txt ; chmod +x /etc/u.txt 
+curl -sL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh 
+sed -i 's/#Color/Color/' ${D} ; sed -i "s/#${E}/${E}/" ${D} ; pacman -Syyyu --needed git cmake pkg-config  
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; hwclock --systohc
-echo "LC_ALL=${D}" >> /etc/environment ; echo "${D} UTF-8" >> ${G}gen ; echo "LANG=${D}" >> ${G}conf ; locale-gen ${D} 
-echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> ${H}s ; echo "${U}pc" >> ${H}name
+echo "LC_ALL=${C}" >> /etc/environment ; echo "${C} UTF-8" >> ${F}.gen ; echo "LANG=${C}" >> ${F}.conf ; locale-gen ${C} 
+echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> ${G}s ; echo "${U}pc" >> ${G}name
 printf "${A}GRUB INSTALL ${B}\n" ; sed -i 's/'${N}'auto/'${N}'1920x1080,auto/' /etc/default/grub 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg
 cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${U} --session-command 'sh ~/installMO.sh' 
