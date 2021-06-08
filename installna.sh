0192ea6968d3d2d4d627b4ee83eb07bab7065da1
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-09 00:50:38 +0300

    Update installna.sh

--- installna.sh
+++ installna.sh
@@ -3,15 +3,15 @@ set -euo pipefail ; U=$(cat /etc/u.txt)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
 A="/home/${U}/.config/pikaur.conf" ; B='\e[1;31m' ; C='\e[0m' ; D='reversesearchsorting =' ; E='Adwaita/Oranchelo-Beka'
 F='Adwaita/Oranchelo' ; G='/usr/share/gtk-2.0/gtkrc' ; H='/usr/share/gtk-3.0/settings.ini' ; I='Cantarell 11/Fira Sans Condensed Book'
-X='/usr/share/sddm/scripts/Xsetup'
+J='gtk-cursor-theme-name=' ; K='capitaine-cursors-light' ; X='/usr/share/sddm/scripts/Xsetup' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
 printf "${B}DISPLAY MANAGER ENABLED${C}\n" ; systemctl enable sddm ; printf "${B}NETWORK ENABLED${C}\n" ; systemctl enable NetworkManager
 if [[ "$( pacman -Qd | grep -Ec tpl )" == [1-9] ]] ; then printf "${B}NETWORK ENABLED${C}\n" && systemctl enable tlp && systemctl enable acpid ; fi
 { echo 'M="$( xrandr | grep -Ec '"'HDMI-1|HDMI1|eDP1|eDP-1|VGA1|VGA-1|DVI1|DVI-1'"' )"' ; echo 'M1="$( xrandr | grep -Eo '"'eDP1|eDP-1'"' )"' 
 echo 'M2="$( xrandr | grep -Eo '"'HDMI-1|HDMI1|DVI-1|DVI1|VGA1|VGA-1'"' )"' ; echo 'if [[ "${M}" == "2" ]]'
 echo 'then xrandr --output "${M1}" --mode 1920x1080 --pos 0x0 --rotate normal --output "${M2}" --primary --auto --pos 1920x0 --rotate normal'
-echo 'fi' ; } >> ${X} ; sed -i -e "1 s/${E}/" -e "2 s/${F}/" ${H} ; sed -i -e "2 s/${E}/" -e "3 s/${F}/" ${H} 
-echo "gtk-cursor-theme-name= capitaine-cursors-light" >> ${H} ; echo 'gtk-cursor-theme-name= "capitaine-cursors-light"' >> ${G}
+echo 'fi' ; } >> ${X} ; sed -i -e "1 s/${E}/" -e "2 s/${F}/" ${G} ; sed -i -e "2 s/${E}/" -e "3 s/${F}/" ${H} 
+echo ''"${G}"' "'"${K}"'"' >> ${G} ; echo "${G} ${K}" >> ${H} 
 sed -i "s/${I}/" ${H} ; sed -i "s/${I}/" ${G} ; update-desktop-database ; sed -i "s/${D} no/${D} yes/" ${A}
 printf "${B}Done! ( Type: 'exit' and then 'reboot' )${C}\n" ; rm /etc/u.txt ; rm /home/${U}/installMO.sh ; rm -- "$0"
 #echo -e 'gtk-cursor-theme-name="capitaine-cursors"\ngtk-cursor-theme-size=36" >> .gtkrc-2.0 
