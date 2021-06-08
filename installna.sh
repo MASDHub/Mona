8390ea22824e1baaaabb21f0611e0913188db453
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-08 16:41:11 +0300

    Update installna.sh

--- installna.sh
+++ installna.sh
@@ -12,6 +12,5 @@ echo 'M2="$( xrandr | grep -Eo '"'HDMI-1|HDMI1|DVI-1|DVI1|VGA1|VGA-1'"' )"' ; ec
 echo 'then xrandr --output "${M1}" --mode 1920x1080 --pos 0x0 --rotate normal --output "${M2}" --primary --auto --pos 1920x0 --rotate normal'
 echo 'fi' ; } >> ${X} ; sed -i -e "2 s/${F}/" -e "3 s/${F1}/" /usr/share/gtk-3.0/settings.ini ; sed -i -e "1 s/${F}/" -e "2 s/${F2}/" /usr/share/gtk-2.0/gtkrc
 printf "${B}Done! ( Type: 'exit' and then 'reboot' )${C}\n" ; rm /etc/u.txt ; rm /home/${U}/installMO.sh ; rm -- "$0"
-#sed -i '0,10d' >> .gtkrc-2.0 ; { echo -e 'gtk-theme-name="Oranchelo"\gtk-icon-theme-name="Oranchelo-Light"\ngtk-font-name="Fira Sans Compressed Book 12"'
 #echo -e 'gtk-cursor-theme-name="capitaine-cursors"\ngtk-cursor-theme-size=36" >> .gtkrc-2.0 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
