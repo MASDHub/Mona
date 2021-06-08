73895a6ecb89950336df78240cec1207e59bc1b3
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-09 01:50:32 +0300

    Update installna.sh

--- installna.sh
+++ installna.sh
@@ -11,9 +11,9 @@ printf "${B}NETWORK ENABLED${C}\n" ; systemctl enable NetworkManager
 { echo 'M="$( xrandr | grep -Ec '"'HDMI-1|HDMI1|eDP1|eDP-1|VGA1|VGA-1|DVI1|DVI-1'"' )"'
  echo 'M1="$( xrandr | grep -Eo '"'HDMI-1|HDMI1|DVI-1|DVI1|VGA1|VGA-1'"' )"'
  echo 'M2="$( xrandr | grep -Eo '"'eDP1|eDP-1'"' )"' ; echo 'if [[ "${M}" -ge "2" ]]' 
- echo 'then xrandr --output "${M2}" --mode 1920x1080 --output "${M1}" --primary --auto'; echo 'fi' ; } >> ${X} 
-sed -i -e "1 s/${E}/" -e "2 s/${F}/" ${G} ; sed -i -e "2 s/${E}/" -e "3 s/${F}/" ${H} ; sed -i "s/${D}no/${D}yes/" ${A}
-echo ''"${J}"' "'"${K}"'"' >> ${G} ; echo "${J} ${K}" >> ${H} ; sed -i "s/${I}/" ${H} ; sed -i "s/${I}/" ${G}
-update-desktop-database ; printf "${B}Done! ( Type: 'exit' and then 'reboot' )${C}\n" ; rm /etc/u.txt ; rm /home/${U}/installMO.sh 
+ echo 'then xrandr --output "${M2}" --mode 1920x1080 --output "${M1}" --primary --auto' ; echo 'fi' ; } >> ${X} 
+sed -i -e "1 s/${E}/" -e "2 s/${F}/" "${G}" ; sed -i -e "2 s/${E}/" -e "3 s/${F}/" ${H} ; sed -i "s/${D}no/${D}yes/" ${A}
+echo ''"${J}"' "'"${K}"'"' >> "${G}" ; echo "${J} ${K}" >> ${H} ; sed -i "s/${I}/" ${H} ; sed -i "s/${I}/" ${G}
+update-desktop-database ; printf "${B}Done! to Finish Type: 'exit' then 'reboot'${C}\n" ; rm /etc/u.txt ; rm /home/${U}/installMO.sh 
 rm -- "$0"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
