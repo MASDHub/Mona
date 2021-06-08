1dfcd3db7e387be8fc7e003d63ea26267c5f6dca
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-08 19:22:18 +0300

    Update installna.sh

--- installna.sh
+++ installna.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail ; U=$(cat /etc/u.txt)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
-A="/home/${U}/.config/pikaur.conf" ; B='\e[1;31m' ; C='\e[0m' ; D='reversesearchsorting =' ; E=Adwita/Oranchelo-Beka  
-F=Adwita/Oranchelo ; G="/usr/share/gtk-2.0/gtkrc" ; H="/usr/share/gtk-3.0/settings.ini" ; X='/usr/share/sddm/scripts/Xsetup' 
+A="/home/${U}/.config/pikaur.conf" ; B='\e[1;31m' ; C='\e[0m' ; D='reversesearchsorting =' ; E='Adwita/Oranchelo-Beka'
+F='Adwita/Oranchelo' ; G="/usr/share/gtk-2.0/gtkrc" ; H="/usr/share/gtk-3.0/settings.ini" ; X='/usr/share/sddm/scripts/Xsetup' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
 printf "${B}DISPLAY MANAGER ENABLED${C}\n" ; systemctl enable sddm ; printf "${B}NETWORK ENABLED${C}\n" ; systemctl enable NetworkManager
 if [[ "$( pacman -Qd | grep -Ec tpl )" == [1-9] ]] ; then printf "${B}NETWORK ENABLED${C}\n" && systemctl enable tlp && systemctl enable acpid ; fi
