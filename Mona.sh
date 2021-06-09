8a56d81d12bc13da4904770f154b9a3db0b5636b
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-10 02:53:45 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -20,9 +20,8 @@ ${H}-key --init ; ${H}-key --populate archlinux>/dev/null
 sed -i -e 's/#Color/Color/' -e "s/#${I}/${I}/" /etc/${H}.conf
 timedatectl set-timezone "$(${T}timezone)" ; timedatectl set-timezone
 reflector -p https -c "$(${T}country_name)" -f 2 --save /etc/${H}.d/mirrorlist
-if [[ "${M}" == 'AMD' ]] || [[ "${M}" == 'Intel' ]] ; then M1="${M}"; fi #; if [[ "${M1}" == 'AMD' ]] 
-#then M2="amd-ucode ${L}amdgpu" && M3='amdgpu btrfs' ; else M2="intel-ucode ${L}intel" && M3='i915 btrfs' ; fi
-#sed -i "s/${J}()/${J}(${M3})/" ${K} || printf "${A}No GPU Detected${B}\n" 
+if [ "${M}" == 'Intel' ] ; then M1='intel-ucode ${L}intel' && "s/${J}()/${J}(${i915 btrfs})/"; fi
+if [ "${M}" == 'AMD' ] ; then M1='amd-ucode ${L}amdgpu' && "s/${J}()/${J}(${amdgpu btrfs})/" ; fi 
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/install.sh
 pacstrap -i /mnt  base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub vim $M2 \
 xorg lxqt-policykit obconf nm-connection-editor network-manager-applet gufw xorg-xprop sddm xterm alacritty \
