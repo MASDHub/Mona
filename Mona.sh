6fea827501a10317d38d8e3deaa94610f124fa82
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-10 21:45:27 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -10,18 +10,18 @@ J='otf-fira-' ; K='firefox-developer-edition-i18n-' ; L='libreoffice-still-'    
 gpg -k | pacman-key --init | pacman-key --populate archlinux
 timedatectl set-timezone "$(${I}timezone)" ; timedatectl set-ntp true
 for i in {1..20} ; do echo -ne "${A}\r${C:0:$i}${B}" && sleep 1.8 
-done ; echo "" ; lsblk -do ${E} ; echo -n "${A}Choose Device name: ${B}"
+done ; echo "" ; lsblk -do ${E} ; printf "${A}Choose Device name: ${B}"
 read -r D ; until [[ "${D}" == +(nvme0n1|sda|sdb|hda|hdb|hdc|hdd|mmcblk0) ]] 
-do printf "Try Again\n" && read -r D ; done ; D1="/dev/${D}" 
+do printf "${A}Try Again${B}\n" && read -r D ; done ; D1="/dev/${D}" 
 sgdisk ${D1} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p
 E1="$(ls /dev/* | grep -E "^${D1}p?1$")" ; mkfs.vfat ${E1}
 E2="$(ls /dev/* | grep -E "^${D1}p?2$")" ; mkfs.btrfs -fq ${E2} 
 mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt 
  ${G} ${E2} /mnt ; mkdir /mnt/{boot,home} ; ${G}home ${E2} /mnt/home
 reflector -p https -c "$(${I}country_name)" -f 2 --save /etc/pacman.d/mirrorlist
-if [ "${H}" == 'Intel' ] ; then H1='intel-ucode xf86-video-intel' && H2='i915'
-elif [ "${H}" == 'AMD' ] ; then H1='amd-ucode xf86-video-amdgpu'  && H2='amdgpu' ; fi
-if [ -n "${H}" ] ; then sed -i "s/MODULES=()/MODULES=(${H2} btrfs)/" /etc/mkinitcpio.conf ; fi 
+if [ "${H}" == 'Intel' ] ; then H1='intel-ucode xf86-video-intel vulkan-intel' && H2='i915'
+elif [ "${H}" == 'AMD' ] ; then H1='amd-ucode xf86-video-amdgpu vulkan-radeon' && H2='amdgpu' ; fi
+if [[ -n "${H}" ]] ; then sed -i "s/MODULES=()/MODULES=(${H2} btrfs)/" /etc/mkinitcpio.conf ; fi 
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/install.sh ; lsblk -o ${E}
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub vim \
 xorg lxqt-policykit obconf-qt nm-connection-editor network-manager-applet gufw xorg-xprop sddm xterm alacritty \
