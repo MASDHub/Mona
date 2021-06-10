8b2009ebb5b50467ce7b65c732d7f81c5ebefd87
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-10 20:15:29 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -1,12 +1,11 @@
 #!/bin/bash  
 set -euo pipefail ; setfont ter-124b 
-A='\e[1;31m' ; B='\e[0m' ; C="name,size -e 7,11"; 
-sed -i -e 's/#Color/Color/' -e 's/#TotalDownload/TotalDownload/' /etc/pacman.conf 
+A='\e[1;31m' ; B='\e[0m' ; C="name,size -e 7,11"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@'  # Mozart - Moonlight Sonata 
-J='MODULES=' ; K='curl -s https://ipapi.co/'                                   #  0:35 ━❍──────── -5:32    
-L='xf86-video-' ; M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)" ; N='' #    ↻     ⊲  Ⅱ  ⊳     ↺     
-O='otf-fira-' ; P='firefox-developer-edition-i18n-' ; Q='libreoffice-still-'   #    VOLUME: ▁▂▃▄▅▆▇ 100%   
+sed -i -e 's/#Color/Color/' -e 's/#TotalDownload/TotalDownload/' /etc/pacman.conf # Mozart - Moonlight Sonata 
+F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@'     #  0:35 ━❍──────── -5:32    
+M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)"; K='curl -s https://ipapi.co/'      #   ↻     ⊲  Ⅱ  ⊳     ↺     
+O='otf-fira-' ; P='firefox-developer-edition-i18n-' ; Q='libreoffice-still-'      #  VOLUME: ▁▂▃▄▅▆▇ 100%   
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k | pacman-key --init | pacman-key --populate archlinux
 timedatectl set-timezone "$(${K}timezone)" ; timedatectl set-ntp true
@@ -19,9 +18,9 @@ E2="$(ls /dev/* | grep -E "^${D1}p?2$")" ; mkfs.btrfs -fq ${E2}
 mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt 
  ${G} ${E2} /mnt ; mkdir /mnt/{boot,home} ; ${G}home ${E2} /mnt/home
 reflector -p https -c "$(${K}country_name)" -f 2 --save /etc/pacman.d/mirrorlist
-if [ "${M}" == 'Intel' ] ; then M1="intel-ucode ${L}intel" && M2='i915'
-elif [ "${M}" == 'AMD' ] ; then M1="amd-ucode ${L}amdgpu" && M2='amdgpu' ; fi
-if [ -n "${M}" ] ; then sed -i "s/${J}()/${J}(${M2} btrfs)/" /etc/mkinitcpio.conf ; fi 
+if [ "${M}" == 'Intel' ] ; then M1="intel-ucode xf86-video-intel" && M2='i915'
+elif [ "${M}" == 'AMD' ] ; then M1="amd-ucode xf86-video-amdgpu" && M2='amdgpu' ; fi
+if [ -n "${M}" ] ; then sed -i "s/MODULES=()/MODULES=(${M2} btrfs)/" /etc/mkinitcpio.conf ; fi 
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/install.sh ; lsblk -o ${C}
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub vim \
 xorg lxqt-policykit obconf-qt nm-connection-editor network-manager-applet gufw xorg-xprop sddm xterm alacritty \
