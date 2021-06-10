64b8b23fe4b87a4d79d7f5873367a45d25598c5d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-10 03:48:45 +0300

    Update installna.sh

--- installna.sh
+++ installna.sh
@@ -16,6 +16,6 @@ printf "${B}NETWORK ENABLED${C}\n" ; systemctl enable NetworkManager
  echo 'then xrandr --output "${M3}" --mode 1920x1080 --output "${M2}" --primary --auto' ; echo -e 'fi\nfi' ; } >> ${X} 
 sed -i -e "1 s/${E}/" -e "2 s/${F}/" "${G}" ; sed -i -e "2 s/${E}/" -e "3 s/${F}/" ${H} ; sed -i "s/${D}no/${D}yes/" ${A}
 echo ''"${J}"' "'"${K}"'"' >> "${G}" ; echo "${J} ${K}" >> ${H} ; sed -i "s/${I}/" ${H} ; sed -i "s/${I}/" ${G}
-update-desktop-database ; printf "${B}Done! to Finish Type: 'exit' then 'reboot'${C}\n" ; 
+update-desktop-database ; printf "${B}Done! to Finish Type: 'exit' then 'reboot'${C}\n"
 rm /etc/u.txt ; rm install.sh ; rm /home/${U}/installMO.sh ; umount -a 2>&1>/dev/null ; rm -- "$0"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
