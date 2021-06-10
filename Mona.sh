cb44133c5f46b744edf1b93cf0d791686d394eab
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-10 19:46:21 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -1,28 +1,27 @@
 #!/bin/bash  
 set -euo pipefail ; setfont ter-124b 
-A='\e[1;31m' ; B='\e[0m' ; C="name,size -e 7,11"; T='curl -s https://ipapi.co/'
+A='\e[1;31m' ; B='\e[0m' ; C="name,size -e 7,11"; 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 F='btrfs su cr @' ; G='mount -o noatime,compress=zstd,discard=async,subvol=@'  # Mozart - Moonlight Sonata 
-H='pacman' ; I='TotalDownload' ; J='MODULES=' ; K='/etc/mkinitcpio.conf'       #  0:35 ━❍──────── -5:32    
+H='pacman' ; I='TotalDownload' ; J='MODULES=' ; K='curl -s https://ipapi.co/'  #  0:35 ━❍──────── -5:32    
 L='xf86-video-' ; M="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)" ; N='pipewire' #    ↻     ⊲  Ⅱ  ⊳     ↺     
 O='otf-fira-' ; P='firefox-developer-edition-i18n-' ; Q='libreoffice-still-'   #    VOLUME: ▁▂▃▄▅▆▇ 100%   
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-timedatectl set-timezone "$(${T}timezone)" ; timedatectl set-ntp true
+timedatectl set-timezone "$(${K}timezone)" ; timedatectl set-ntp true
 lsblk -do ${C} ; printf "${A}Choose Device name: ${B}" ; read -r D 
 until [[ "${D}" == +(nvme0n1|sda|sdb|hda|hdb|hdc|hdd|mmcblk0) ]] 
 do printf "Try Again\n" && read -r D ; done ; D1="/dev/${D}" 
 sgdisk ${D1} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p
 E1="$(ls /dev/* | grep -E "^${D1}p?1$")" ; mkfs.vfat ${E1}
 E2="$(ls /dev/* | grep -E "^${D1}p?2$")" ; mkfs.btrfs -fq ${E2} 
-mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home 
-cd ; umount /mnt ; ${G} ${E2} /mnt; mkdir /mnt/{boot,home}
-mount ${E1} /mnt/boot ; ${G}home ${E2} /mnt/home ; lsblk -o ${C}
+mount ${E2} /mnt ; cd /mnt ; ${F} ; ${F}home ; cd ; umount /mnt ; ${G} ${E2} /mnt
+mkdir /mnt/{boot,home} ; mount ${E1} /mnt/boot ; ${G}home ${E2} /mnt/home ; lsblk -o ${C}
 sed -i -e 's/#Color/Color/' -e "s/#${I}/${I}/" /etc/${H}.conf
 gpg -k | ${H}-key --init | ${H}-key --populate archlinux
-reflector -p https -c "$(${T}country_name)" -f 2 --save /etc/${H}.d/mirrorlist
+reflector -p https -c "$(${K}country_name)" -f 2 --save /etc/${H}.d/mirrorlist
 if [ "${M}" == 'Intel' ] ; then M1="intel-ucode ${L}intel" && M2='i915'
 else if [ "${M}" == 'AMD' ] ; then M1="amd-ucode ${L}amdgpu" && M2='amdgpu' 
-elif [ -n "${M}" ]; then sed -i "s/${J}()/${J}(${M2} btrfs)/" ${K} ; fi ; fi
+elif [ -n "${M}" ]; then sed -i "s/${J}()/${J}(${M2} btrfs)/" /etc/mkinitcpio.conf ; fi ; fi
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/install.sh
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub vim \
 xorg lxqt-policykit obconf-qt nm-connection-editor network-manager-applet gufw xorg-xprop sddm xterm alacritty \
