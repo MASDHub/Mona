aef4572cefa752581ea758d961addef64a7a89ab
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-10 23:51:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,17 +17,19 @@ head -n 15 install.sh | tail -n 13
 A='\e[1;31m' ; B='\e[0m' ; C='en_US.UTF-8' ; D='/etc/pacman.conf' ; E='TotalDownload' ; F='/etc/locale'
 G='/etc/host' ; H='systemctl enable' ; W='%wheel ALL=(ALL) ALL'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
-printf "${A}Enter Root Password: ${B}\n" ; until passwd ; do echo "" ; done 
-printf "${A}Enter User Name: ${B}" ; read -r U1 ; U="${U1,,}" ; until [[ ${#U} -gt 4 ]] && [[ "${U}" =~ [a-z] ]] 
-do printf "${A}Try Again\n${B}" && read -r U1 && U="${U1,,}" ; done ; useradd -m -G wheel ${U} ; until passwd ${U} 
-do echo "" ; done ; sed -i "s/# ${W}/ ${W}/" /etc/sudoers ; echo "${U}" >> /etc/u.txt ; chmod +x /etc/u.txt 
+printf "${A}Enter Root Password: ${B}\n" ; until passwd ; do echo "" ; done ; printf "${A}Enter User Name: ${B}" 
+read -r U1 ; U="${U1,,}" ; until [[ ${#U} -gt 4 ]] && [[ "${U}" =~ [a-z] ]] 
+do printf "${A}Try Again\n${B}" && read -r U1 && U="${U1,,}" ; done 
+useradd -m -G wheel ${U} ; until passwd ${U} ; do echo "" ; done 
+sed -i "s/# ${W}/ ${W}/" /etc/sudoers ; echo "${U}" >> /etc/u.txt
 curl -ssL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh 
-sed -i 's/#Color/Color/' ${D} ; sed -i "s/#${E}/${E}/" ${D} ; pacman -Syyyu --needed git cmake pkg-config  
+sed -i -e 's/#Color/Color/' ${D} -e "s/#${E}/${E}/" ${D} ; pacman -Syyyu --needed git cmake pkg-config  
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; hwclock --systohc
 echo "LC_ALL=${C}" >> /etc/environment ; echo "${C} UTF-8" >> ${F}.gen ; echo "LANG=${C}" >> ${F}.conf ; locale-gen ${C} 
 echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> ${G}s ; echo "${U}pc" >> ${G}name
 printf "${A}GRUB INSTALL ${B}\n" ; sed -i 's/'${N}'auto/'${N}'1920x1080,auto/' /etc/default/grub 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg
-printf "${A}DISPLAY MANAGER ENABLED${B}\n" ; ${H} sddm ; printf "${A}NETWORK ENABLED${B}\n" ; ${H} NetworkManager
+printf "${A}CONNECTION SERVICES ENABLED${B}\n" ; ${H} NetworkManager ; ${H} avahi-daemon ; ${H} bluetooth
+printf "${A}DISPLAY MANAGER ENABLED${B}\n" ; ${H} sddm 
 cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${U} --session-command "sh ~/installMO.sh" 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
