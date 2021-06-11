29164c4b68c8e83ca583efdcdc59d674295b79a0
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-11 21:03:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,8 +28,8 @@ ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; h
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh 
 echo -e "LC_ALL=${C}" >> /etc/environment -e "${C} UTF-8" >> ${F}.gen -e "LANG=${C}" >> ${F}.conf ; locale-gen ${C} 
 echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> ${G}s -e "${U}pc" >> ${G}name
-printf "${A}GRUB INSTALLED ${B}\n" ; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 
-grub-mkconfig -o /boot/grub/grub.cfg ; printf "${A}NETWORK ENABLED${B}\n" ; ${H} NetworkManager ; ${H} avahi-daemon 
-printf "${A}BLUETHOOTH ENABLED${B}\n" ; ${H} bluetooth ; printf "${A}DISPLAY MANAGER ENABLED${B}\n" ; ${H} sddm
+echo -e "${A}GRUB INSTALLED ${B}" ; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 
+grub-mkconfig -o /boot/grub/grub.cfg; echo -e "${A}NETWORK ENABLED${B}" ; ${H} NetworkManager ; ${H} avahi-daemon 
+echo -e "${A}BLUETHOOTH ENABLED${B}" ; ${H} bluetooth ; echo -e "${A}DISPLAY MANAGER ENABLED${B}" ; ${H} sddm
 cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${U} --session-command "sh ~/installMO.sh" 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
