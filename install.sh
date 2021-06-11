cdff4b5eb2d72358deec6fe67965260c06c08c2e
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-11 20:58:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ done ; useradd -m -G wheel ${U} ; until passwd ${U} ; do echo ""
 done ; sed -i "s/# ${W}/ ${W}/" /etc/sudoers ; echo "${U}" >> /etc/u
 sed -i -e 's/#Co/Co/' ${D} /etc/pacman.conf ; sed -i 's/auto/1920x1080,auto/' /etc/default/grub 
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; hwclock --systohc
-curl -ssL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh 
+curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh 
 echo -e "LC_ALL=${C}" >> /etc/environment -e "${C} UTF-8" >> ${F}.gen -e "LANG=${C}" >> ${F}.conf ; locale-gen ${C} 
 echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> ${G}s -e "${U}pc" >> ${G}name
 printf "${A}GRUB INSTALLED ${B}\n" ; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 
