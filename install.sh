9be2f4f7bfa3ccebf784d7ff87130a36b6dc856d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 01:49:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,10 +26,10 @@ done ; sed -i '0,/# %/ s/# %/ %/' /etc/sudoers ; echo "${U}" >> /etc/u
 sed -i -e 's/#Co/Co/' /etc/pacman.conf ; sed -i 's/auto/1920x1080,auto/' /etc/default/grub 
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime  ; hwclock --systohc
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh 
-if [[ -n "$(cat /etc/locale.gen | grep -o en_${G})" ]]
+echo -e "127.0.0.1       localhost\n::1             localhost \n127.0.1.1       ${U}pc.localdomain ${U}pc" >> /${E}s 
+echo -e "${U}pc" >> /${E}name ; if [[ -n "$(cat /etc/locale.gen | grep -o en_${G})" ]]
 then sed -i "s/#${G}/${G}/" /${D}.gen && echo "LANG="$(cat /etc/locale.gen | grep en_${G})"" >> /${D}.conf 
 else sed -i "s/#${C}/${C}/" /${D}.gen && echo 'LANG=en_US.UTF-8' >> /${D}.conf ; fi ; locale-gen 
-echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> /${E}s ; echo -e "${U}pc" >> /${E}name
 echo -e "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager ; echo -e "${A}INSTALLING GRUB ${B}" 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg
 echo -e "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth ; echo -e "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm
