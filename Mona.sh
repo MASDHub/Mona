1502f0c0134645b6bbd4e7a6f0547b8694308ffb
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-11 22:06:37 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -11,11 +11,11 @@ gpg -k | pacman-key --init | pacman-key --populate archlinux
 timedatectl set-timezone "$(${J}timezone)" ; timedatectl set-ntp true
 for i in {1..30} ; do echo -ne "${A}\r${D:0:$i}${B}" && sleep 1.8 
 done ; echo "" ; lsblk -do ${C} ; printf "${A}Choose Device name: ${B}"
-read -r D ; until [[ "${D}" == +(nvme0n1|sda|sdb|hda|hdb|hdc|hdd|mmcblk0) ]] 
-do printf "${A}Sorry, try again.${B}\n" && read -r EE ; done ; E="/dev/${EE}" 
-sgdisk ${E} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p
-E1="$(ls /dev/* | grep -E "^${E}p?1$")" ; mkfs.vfat ${E1}
-E2="$(ls /dev/* | grep -E "^${E}p?2$")" ; mkfs.btrfs -fq ${E2} 
+read -r E ; until [[ "${E}" == +(nvme0n1|sda|sdb|hda|hdb|hdc|hdd|mmcblk0) ]] 
+do printf "${A}Sorry, try again.${B}\n" && read -r E ; done ; ED="/dev/${E}" 
+sgdisk ${ED} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p
+E1="$(ls /dev/* | grep -E "^${ED}p?1$")" ; mkfs.vfat ${E1}
+E2="$(ls /dev/* | grep -E "^${ED}p?2$")" ; mkfs.btrfs -fq ${E2} 
 mount ${E2} /mnt ; cd /mnt ; ${G} ; ${G}home ; cd ; umount /mnt ; ${I} ${E2} /mnt 
 mkdir /mnt/{boot,home} ; ${H}home ${E2} /mnt/home ; mount ${E1} /mnt/boot ; lsblk -o ${C}
 if [ "${I}" == 'Intel' ] ; then H1='intel-ucode xf86-video-intel vulkan-intel' && H2='i915'
