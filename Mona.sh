c66ca90119720abcdc25784a0278e74a74f42c0d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-11 21:58:27 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -16,13 +16,13 @@ do printf "${A}Sorry, try again.${B}\n" && read -r EE ; done ; E="/dev/${EE}"
 sgdisk ${E} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p
 E1="$(ls /dev/* | grep -E "^${E}p?1$")" ; mkfs.vfat ${E1}
 E2="$(ls /dev/* | grep -E "^${E}p?2$")" ; mkfs.btrfs -fq ${E2} 
-mount ${E2} /mnt ; cd /mnt ; ${G} ; ${G}home ; cd ; umount /mnt 
- ${I} ${E2} /mnt ; mkdir /mnt/{boot,home} ; ${H}home ${E2} /mnt/home ; mount ${E1} /mnt/boot 
+mount ${E2} /mnt ; cd /mnt ; ${G} ; ${G}home ; cd ; umount /mnt ; ${I} ${E2} /mnt 
+mkdir /mnt/{boot,home} ; ${H}home ${E2} /mnt/home ; mount ${E1} /mnt/boot ; lsblk -o ${C}
 if [ "${I}" == 'Intel' ] ; then H1='intel-ucode xf86-video-intel vulkan-intel' && H2='i915'
 elif [ "${I}" == 'AMD' ] ; then H1='amd-ucode xf86-video-amdgpu vulkan-radeon' && H2='amdgpu' 
-fi ; if [[ -n "${I}" ]]  ; then sed -i -e '/#/d' -e "s/LES=()/LES=(${H2} btrfs)/" /${F} ; fi
+fi ; if [[ -n "${I}" ]]  ; then sed -i -e '/#/d' -e "s/LES=()/LES=(${H2} btrfs)/" /${F} ; fi 
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/install.sh 
-lsblk -o ${C} ; reflector -p https -c "$(${J}country_name)" -f 2 --save /etc/pacman.d/mirrorlist 
+reflector -p https -c "$(${J}country_name)" -f 2 --save /etc/pacman.d/mirrorlist || pacman -Syy
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub git vim \
 lxqt-policykit xlockmore python-pyxdg lxappearance-obconf-gtk3 nm-connection-editor network-manager-applet \
 alsa-utils pipewire pipewire-alsa pipewire-jack libpulse volumeicon blueman vlc gst-plugin-pipewire geeqie \
