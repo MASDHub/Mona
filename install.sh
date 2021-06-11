f3a404a674cdee79ba498c36197f4ffecd4511d8
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-11 21:09:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ head -n 15 install.sh | tail -n 13
 #/88:.__ ,       _%-' ---  -       
  #  '''::===..-'   =  --.  `                 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='\e[1;31m' ; B='\e[0m' ; C='en_US.UTF-8' ; F='/etc/locale' ; G='/etc/host' ; H='systemctl enable' 
+A='\e[1;31m' ; B='\e[0m' ; C='en_US.UTF-8' ; D='etc/locale' ; E='etc/host' ; F='systemctl enable' 
 W='%wheel ALL=(ALL) ALL'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
 printf "${A}Enter Root Password: ${B}\n" ; until passwd ; do echo "" 
@@ -26,10 +26,10 @@ done ; sed -i "s/# ${W}/ ${W}/" /etc/sudoers ; echo "${U}" >> /etc/u
 sed -i -e 's/#Co/Co/' /etc/pacman.conf ; sed -i 's/auto/1920x1080,auto/' /etc/default/grub 
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime ; hwclock --systohc
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh 
-echo -e "LC_ALL=${C}" >> /etc/environment -e "${C} UTF-8" >> ${F}.gen -e "LANG=${C}" >> ${F}.conf ; locale-gen ${C} 
-echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> ${G}s -e "${U}pc" >> ${G}name
-echo -e "${A}GRUB INSTALLED ${B}" ; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB 
-grub-mkconfig -o /boot/grub/grub.cfg; echo -e "${A}NETWORK ENABLED${B}" ; ${H} NetworkManager ; ${H} avahi-daemon 
-echo -e "${A}BLUETHOOTH ENABLED${B}" ; ${H} bluetooth ; echo -e "${A}DISPLAY MANAGER ENABLED${B}" ; ${H} sddm
+echo -e "LC_ALL=${C}" >> /etc/environment -e "${C} UTF-8" >> /${D}.gen -e "LANG=${C}" >> /${D}.conf ; locale-gen ${C} 
+echo -e "127.0.0.1 localhost \n::1 localhost \n127.0.1.1 ${U}pc.localdomain ${U}pc \n" >> /${E}s -e "${U}pc" >> /${E}name
+echo -e "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager ; echo -e "${A}GRUB INSTALLED ${B}" 
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth ; echo -e "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm
 cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${U} --session-command "sh ~/installMO.sh" 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
