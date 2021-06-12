a01242ce042699954d597a0d90801e3f6c9f8fce
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 20:52:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,9 +29,9 @@ echo -e "127.0.0.1${I}localhost\n::1${J}localhost\n127.0.1.1${I}${U}pc.localdoma
 echo -e "${U}pc" >> /${E}name ; if [[ "$(cat /etc/locale.gen | grep -c en_${G})" == 1 ]]
 then sed -i "s/#${H}/${H}/" /${D}.gen && echo -e "LANG=${H}" >> /${D}.conf
 else sed -i "s/#${C}/${C}/" /${D}.gen && echo -e "LANG=${C}" >> /${D}.conf 
-fi ; locale-gen ; echo "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm
-echo "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager ; echo "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth
-echo "${A}INSTALLING GRUB ${B}"; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+fi ; locale-gen ; echo -e "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm
+echo -e "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager ; echo -e "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth
+echo -e "${A}INSTALLING GRUB ${B}"; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 sed -i 's/auto/1920x1080,auto/' /etc/default/grub ; grub-mkconfig -o /boot/grub/grub.cfg
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh
 cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${U} --session-command "sh ~/installMO.sh" 
