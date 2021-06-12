240cfcfac31e34ed52fe6440a676e12cdb17eedc
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 10:48:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,11 +18,11 @@ head -n 16 /etc/install.sh | tail -n 13
 A='\e[1;31m' ; B='\e[0m' ; C='en_US.UTF-8' ; D='etc/locale' ; E='etc/host' ; F='systemctl enable' 
 F="$(curl -s https://ipapi.co/country_code)" ; G="$(cat /${D}.gen | grep en_${F} | cut 2-20)"
 H='       ' ; I='             ' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
-echo -e "${A}Enter Root Password: ${B}" ; until passwd  ; do echo ''
-done ; echo -ne "\n${A}Enter User Name: ${B}" ; read U1 ; U="${U1,,}"
+printf "${A}Enter Root Password: ${B}" ; until passwd ; do echo ''
+done ; printf "\n${A}Enter User Name: ${B}" ; read U1 ; U="${U1,,}"
 until [[ ${#U} -gt 4 ]] && [[ "${U}" =~ [a-z] ]]
-do printf "\n${A}Try again: ${B}" && read U1 ; done
-useradd -m -G wheel ${U} ; until passwd ${U} ; do echo "" ; done
+do printf "\n${A}Try again : ${B}" && read U1 ; done
+useradd -m -G wheel "${U}" ; until passwd ${U}; do echo "" ; done
 sed -i '0,/# %/ s/# %/ %/' /etc/sudoers ; echo "${U}" >> /etc/u
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime  ; hwclock --systohc
 echo -e "127.0.0.1${H}localhost\n::1${I}localhost\n127.0.1.1${H}${U}pc.localdomain ${U}pc" >> /${E}s
