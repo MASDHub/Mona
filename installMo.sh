5e68a348f8f3ba087b80b947c7735ae2593c2bdd
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 10:43:19 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -5,7 +5,7 @@ A='.config/openbox' ; B='<item label="' ; C='"><action name="Execute"><command>'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then R='uacpid cbatticon'
 cd ~ ; git clone https://aur.archlinux.org/pikaur.git ; cd pikaur ; makepkg -fsri
-pikaur -Syyu --needed picom-git oranchelo-icon-theme-git obkey ttf-ms-fonts timeshift
+pikaur -Syyu picom-git oranchelo-icon-theme-git obkey ttf-ms-fonts timeshift
 mkdir ~/${A} ; cp -a /etc/xdg/openbox/ ~/.config/ ; sed -i '5,$d' ~/${A}/menu.xml #echo "Xcursor.size: 36" >> ~/.Xresources
 { echo -e "<menu id="'"root-menu"'" label="'"Openbox 3"'">\n${B}Files${C}nemo${D}
 ${B}Search${C}rofi -show drun${D}\n${B}Web${C}firefox-developer-edition${D}\n${B}Terminal${C}alacritty${D}
@@ -16,7 +16,7 @@ sed -i -e '50,120 s/8/12/' -e '50,120 s/9/13/' -e 's/sans/Fira Sans Condensed Bo
 -e 's/W-e/0x85/' -e 's/Konqueror/Dmenu/' -e 's/kfmclient openProfile filemanagement/rofi -show drun/' \
 -e '131 s/4/2/' ~/${A}/rc.xml ; { echo -e 'lxqt-policykit &\n\npicom &\n\n\nplank &\n
 trayer --monitor primary --height 40 --align right --iconspacing 10 --transparent true --tint 0x716966 &\n
-(nm-applet) &\n\n(volumeicon) &' ; } > ~/${A}/autostart ; pikaur -Scc --noconfirm
+(nm-applet) &\n\n(volumeicon) &' ; } > ~/${A}/autostart ; pikaur -Scc
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installna.sh > ~/installna.sh
 echo "exec openbox-session" >> ~/.xinitrc ; su --login root -c "sh /home/$USER/installna.sh"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
