c43a18d469ae08a356dbef7f5517ee50d76f8a36
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 04:21:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ head -n 15 /etc/install.sh | tail -n 13
  #  '''::===..-'   =  --.  `                 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 A='\e[1;31m' ; B='\e[0m' ; C='en_US.UTF-8' ; D='etc/locale' ; E='etc/host' ; F='systemctl enable' 
-G="$(curl -s https://ipapi.co/country_code)" ; G1="$(cat /${D} | grep en_${G} | cut 2-20)"
-F='       ' ; F1='             ' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
+F1="$(curl -s https://ipapi.co/country_code)" ; F2="$(cat /${D} | grep en_${F1} | cut 2-20)"
+G1='       ' ; G2='             ' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
 echo -e "${A}Enter Root Password: ${B}" ; until passwd ; do echo "" 
 done ; echo -ne "\n${A}Enter User Name: ${B}" ; read -r U1 ; U="${U1,,}"
 until [[ ${#U} -gt 4 ]] && [[ "${U}" =~ [a-z] ]] 
@@ -26,10 +26,10 @@ done ; sed -i '0,/# %/ s/# %/ %/' /etc/sudoers ; echo "${U}" >> /etc/u
 sed -i -e 's/#Co/Co/' /etc/pacman.conf ; sed -i 's/auto/1920x1080,auto/' /etc/default/grub 
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime  ; hwclock --systohc
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh 
-echo -e "127.0.0.1${F}localhost\n::1${F1}localhost \n127.0.1.1${F}${U}pc.localdomain ${U}pc" >> /${E}s 
-echo -e "${U}pc" >> /${E}name ; if [[ "$(cat /etc/locale.gen | grep -c en_${G})" == 1 ]]
-then sed -i "s/#${G1}/${G1}/" /${D}.gen && echo "LANG=${G1}" >> /${D}.conf 
-else sed -i "s/#${C}/${C}/" /${D}.gen && echo "LANG=${C}" >> /${D}.conf ; fi ; locale-gen 
+echo -e "127.0.0.1${G1}localhost\n::1${G2}localhost \n127.0.1.1${G1}${U}pc.localdomain ${U}pc" >> /${E}s 
+echo -e "${U}pc" >> /${E}name ; if [[ "$(cat /etc/locale.gen | grep -c en_${F1})" == 1 ]]
+then sed -i "s/#${F2}/${F2}/" /${D}.gen && echo -e "LANG=${F2}" >> /${D}.conf 
+else sed -i "s/#${C}/${C}/" /${D}.gen && echo -e "LANG=${C}" >> /${D}.conf ; fi ; locale-gen 
 echo -e "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager ; echo -e "${A}INSTALLING GRUB ${B}" 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ; grub-mkconfig -o /boot/grub/grub.cfg
 echo -e "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth ; echo -e "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm
