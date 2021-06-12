79d2c2e552161b73dec11d50bf68feb8fcff83fc
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 13:01:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ head -n 16 /etc/install.sh | tail -n 13
  #  '''::===..-'   =  --.  `                 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 A='\e[1;31m' ; B='\e[0m' ; C='en_US.UTF-8' ; D='etc/locale' ; E='etc/host' ; F='systemctl enable' 
-F="$(curl -s https://ipapi.co/country_code)" ; G="$(cat /${D}.gen | grep en_${F} | cut 2-20)"
-H='       ' ; I='             ' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
+G="$(curl -s https://ipapi.co/country_code)" ; H="$(cat /${D}.gen | grep en_${G} | cut 2-20)"
+I='       ' ; J='             ' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#           
 printf "${A}Enter Root Password: ${B}" ; until passwd ; do echo ''
 done ; printf "\n${A}Enter User Name: ${B}" ; read U1 ; U="${U1,,}"
 until [[ ${#U} -gt 4 ]] && [[ "${U}" =~ [a-z] ]]
@@ -25,9 +25,9 @@ do printf "\n${A}Try again : ${B}" && read U1 ; done
 useradd -m -G wheel "${U}" ; until passwd ${U}; do echo "" ; done
 sed -i '0,/# %/ s/# %/ %/' /etc/sudoers ; echo "${U}" >> /etc/u
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime  ; hwclock --systohc
-echo -e "127.0.0.1${H}localhost\n::1${I}localhost\n127.0.1.1${H}${U}pc.localdomain ${U}pc" >> /${E}s
-echo -e "${U}pc" >> /${E}name ; if [[ "$(cat /etc/locale.gen | grep -c en_${F})" == 1 ]]
-then sed -i "s/#${G}/${G}/" /${D}.gen && echo -e "LANG=${G}" >> /${D}.conf
+echo -e "127.0.0.1${I}localhost\n::1${J}localhost\n127.0.1.1${I}${U}pc.localdomain ${U}pc" >> /${E}s
+echo -e "${U}pc" >> /${E}name ; if [[ "$(cat /etc/locale.gen | grep -c en_${G})" == 1 ]]
+then sed -i "s/#${H}/${H}/" /${D}.gen && echo -e "LANG=${H}" >> /${D}.conf
 else sed -i "s/#${C}/${C}/" /${D}.gen && echo -e "LANG=${C}" >> /${D}.conf 
 fi ; locale-gen ; echo "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm
 echo "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager ; echo "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth
