736a54b8d7b0a247eea626456487264b6e43b984
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 10:14:00 +0300

    Update installna.sh

--- installna.sh
+++ installna.sh
@@ -5,14 +5,16 @@ A="/home/${U}/.config/pikaur.conf" ; B='\e[1;31m' ; C='\e[0m' ; D='reversesearch
 F='Adwaita/Oranchelo' ; G='/usr/share/gtk-2.0/gtkrc' ; H='/usr/share/gtk-3.0/settings.ini' ; I='Cantarell 11/Fira Sans Condensed Book'
 J='gtk-cursor-theme-name=' ; K='Captain Frank' ; X='/usr/share/sddm/scripts/Xsetup' 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# 
-#if [[ "$( pacman -Qd | grep -Ec tpl )" == [1-9] ]] ; then printf "${B}NETWORK ENABLED${C}\n" && systemctl enable tlp && systemctl enable acpid ; fi
+#if [[ "$( pacman -Qd | grep -Ec tpl )" == [1-9] ]] ; systemctl enable tlp && systemctl enable acpid ; fi
 { echo 'M="$(find /home/*/.screenlayout/*.sh)"' ; echo 'M1="$( xrandr | grep -Eo '"'eDP1|eDP-1'"' )"' 
  echo 'M2="$( xrandr | grep -Ec '"'HDMI-1|HDMI1|eDP1|eDP-1|VGA1|VGA-1|DVI1|DVI-1'"' )"'
  echo 'M3="$( xrandr | grep -Eo '"'HDMI-1|HDMI1|DVI-1|DVI1|VGA1|VGA-1'"' )"'
  echo 'if [ -r "${M}" ] && $(grep -q xrandr ${M}) ; then ${M} ; else if [[ "${M2}" -ge "2" ]]' 
  echo 'then xrandr --output "${M1}" --mode 1920x1080 --output "${M3}" --primary --auto' ; echo -e 'fi\nfi' ; } >> ${X} 
-sed -i -e "1 s/${E}/" -e "2 s/${F}/" "${G}" ; sed -i -e "2 s/${E}/" -e "3 s/${F}/" ${H} ; sed -i "s/${D}no/${D}yes/" ${A}
-echo ''"${J}"' "'"${K}"'"' >> "${G}" ; echo "${J} ${K}" >> ${H} ; sed -i "s/${I}/" ${H} ; sed -i "s/${I}/" ${G}
-gtk4-update-icon-cache -f -t /usr/share/icons/Oranchelo-Beka/ ; printf "${B}Done! to Finish Type: 'exit' then 'reboot'${C}\n" 
-rm /etc/u.txt /home/${U}/installMO.sh /etc/install.sh ; umount -fa > /dev/null 2>&1 && rm -- "$0"  
+sed -i -e 's/#Co/Co/' /etc/pacman.conf ; sed -i "s/${D}no/${D}yes/" ${A}
+sed -i -e "1 s/${E}/" -e "2 s/${F}/" -e "s/${I}/" ${G} ; echo ''"${J}"' "'"${K}"'"' >> "${G}"
+sed -i -e "2 s/${E}/" -e "3 s/${F}/" -e "s/${I}/" ${H} ; echo "${J} ${K}" >> ${H} 
+rm /etc/u.txt /home/${U}/installMO.sh /etc/install.sh
+echo -e "${B}Done!\nType: 'exit' then 'reboot'${C}" 
+rm -- "$0"  
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
