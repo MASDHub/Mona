e50f016c0b1110ca192ca2602f7a8fbf223705b7
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 12:21:15 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -21,7 +21,7 @@ mount ${E2} /mnt; cd /mnt ; ${G}home ; ${G} ; cd ; umount  /mnt
 mount ${E1} /mnt/boot ; lsblk -o NAME,MOUNTPOINT,PARTUUID -e 7,11
 if [ "${I}" == Intel ]; then H1='intel' && H2='i915 '
 elif [ "${I}" == AMD ]; then H1='amd' && H2='amdgpu '
-elif [[ ${#I} -gt 1 ]]; then H2="${H1}-ucode xf86-video-${H1}"
+elif [[ ${#I} -gt 1 ]]; then H3="${H1}-ucode xf86-video-${H1}"
 fi ; sed -i "s/LES=()/LES=(${H2}btrfs)/" /${F}
 reflector -p https -c "$(curl -s ${J}country_name)" -f 2 --save /etc/pacman.d/mirrorlist
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub vim git \
@@ -29,7 +29,7 @@ lxqt-policykit xlockmore python-pyxdg lxappearance-obconf-gtk3 nm-connection-edi
 alsa-utils pipewire pipewire-alsa pipewire-jack libpulse volumeicon blueman vlc gst-plugin-pipewire geeqie \
 nemo-fileroller nemo-preview geany-plugins gvfs-mtp gvfs-afc capitaine-cursors alacritty xterm trayer rofi \
 ${K}en-us ${K}de ${K}ja ${K}zh-cn ${K}ru ${K}ar ${K}pt-br otf-fira-sans otf-fira-mono pkg-config sddm gufw \
-${L}en-gb ${L}hi ${L}ko ${L}zh-tw ${L}uk ${L}he ${L}es cmake htop arandr galculator plank ${H2}
+${L}en-gb ${L}hi ${L}ko ${L}zh-tw ${L}uk ${L}he ${L}es cmake htop arandr galculator plank ${H3}
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/etc/install.sh
 cp /${F} /mnt/${F} ; genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh /etc/install.sh
