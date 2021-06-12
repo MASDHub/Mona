aa4e9645888fe4ff4808c7de2cafb913b3ecac50
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 10:22:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,14 +25,14 @@ do printf "\n${A}Try again: ${B}" && read U1 ; done
 useradd -m -G wheel ${U} ; until passwd ${U} ; do echo "" ; done
 sed -i '0,/# %/ s/# %/ %/' /etc/sudoers ; echo "${U}" >> /etc/u
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime  ; hwclock --systohc
-curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh
 echo -e "127.0.0.1${H}localhost\n::1${I}localhost\n127.0.1.1${H}${U}pc.localdomain ${U}pc" >> /${E}s
 echo -e "${U}pc" >> /${E}name ; if [[ "$(cat /etc/locale.gen | grep -c en_${F})" == 1 ]]
 then sed -i "s/#${G}/${G}/" /${D}.gen && echo -e "LANG=${G}" >> /${D}.conf
-else sed -i "s/#${C}/${C}/" /${D}.gen && echo -e "LANG=${C}" >> /${D}.conf ; fi ; locale-gen 
+else sed -i "s/#${C}/${C}/" /${D}.gen && echo -e "LANG=${C}" >> /${D}.conf 
+fi ; locale-gen ; echo "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm
 echo "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager ; echo "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth
 echo "${A}INSTALLING GRUB ${B}"; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 sed -i 's/auto/1920x1080,auto/' /etc/default/grub ; grub-mkconfig -o /boot/grub/grub.cfg
-echo "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm
+curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh
 cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${U} --session-command "sh ~/installMO.sh" 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
