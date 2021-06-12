7724879d5564012c3167bcaed0287d10143e17a9
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-12 08:58:43 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -8,11 +8,10 @@ I="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)" ; J='https://ipapi.co/'         # 
 K='firefox-developer-edition-i18n-' ; L='libreoffice-still-'                  #     VOLUME: ▁▂▃▄▅▆▇ 100%
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k | pacman-key --init | pacman-key --populate 
-timedatectl set-timezone "$(curl -s ${J}timezone)" ; timedatectl set-ntp true
-for i in {1..30} ; do echo -ne "${A}\r${D:0:$i}${B}" && sleep 1.8 ; done
-echo "" ; lsblk -do NAME,SIZE -e 7,11 | egrep --color "${C}|NAME"
-echo -en "\n${A}Choose Device name: ${B}"; read -r E
-until [[ "${E}" == +(${C}) ]] ; do echo -en "${A}Sorry, try again: ${B}" && read -r E
+timedatectl set-timezone "$(curl -s ${J}timezone)" #; timedatectl set-ntp true
+lsblk -do NAME,SIZE -e 7,11 | egrep --color "${C}|NAME" #echo "" ;  for i in {1..30} ; do echo -ne "${A}\r${D:0:$i}${B}" && sleep 1.8 ; done
+echo -en "\n${A}Choose Device name: ${B}"; read E
+until [[ "${E}" == +(${C}) ]] ; do printf "${A}try again: ${B}" && read -r E
 done ; ED="/dev/${E}" ; sgdisk ${ED} -Z -o -n 1::+512M -t 1:EF00 -n -i -v -p
 E1="$(ls /dev/* | grep -E "^${ED}p?1$")" ; mkfs.vfat ${E1}
 E2="$(ls /dev/* | grep -E "^${ED}p?2$")" ; mkfs.btrfs -fq ${E2}
@@ -22,7 +21,6 @@ lsblk -o NAME,SIZE,MOUNTPOINT -e 7,11 | egrep  --color '|/mnt/|home|boot|MOUNTPO
 if [ "${I}" == 'Intel' ] ; then H1='intel-ucode xf86-video-intel vulkan-intel' && H2='i915'
 elif [ "${I}" == 'AMD' ] ; then H1='amd-ucode xf86-video-amdgpu vulkan-radeon' && H2='amdgpu'
 fi ; if [[ -n "${I}" ]]  ; then sed -i -e '/#/d' -e "s/LES=()/LES=(${H2} btrfs)/" /${F} ; fi
-curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/install.sh
 reflector -p https -c "$(curl -s ${J}country_name)" -f 2 --save /etc/pacman.d/mirrorlist || pacman -Syy
 pacstrap -i /mnt base base-devel linux linux-headers linux-firmware networkmanager efibootmgr grub git vim \
 lxqt-policykit xlockmore python-pyxdg lxappearance-obconf-gtk3 nm-connection-editor network-manager-applet \
@@ -30,6 +28,7 @@ alsa-utils pipewire pipewire-alsa pipewire-jack libpulse volumeicon blueman vlc 
 nemo-fileroller nemo-preview geany-plugins gvfs-mtp gvfs-afc obconf-qt pkg-config otf-fira-sans plank rofi \
 ${K}en-us ${K}de ${K}ja ${K}zh-cn ${K}ru ${K}ar ${K}pt-br arandr otf-fira-mono galculator alacritty trayer \
 ${L}en-gb ${L}hi ${L}ko ${L}zh-tw ${L}uk ${L}he ${L}es gufw cmake xterm htop sddm xorg ${H1}
-sed -i "s/LES=()/LES=(${H2} btrfs)/" /mnt/${F} ; mv /mnt/install.sh /mnt/etc/install.sh
-genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt sh /etc/install.sh
+curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/etc/install.sh
+sed -i "s/LES=()/LES=(${H2} btrfs)/" /mnt/${F} ; genfstab -U /mnt >> /mnt/etc/fstab 
+arch-chroot /mnt sh /etc/install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
