dd6126ccc7487795daf4557345c6d494dea8600f
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-13 23:21:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,23 +24,30 @@ until [ ${#U} -gt 4 ] && [[ "${U}" =~ [a-z] ]]
 do echo -en "\n${A}Try again : ${B}" && read U1 && U="${U1,,}" ; done  
 useradd -m -G wheel "${U}" ; until passwd ${U} ; do echo "" ; done
 sed -i '0,/# %/ s/# %/ %/' /etc/sudoers 
+sed -i "s/#en_US.U/en_US.U/" /etc/locale.gen 
+sed -i -e '1 s/"'${C}-Beka'"/' -e '2 s/"'${C}'"/' -e 's/"'${D}'"/' ${E}/gtk-2.0/gtkrc   
+sed -i -e "2 s/${C}-Beka/" -e "3 s/${C}/" -e "s/${D}/" ${E}/gtk-3.0/settings.ini 
 ln -sf "/share/zoneinfo/$(curl -s https://ipapi.co/timezone)" /etc/localtime  
-sed -i "s/#en_US.U/en_US.U/" /etc/locale.gen && echo -e "LANG=${C}" >> /etc/locale.conf
-echo -e "127.0.0.1${I}localhost\n::1${J}localhost\n127.0.1.1${I}${U}pc.localdomain ${U}pc" >> /etc/hosts
-echo -e "${U}pc" >> /etc/hostname ; hwclock --systohc ; locale-gen ; rm /etc/install.sh
-curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh
+{ echo -e "127.0.0.1       localhost
+::1             localhost\n
+127.0.1.1       ${U}pc.localdomain ${U}pc" ; } >> /etc/hosts
+echo -e "LANG=${C}" >> /etc/locale.conf ; locale-gen
+echo -e "${U}pc" >> /etc/hostname ; hwclock --systohc 
 #if [[ "$( pacman -Qd | grep -Ec tpl )" == [1-9] ]] ; systemctl enable tlp && systemctl enable acpid ; fi
-{ echo 'M="$(find /home/*/.screenlayout/*.sh)"' ; echo 'M1="$( xrandr | grep -Eo '"'eDP1|eDP-1'"' )"'
+{ echo 'M="$(find /home/*/.screenlayout/*.sh)"' 
+echo 'M1="$( xrandr | grep -Eo '"'eDP1|eDP-1'"' )"'
 echo 'M2="$( xrandr | grep -Ec '"'HDMI-1|HDMI1|eDP1|eDP-1|VGA1|VGA-1|DVI1|DVI-1'"' )"'
 echo 'M3="$( xrandr | grep -Eo '"'HDMI-1|HDMI1|DVI-1|DVI1|VGA1|VGA-1'"' )"'
 echo 'if [ -r "${M}" ] && $(grep -q xrandr ${M}) ; then ${M} ; else if [[ "${M2}" -ge "2" ]]'
 echo 'then xrandr --output "${M1}" --mode 1920x1080 --output "${M3}" --primary --auto'
 echo -e 'fi\nfi' ; } >> ${E}/sddm/scripts/Xsetup
-sed -i -e '1 s/"'${C}-Beka'"/' -e '2 s/"'${C}'"/' -e 's/"'${D}'"/' ${E}/gtk-2.0/gtkrc   
-sed -i -e "2 s/${C}-Beka/" -e "3 s/${C}/" -e "s/${D}/" ${E}/gtk-3.0/settings.ini 
-echo -e "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager ; echo -e "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm 
-echo -e "${A}INSTALLING GRUB ${B}"; grub-install --target=x 86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-sed -i 's/auto/1920x1080,auto/' /etc/default/grub ; grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager 
+echo -e "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm 
+echo -e "${A}INSTALLING GRUB ${B}"; grub-install \
+--target=x 86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+sed -i 's/auto/1920x1080,auto/' /etc/default/grub 
+grub-mkconfig -o /boot/grub/grub.cfg ; rm /etc/install.sh
+curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/installMo.sh > /home/${U}/installMO.sh
 cd / ; chown root:root /home ; chmod 755 /home ; runuser --login ${U} --session-command "sh ~/installMO.sh"
 # echo -e "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
