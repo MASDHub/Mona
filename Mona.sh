547be534ae99bf32c6d4b89d69470823cf094226
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-13 17:23:38 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -2,10 +2,10 @@
 set -euo pipefail 
 setfont ter-124b ; timedatectl set-ntp true 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='\e[1;31m' ; B='\e[0m' ; sed -i -e 's/#Co/Co/' /etc/pacman.conf #  Mozart - Moonlight Sonata         
-C='nvme0n1|sda|sdb|hda|hdb|hdc|hdd|mmcblk0' ; E='btrfs su cr'     #    0:35 ━❍──────── -5:32
-F='mount -o noatime,compress=zstd,discard=async,subvol=@'         #     ↻     ⊲  Ⅱ  ⊳     ↺
-G="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)"                     #     VOLUME: ▁▂▃▄▅▆▇ 100%
+A='\e[1;31m' ; B='\e[0m' ; sed -i -e 's/#Co/Co/' /etc/pacman.conf # Mozart - Moonlight Sonata
+C='nvme0n1|sda|sdb|hda|hdb|hdc|hdd|mmcblk0' ; E='btrfs su cr'     #  0:35 ━❍──────── -5:32
+F='mount -o noatime,compress=zstd,discard=async,subvol=@'         #   ↻     ⊲  Ⅱ  ⊳     ↺
+G="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)"                     #  VOLUME: ▁▂▃▄▅▆▇ 100%
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k | pacman-key --populate
 lsblk  | egrep --color "${C}|NAME"
@@ -17,7 +17,7 @@ D1="$(ls /dev/* | grep -E "^/dev/${D}p?1$")"
 D2="$(ls /dev/* | grep -E "^/dev/${D}p?2$")"
 mkfs.vfat ${D1} ; mkfs.btrfs -fq ${D2}
 mount ${D2} /mnt; cd /mnt ; ${E}home 
-${E} ; cd ; umount  /mnt ; ${F} ${D2} /mnt
+${E} ; cd ; umount /mnt ; ${F} ${D2} /mnt
 mkdir /mnt/{boot,home} ; ${F}home ${D2} /mnt/home
 mount ${D1} /mnt/boot ; lsblk -fs -e 7,11
 if [ "${G}" == Intel ] ; then G1='intel' && G2='i915 '
@@ -35,4 +35,4 @@ capitaine-cursors plank geeqie trayer arandr alacritty cmake pkg-config libpulse
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/etc/install.sh
 cp /etc/mkinitcpio.conf /mnt/etc/mkinitcpio.conf ; genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh /etc/install.sh 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
