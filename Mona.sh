5d290b86f8895e05ae9a5424dc2eb334b3d15f76
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-13 18:10:23 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail 
-setfont ter-124b ; timedatectl set-ntp true 
+setfont ter-124b ; timedatectl set-ntp true
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 A='\e[1;31m' ; B='\e[0m' ; sed -i -e 's/#Co/Co/' /etc/pacman.conf # Mozart - Moonlight Sonata
 C='nvme0n1|sda|sdb|hda|hdb|hdc|hdd|mmcblk0' ; E='btrfs su cr'     #  0:35 ━❍──────── -5:32
@@ -12,16 +12,16 @@ lsblk  | egrep --color "${C}|NAME"
 printf "\n${A}Enter Device name: ${B}"
 read D ; until [[ "${D}" == +(${C}) ]]
 do printf "${A}Try again: ${B}" && read D ; done
-sgdisk /dev/${D} -Z -o -n 1::+512M -t 1:EF00 -n 
+sgdisk /dev/${D} -Z -o -n 1::+512M -t 1:EF00 -n
 D1="$(ls /dev/* | grep -E "^/dev/${D}p?1$")"
 D2="$(ls /dev/* | grep -E "^/dev/${D}p?2$")"
 mkfs.vfat ${D1} ; mkfs.btrfs -fq ${D2}
-mount ${D2} /mnt; cd /mnt ; ${E}home 
+mount ${D2} /mnt; cd /mnt ; ${E}home
 ${E} ; cd ; umount /mnt ; ${F} ${D2} /mnt
 mkdir /mnt/{boot,home} ; ${F}home ${D2} /mnt/home
 mount ${D1} /mnt/boot ; lsblk -fs -e 7,11
 if [ "${G}" == Intel ] ; then H='intel' && I='i915 '
-fi ; if [ "${G}" == AMD ]; then H='amd' && I='amdgpu ' 
+fi ; if [ "${G}" == AMD ]; then H='amd' && I='amdgpu '
 fi ; if [[ ${#G} -gt 1 ]] ; then J=''${H}'-ucode xf86-video-'${H}''
 fi ; sed -i "s/ULES=()/ULES=(${I}btrfs)/" /etc/mkinitcpio.conf
 reflector -p https -f 2 --score 10 --save /etc/pacman.d/mirrorlist
@@ -33,7 +33,7 @@ libpulse gst-plugin-pipewire geany-plugins gvfs-mtp gvfs-afc arandr \
 lxappearance-obconf-gtk3 geeqie nemo-fileroller nemo-preview trayer \
 firefox firefox-ublock-origin libreoffice-still galculator htop git \
 capitaine-cursors otf-fira-sans otf-fira-mono python-pyxdg sddm vlc \
-xorg-xinit volumeicon cmake pkg-config ${J} ; cp /etc/mkinitcpio.conf /mnt/etc/mkinitcpio.conf 
+xorg-xinit volumeicon cmake pkg-config ${J} ; cp /etc/mkinitcpio.conf /mnt/etc/mkinitcpio.conf
 curl -sSL https://raw.githubusercontent.com/djSharcode/Mona/main/install.sh > /mnt/etc/install.sh
-genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt sh /etc/install.sh 
+genfstab -U /mnt >> /mnt/etc/fstab ; arch-chroot /mnt sh /etc/install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
