ebd8d5406d8d498078f0944e120d894c2f6cda55
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-14 09:00:47 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -17,13 +17,13 @@ echo -en "${A}Enter Device to Install: ${B}"
 read -r C ; until sgdisk /dev/${C} -Z \
 -n 1::+512M -t 1:EF00 -n -i -v -p ; do
 echo -en "${A}Try again: ${B}" && read C ; done
-D1="$(ls /dev/* | grep -E "^$/dev/${C}p?1$") "
-D2="$(ls /dev/* | grep -E "^$/dev/${C}p?2$") "
+D1="$( ls /dev/* | grep -E "^$/dev/${C}p?1$" )"
+D2="$( ls /dev/* | grep -E "^$/dev/${C}p?2$" ) "
 mkfs.vfat ${D1} ; mkfs.btrfs -fq ${D2}
 mount ${D2}/mnt ; cd /mnt ; ${E}home
 ${E} ; cd ; umount /mnt ; ${F}${D2}/mnt
 mkdir /mnt/{boot,home} ; ${F}home${D2}/mnt/home
-mount ${D1}/mnt/boot  ; lsblk | egrep --color 'disk|part'
+mount ${D1} /mnt/boot ; lsblk | egrep --color 'disk|part'
 if [[ ${G} == AMD ]] ; then H='amd-ucode' \
 && I='amdgpu ' ; fi ; if [[ ${G} == Intel ]]
 then H='intel-ucode' && I='i915 ' ; fi
