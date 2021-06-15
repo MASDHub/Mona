3118c1bd1f8eeb3914a5532d7af2b9c23f5b1fcf
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-15 17:27:36 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -5,13 +5,16 @@ A='.config/openbox' ; B='<item label="'
 C='"><action name="Execute"><command>'
 D='</command></action></item>' 
 E='<separator></separator>'
+F='.config/plank/dock1/launchers' 
+G='net/launchpad/plank/docks'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #if [[ -n "$(grep -E '[8|9|10|11|12|13|14]' /sys/class/dmi/id/chassis_type)" ]] ; then R='uacpid cbatticon'
 cp -dR /etc/xdg/openbox/ ~/.config/
 git clone https://aur.archlinux.org/pikaur.git  
 cd pikaur ; makepkg -fsri ;pikaur -S picom-git \
 oranchelo-icon-theme-git ttf-ms-fonts timeshift
-sed -i '5,$d' ~/${A}/menu.xml ; sed -i 's/g = no/g = yes/' ~/.config/pikaur.conf
+sed -i 's/g = no/g = yes/' ~/.config/pikaur.conf
+sed -i '5,$d' ~/${A}/menu.xml 
 { echo -e "<menu id="'"root-menu"'" label="'"Openbox 3"'">
 ${B}Files${C}nemo${D}\n${B}Search${C}rofi -show drun${D}
 ${B}Web${C}firefox${D}\n${B}Terminal${C}alacritty${D}
@@ -21,26 +24,34 @@ ${B}Lock Screen${C}xlock -mode atlantis +description \
 -echokeys -echokey '*' -info "'"Enter password to unlock"'"${D}
 ${B}Reboot${C}reboot${D}\n${B}Power Off${C}poweroff${D}
 </menu></openbox_menu>" ; } >> ~/${A}/menu.xml
-sed -i -e '50,120 s/8/12/' -e '50,120 s/9/13/' -e 's/A-space/0x85/' \
--e '131 s/4/2/' -e 's/sans/Fira Sans Condensed Book/g' -e 's/W-e/0x85/' \
+sed -i -e '50,120 s/9/13/' -e 's/W-e/0x85/' -e '131 s/4/2/' \
 -e 's/kfmclient openProfile filemanagement/rofi -show drun/' \
--e 's/Konqueror/Dmenu/' -e 's/Clearlooks/Bear2/' ~/${A}/rc.xml
-{ echo -e 'lxqt-policykit &\n\npicom --experimental-backends &\n\nplank &\n
-trayer --monitor primary --height 40 --align right --transparent true --tint 0x716966 &\n
-(nm-applet) &\n\n(volumeicon) &\n\n(sleep 2 && sh ~/mona.sh) &' ; } > ~/${A}/autostart
-{ echo 'dconf dump /net/launchpad/plank/docks/ > ~/docks.ini'
-echo 'sleep 2 && echo -e "'"[PlankDockItemPreferences]\nLauncher=file:///usr/share/applications/nemo.desktop"'" > ~/.config/plank/dock1/launchers/nemo.dockitem'
-echo 'sleep 2 && sed -i '"'s/bottom/right/'"' ~/docks.ini'
-echo 'sleep 2 && sed -i '"'s/vlc-1.dockitem/nemo.dockitem/'"' ~/docks.ini'
-echo 'sleep 2 && cat ~/docks.ini | dconf load /net/launchpad/plank/docks/'
-echo 'sleep 1 && rm ~/.config/plank/dock1/launchers/geeqie.dockitem'
-echo 'sleep 1 && rm ~/.config/plank/dock1/launchers/vlc-1.dockitem'
-echo 'sleep 1 && rm ~/.config/plank/dock1/launchers/vlc.dockitem'
+-e 's/sans/Fira Sans Condensed Book/g' -e 's/Konqueror/menu/' \
+-e '50,120 s/8/12/' -e 's/A-space/0x85/' \
+-e 's/Clearlooks/Bear2/' ~/${A}/rc.xml
+{ echo -e "lxqt-policykit &\n\npicom \
+--experimental-backends &\n\nplank &\n 
+trayer --monitor primary --height 40 \
+--align right --transparent true --tint \
+0x716966 &\n\n(nm-applet) &\n\n(volumeicon) &
+\n(sleep 2 && sh ~/mona.sh) &" ; } > ~/${A}/autostart
+{ echo 'dconf dump /'"${G}"'/ > ~/d.ini'
+echo -e 'sleep 2 && echo -e "'"[PlankDockItemPreferences]
+Launcher=file:///usr/share/applications/nemo.desktop"'" \
+> ~/'"${F}"'/nemo.dockitem'
+echo 'sleep 2 && sed -i '"'s/bottom/right/'"' ~/d.ini'
+echo 'sleep 2 && sed -i '"'s/vlc-1.dockitem/nemo.dockitem/'"' ~/d.ini'
+echo 'sleep 2 && cat ~/d.ini | dconf load /'"${G}"'/'
+echo -e 'sleep 1 && rm ~/'"${F}"'/geeqie.dockitem'
+echo -e 'sleep 1 && rm ~/'"${F}"'/vlc-1.dockitem'
+echo -e 'sleep 1 && rm ~/'"${F}"'/vlc.dockitem'
 echo 'sleep 1 && sed -i '"'13,"'$d'"'"' ~/.config/openbox/autostart'
-echo 'pkill volumeicon'
-echo 'sleep 2 && sed -i -e '"'s/b_slider=false/b_slider=true/'"' -e '"'s/l=false/l=true/'"' -e '"'s/5/1/'"' ~/.config/volumeicon/volumeicon'
-echo 'sleep 1 && volumeicon'
-echo 'sleep 10 && rm ~/docks.ini'; echo 'sleep 20 && rm -- "$0"' ; } > ~/mona.sh 
+echo 'pkill volumeicon' ; echo -e 'sleep 2 && \
+sed -i -e '"'s/b_slider=false/b_slider=true/'"' \
+-e '"'s/5/1/'"' -e '"'s/l=false/l=true/'"' \
+~/.config/volumeicon/volumeicon' ; echo 'sleep 1 && volumeicon'
+echo 'sleep 10 && rm ~/d.ini'; echo 'sleep 20 && rm -- "$0"' ; } > ~/mona.sh 
+
 echo -e "Done!\nTo Finish Type: 'reboot'" ; U="$( cat /etc/U )"
 su --login root -c "chmod 755 /home/${U}/mona.sh && rm /etc/U && rm -- "$0""
  
