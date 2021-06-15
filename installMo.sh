02bccdb7b0180474c6d28959354363818c0c13e1
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-15 07:53:23 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -26,15 +26,16 @@ sed -i -e '50,120 s/8/12/' -e '50,120 s/9/13/' -e 's/A-space/0x85/' \
 trayer --monitor primary --height 40 --align right --transparent true --tint 0x716966 &\n
 (nm-applet) &\n\n(volumeicon) &\n\n(sleep 5 && sh ~/mona.sh) &' ; } > ~/${A}/autostart
 { echo 'dconf dump /net/launchpad/plank/docks/ > ~/docks.ini'
+echo 'sleep 2 && echo -e "'"[PlankDockItemPreferences]\nLauncher=file:///usr/share/applications/nemo.desktop"'" > ~/.config/plank/dock1/launchers/nemo.dockitem'
 echo 'sleep 2 && sed -i '"'s/bottom/right/'"' ~/docks.ini'
+echo 'sleep 2 && sed -i '"'s/vlc-1.dockitem/nemo.dockitem/'"' ~/docks.ini'
 echo 'sleep 2 && cat ~/docks.ini | dconf load /net/launchpad/plank/docks/'
 echo 'sleep 2 && rm ~/.config/plank/dock1/launchers/geeqie.dockitem'
 echo 'sleep 2 && rm ~/.config/plank/dock1/launchers/vlc-1.dockitem'
 echo 'sleep 2 && rm ~/.config/plank/dock1/launchers/vlc.dockitem'
 echo 'sleep 2 && sed -i '"'13,"'$d'"'"' ~/.config/openbox/autostart'
-echo 'sleep 2 && echo -e "'"[PlankDockItemPreferences]\nLauncher=file:///usr/share/applications/nemo.desktop"'" > ~/.config/plank/dock1/launchers/nemo.dockitem'
-echo 'sleep 2 && sed -i "'"s/slider=false/slider=true/"'" ~/.config/volumeicon'
-echo 'sleep 2 && rm -- "$0"' ; } > ~/mona.sh 
+echo 'sleep 2 && sed -i "'"s/b_slider=false/b_slider=true/"'" ~/.config/volumeicon/volumeicon'
+echo 'sleep 18 && rm ~/docks.ini'; echo 'sleep 20 && rm -- "$0"' ; } > ~/mona.sh 
 echo -e "Done!\nType: 'exit' then 'reboot'"
 su --login root -c "chmod 755 /home/${U}/mona.sh && rm /etc/U && rm -- "$0""
  
