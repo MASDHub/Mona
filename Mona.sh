105a31eff4cdd527a3215e30fed054c2e1dc0613
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-17 02:04:20 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -6,37 +6,37 @@ head -n 8 -- "$0" | tail -n 5
  #         0:35 ━❍──────── -5:32           #
    #       ↻     ⊲  Ⅱ  ⊳     ↺               #
 #          VOLUME: ▁▂▃▄▅▆▇ 100%          # 
-A='\e[1;31m' ; B='\e[0m' ; E='btrfs su cr @'  #
+E='btrfs su cr @'                             #
 F='mount -o noatime,compress=zstd,subvol=@'   #
 G="$(lscpu | grep -Eo 'AMD|Intel' | sort -u)" #
-J='etc/mkinitcpio.conf' ; K='timedatectl set' #
-L='etc/pacman' ; sed -i 's/#Co/Co/' ${L}.conf #
+H='etc/mkinitcpio.conf' ; I='timedatectl set' #
+J='etc/pacman' ; sed -i 's/#Co/Co/' ${J}.conf #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k | pacman-key --populate
 lsblk  | egrep --color 'NAME|SIZE|disk'
-read -r -p "Enter Disk for Install: " C
-until sgdisk /dev/${C} -Z -o -n\
- 1::+512M -t 1:EF00 -n -i -v -p
-do read -r -p "Try Again: " C ; done
-D1="$(ls /dev/* | egrep "^/dev/${C}p?1$") "
-D2="$(ls /dev/* | egrep "^/dev/${C}p?2$") "
-mkfs.vfat ${D1} ; mkfs.btrfs -fq ${D2}
-mount ${D2}/mnt ; cd /mnt
+read -r -p "Enter Install Disk :  " \
+A ; until sgdisk /dev/${A} -Z -o -n \
+1::+512M -t 1:EF00 -n -i -v -p ; do \
+read -r -p "No, Try Again :" A ; done
+B="$(ls /dev/* | egrep "^/dev/${A}p?1$") "
+C="$(ls /dev/* | egrep "^/dev/${A}p?2$") "
+mkfs.vfat ${B} ; mkfs.btrfs -fq ${C}
+mount ${C}/mnt ; cd /mnt
 ${E}home ; ${E} ; cd ; umount /mnt
-${F} ${D2}/mnt  ; mkdir /mnt/{boot,home}
-mount ${D1}/mnt/boot; ${F}home ${D2}/mnt/home
-if [[ ${G} == AMD ]]; then I='amdgpu ' \
-&& H='amd-ucode   ' ; elif \
-[[ ${G} == Intel ]] ; then I='i915 ' \
-&& H='intel-ucode ' ; fi ; ${K}-timezone 
+${F} ${C}/mnt  ; mkdir /mnt/{boot,home}
+mount ${B}/mnt/boot ; ${F}home ${C}/mnt/home
+if [[ ${G} == AMD ]]; then G1='amdgpu ' \
+&& G2='amd-ucode  ' ; elif \
+[[ ${G} == Intel ]] ; then G1='i915 ' \
+&& G2='intel-ucode' ; fi ; ${I}-timezone 
 "$(curl -s https://ipapi.co/timezone)" &&\
- ${K}-ntp true ; reflector -p https -f 2 \
+ ${I}-ntp true ; reflector -p https -f 2 \
 -c "$(curl -s https://ipapi.co/country)" \
---sort rate --save /${L}.d/mirrorlist || \
+--sort rate --save /${J}.d/mirrorlist || \
 reflector -p https --score 5 --sort rate \
--a 12 -f 2 --save /${L}.d/mirrorlist 
+-a 12 -f 2 --save /${J}.d/mirrorlist 
 lsblk -o NAME,SIZE,MOUNTPOINT -e 7,11 
-sed -i "s/ULES=()/ULES=(${I}btrfs)/" /${J}
+sed -i "s/ULES=()/ULES=(${G1}btrfs)/" /${H}
 pacstrap -i /mnt base base-devel linux xorg \
 linux-headers linux-firmware efibootmgr vim \
 networkmanager rofi gufw htop obconf-qt git \
@@ -49,10 +49,10 @@ firefox firefox-ublock-origin geany-plugins \
 libreoffice-still otf-fira-mono trayer sddm \
 pkg-config otf-fira-sans xlockmore libpulse \
 volumeicon screengrab galculator xorg-xinit \
-arandr ${H} ; cp /${J} /mnt/${J} ; curl -sL \
+arandr ${G2}; cp /${H} /mnt/${H} ; curl -sL \
 https://raw.githubusercontent.com/djsharcode\
 /Mona/main/install.sh -o /mnt/etc/install.sh
-cp /${L}.conf /mnt/${L}.conf
+cp /${J}.conf /mnt/${J}.conf
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh /etc/install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
