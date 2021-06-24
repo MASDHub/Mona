9710928f47c641d372173a040328a8d41b5130aa
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-24 22:34:32 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -26,16 +26,16 @@ mount ${C} /mnt ; cd /mnt
 ${E}home ; ${E} ; cd ; umount /mnt
 ${F} ${C} /mnt  ; mkdir /mnt/{boot,home}
 ${F}home${C}/mnt/home; mount${B}/mnt/boot
-if [ ${G} == Intel ] ; then G1='i915 '\  
+if [ ${G} == Intel ] ; then G1='i915 '\
  && G2='intel-ucode' ; elif [ ${G} == AMD ]
 then G2='amd-ucode ' && G1='amdgpu '
-fi; ${I}timezone${T} && ${I}ntp true 
-lsblk -o NAME,SIZE,MOUNTPOINT -e 7,11 
+fi; ${I}timezone${T} && ${I}ntp true
+lsblk -o NAME,SIZE,MOUNTPOINT -e 7,11
 sed -i "s/ULES=()/ULES=(${G1}btrfs)/" ${H}
-reflector -p https --sort rate -l 4 -f 2 -c \ 
+reflector -p https --sort rate -l 4 -f 2 -c \
 "$(curl -s https://ipapi.co/country)" -a 12 \
 --save ${J}.d/mirrorlist || reflector -a 12 \
--p https --score 5 --save ${J}.d/mirrorlist    
+-p https --score 5 --save ${J}.d/mirrorlist
 pacstrap -i /mnt base base-devel linux xorg \
 linux-headers linux-firmware efibootmgr vim \
 networkmanager rofi gufw htop obconf-qt git \
@@ -52,7 +52,7 @@ arandr ${G2}; cp ${H} /mnt${H} ; curl -sL \
 https://raw.githubusercontent.com/djsharcode\
 /Mona/main/install.sh -o /mnt/etc/install.sh
 echo "${T}" > /mnt/etc/T
-cp ${J}.conf /mnt${J}.conf 
+cp ${J}.conf /mnt${J}.conf
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh /etc/install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
