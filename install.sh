6ae97d6eb32ab438856deeb0cc740fac05160cb3
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-25 18:17:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ head -n 16 -- "$0" | tail -n 13
 #d88%            %%%8--'-.        
 #/88:.__ ,       _%-' ---  -       
  #  '''::===..-'   =  --.  `                 
-Z='       ' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='\e[1;31m' ; B='\e[0m' ; E='usr/share'
+S='       ' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+A='\e[1;31m' ; B='\e[0m' ; C='/usr/share/'
 D='Cantarell 11/Fira Sans Condensed Book'
-C='Adwaita/Oranchelo' ; F='systemctl enable'
-S='             ' #~~~~~~~~~~~~~~~~~~~~~~~~~#
+E='Adwaita/Oranchelo' ; F='systemctl enable'
+Z='             ' #~~~~~~~~~~~~~~~~~~~~~~~~~#
 echo -en "\n${A}User Name: ${B}"
 read -r R; until [[ ${#U} -gt 4 ]] && \
 [[ "${U}" =~ ^[a-z]*$ ]] ; do read -r \
@@ -33,33 +33,33 @@ sed -i 's/#en_US./en_US./' /etc/locale.gen
 echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
 P="${U}pc1" ; echo ${P} >> /etc/hostname
 locale-gen; echo home/$U > /etc/U ; echo \
--e "127.0.0.1${Z}localhost\n::1${S}localhost
-127.0.1.1${Z}$P.localdomain $P" > /etc/hosts
-X="$(cat /etc/U)/.xinitrc" ; ln -sf \
-/usr/share/zoneinfo/"$(cat /etc/T)" \
+-e "127.0.0.1${S}localhost\n::1${Z}localhost
+127.0.1.1${S}$P.localdomain $P" > /etc/hosts 
+ln -sf /usr/share/zoneinfo/$(cat /etc/T) \
 /etc/localtime ; hwclock --systohc
-cp /etc/X11/xinit/xinitrc /${X}
-sed -i 's/twm/openbox-session/' \
-/${X}; sed -i -e "3 s/$C/" -e "s/$D/" \
-"2 s/$C-Beka/" /$E/gtk-3.0/settings.ini
-sed -i -e "2 s/$C/" -e "1 s/$C-Beka/" \
--e "s/$D/" /$E/gtk-2.0/gtkrc ; echo '
+X="/$(cat /etc/U)/.xinitrc"
+cp /etc/X11/xinit/xinitrc $X
+sed -i 's/twm/openbox-session/' $X
+sed -i -e "2 s/$E-Beka/" -e "s/$D/" \
+"3 s/$E/" ${C}gtk-3.0/settings.ini
+sed -i -e "1 s/$E-Beka/" -e "s/$D/" \
+-e "2 s/$E/" ${C}gtk-2.0/gtkrc ; echo '
 M="$(find /home/*/.screenlayout/*.sh)"
-M1="$(xrandr|egrep -o '"'eDP1|eDP-1'"')"
-M2="$(xrandr|egrep -c '"'HDMI|eDP|VGA'"')"
-M3="$(xrandr|egrep -o '"'HDMI-1|HDMI1|VGA1|VGA-1'"')"
-if [ -r "${M}" ] && $(grep -q xrandr ${M}) 
-then ${M} ; else if [[ "${M2}" -ge "2" ]] 
-then xrandr --output ${M1} --off --output ${M3} --auto
-fi ; fi' >> /${E}/sddm/scripts/Xsetup ; echo -e \
-"${A}DISPLAY MANAGER${B}" ; ${F} sddm ; echo -e \
-"${A}NETWORK${B}"; ${F} NetworkManager; echo -e \
-"${A}GRUB${B}"; grub-install --target=x86_64-efi\
- --efi-directory=/boot --bootloader-id=GRUB
-sed -i 's/auto/1920x1080,auto/' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg ; curl -sSL \
-https://raw.githubusercontent.com/djSharcode/Mona\
-/main/installMo.sh > /"$(cat /etc/U)"/installMO.sh 
+N="$(xrandr|egrep -o '"'eDP1|eDP-1'"')"
+O="$(xrandr|egrep -c '"'HDMI|eDP|VGA'"')"
+P="$(xrandr|egrep -o '"'HDMI-1|HDMI1|VGA1|VGA-1'"')"
+if [ -r "${M}" ] && $(grep -q xrandr ${M})
+then ${M} ; else if [[ "${O}" -ge "2" ]]
+then xrandr --output $N --off --output $P --auto
+fi ; fi' >> ${C}sddm/scripts/Xsetup; echo \
+-e "${A}DISPLAY MGR${B}"; ${F} sddm; echo \
+-e "${A}NETWORKS${B}"; ${F} NetworkManager
+echo -e "\n${A}GRUB${B}\n" ; grub-install \
+--target=x86_64-efi --efi-directory=/boot \
+--bootloader-id=GRUB; sed -i 's/auto/1920x1080,auto/' \
+/etc/default/grub; grub-mkconfig -o /boot/grub/grub.cfg
+curl -sSL https://raw.githubusercontent.com/djSharcode\
+/Mona/main/installMo.sh > /"$(cat /etc/U)"/installMO.sh 
 rm /etc/T ; cd /; chown root:root /home; chmod 755 /home
 runuser --login $U --session-command "sh ~/installMO.sh"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
