14ddb0d1662f106cc224417bffd7f0c6226120d8
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-25 22:08:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,8 +21,8 @@ E='Adwaita/Oranchelo' ; F='systemctl enable'
 Z='             ' #~~~~~~~~~~~~~~~~~~~~~~~~~#
 printf "${A}User Name: ${B}"; read -r R \
 && U="${R,,}" ; until [[ ${#U} -gt 4 ]] \
-&& [[ "${U}" =~ ^[a-z]*$ ]]; do read -r \
--p 'Try Again: ' R && U="${R,,}" 
+&& [[ "${U}" =~ ^[a-z]*$ ]]; do printf "\
+${A}Retry:${B}" && read -r R && U="${R,,}" 
 done ; useradd -m -G wheel ${U} ; echo -\
 e "${A}Enter User Password${B}" ; until \
 passwd  "${U}" ; do echo ; done ; echo -\
