423c17c579122028379f392728b81f2926c08cb2
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-25 08:54:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,9 +15,10 @@ head -n 16 -- "$0" | tail -n 13
 #/88:.__ ,       _%-' ---  -       
  #  '''::===..-'   =  --.  `                 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='\e[1;31m' ; B='\e[0m' ; E='/usr/share/gtk'
+A='\e[1;31m' ; B='\e[0m' ; E='/usr/share/gtk-'
 D='Cantarell 11/Fira Sans Condensed Book'
 C='Adwaita/Oranchelo' ; F='systemctl enable'
+${X}='xinit'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 echo -en "\n${A}User Name: ${B}"
 read R; until [[ ${#U} -gt 4 ]] \
@@ -27,40 +28,46 @@ done ; useradd -m -G wheel "${U}"
 echo -e "${A}User Password: ${B}"
 until passwd ${U}; do echo; done 
 echo -e "${A}Root${B} Password:"
-until passwd ; do echo ; done
+until passwd; do echo; done
 sed -i '0,/# %/ s/# %/ %/' /etc/sudoers
 sed -i 's/#en_US./en_US./' /etc/locale.gen
 echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
 locale-gen ; echo "${P}" > /etc/hostname
-rm -- "$0" ; echo "${U}" > /etc/U
-P="${U}pc" ; echo -e "
+P="${U}pc" ; echo "${U}" > /etc/U ; echo "
 127.0.0.1       localhost
 ::1             localhost
-127.0.1.1       ${P}.localdomain ${P}" >> \
-/etc/hosts; sed -i -e "1 s/${C}-Beka/" -e \
- "2 s/${C}/" -e "s/${D}/" ${E}-2.0/gtkrc
-sed -i -e "3 s/${C}/" -e "2 s/${C}-Beka/" \
--e "s/${D}/" ${E}-3.0/settings.ini
-ln -sf /usr/share/zoneinfo/"$(cat /etc/T)" \
-/etc/localtime ; hwclock --systohc ; echo -e '
-M="$(find /home/*/.screenlayout/*.sh)"
-M1="$(xrandr | grep -Eo '"'eDP1|eDP-1'"')"
-M2="$(xrandr | grep -Ec '"'HDMI-1|HDMI1|eDP1|eDP-1|VGA1|VGA-1|DVI1|DVI-1'"')"
-M3="$(xrandr | grep -Eo '"'HDMI-1|HDMI1|DVI-1|DVI1|VGA1|VGA-1'"')"
+127.0.1.1       ${P}.localdomain $P"\
+ > /etc/hosts ; rm -- "$0" ; ln -sf \
+/usr/share/zoneinfo/"$(cat /etc/T)" \
+/etc/localtime ; hwclock --systohc 
+cp /etc/X11/${X}/${X}c /home/${U}/.${X}c  
+sed -i 's/twm/openbox-session/'\
+ /home/${U}/.${X}c ; sed -i -e \
+"1 s/${C}-Beka/" -e "s/$D/" -e \
+"2 s/$C/" -e ${E}2.0/gtkrc 
+sed -i -e "2 s/${C}-Beka/" \
+-e "s/$D/" -e "3 s/$C/" -e \
+${E}3.0/settings.ini; echo \
+'M="$(find /home/*/.screenlayout/*.sh)"
+M1="$(xrandr|egrep -o '"'eDP1|eDP-1'"')"
+M2="$(xrandr|egrep -c '"'HDMI|eDP|VGA'"')"
+M3="$(xrandr|egrep -o '"'HDMI-1|HDMI1|VGA1|VGA-1'"')"
 if [ -r "${M}" ] && $(grep -q xrandr ${M}) 
 then ${M} ; else if [[ "${M2}" -ge "2" ]]
-then xrandr --output "${M1}" --mode 1920x1080 --output "${M3}" --primary --auto
-fi\nfi' >> ${E}sddm/scripts/Xsetup
-echo -e "${A}DISPLAY MANAGER ENABLED${B}" ; ${F} sddm 
-echo -e "${A}NETWORK ENABLED${B}" ; ${F} NetworkManager 
+then xrandr --output ${M1} --off --output ${M3} --auto
+fi ; fi' >> ${E}sddm/scripts/Xsetup
+echo -e "${A}DISPLAY ENABLED${B}" ; ${F} sddm 
+echo -e "${A}NETWORK CONNECTED${B}" ; ${F} NetworkManager 
 echo -e "${A}INSTALLING GRUB${B}" ; grub-install \
 --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 sed -i 's/auto/1920x1080,1024x768x32,auto/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-cp /etc/X11/xinit/xinitrc /home/${U}/.xinitrc
-sed -i 's/twm/openbox-session/' /home/${U}/.xinitrc
+
 curl -sSL https://raw.githubusercontent.com/djSharcode/\
 Mona/main/installMo.sh > /home/${U}/installMO.sh
+
+rm /etc/T ;
+
 cd / ; chown root:root /home ; chmod 755 /home
 runuser --login ${U} --session-command "sh ~/installMO.sh"
 # echo -e "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth
