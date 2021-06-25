6d383b30f9ca7b9bb2226a3829b220a1a2e712de
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-25 10:41:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,9 +15,9 @@ head -n 16 -- "$0" | tail -n 13
 #/88:.__ ,       _%-' ---  -       
  #  '''::===..-'   =  --.  `                 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='\e[1;31m' ; B='\e[0m' ; E='/usr/share/gtk-'
+A='\e[1;31m' ; B='\e[0m' ; E='usr/share'
 D='Cantarell 11/Fira Sans Condensed Book'
-C='Adwaita/Oranchelo' ; F='systemctl enable '
+C='Adwaita/Oranchelo' ; F='systemctl enable'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 echo -en "\n${A}User Name: ${B}"
 read R; until [[ ${#U} -gt 4 ]] \
@@ -35,17 +35,17 @@ P="${U}pc1" ; echo ${P} >> /etc/hostname
 locale-gen; echo home/$U > /etc/U ; echo "
 127.0.0.1       localhost
 ::1             localhost
-127.0.1.1       $P.localdomain $P"\
- > /etc/hosts; rm -- "$0"; ln -sf \
-/usr/share/zoneinfo/$(cat /etc/T) \
+127.0.1.1       ${P}.localdomain $P"\
+ > /etc/hosts ; rm -- "$0" ; ln -sf \
+/usr/share/zoneinfo/"$(cat /etc/T)" \
 /etc/localtime ; hwclock --systohc
 X="/$(cat /etc/U)/" ; cp \
 /etc/X11/xinit/xinitrc ${X}.xinitrc
 sed -i 's/twm/openbox-session/' ${X}.xinitrc
 sed -i -e "3 s/$C/" -e "s/$D/" -e \
-"2 s/$C-Beka/" $E3.0/settings.ini
+"2 s/$C-Beka/" /$E/gtk-3.0/settings.ini
 sed -i -e "2 s/$C/" -e "s/$D/" -e \
-"1 s/$C-Beka/" $E2.0/gtkrc ; echo \
+"1 s/$C-Beka/" /$E/gtk-2.0/gtkrc; echo \
 'M="$(find /home/*/.screenlayout/*.sh)"
 M1="$(xrandr|egrep -o '"'eDP1|eDP-1'"')"
 M2="$(xrandr|egrep -c '"'HDMI|eDP|VGA'"')"
@@ -53,17 +53,15 @@ M3="$(xrandr|egrep -o '"'HDMI-1|HDMI1|VGA1|VGA-1'"')"
 if [ -r "${M}" ] && $(grep -q xrandr ${M}) 
 then ${M} ; else if [[ "${M2}" -ge "2" ]] 
 then xrandr --output ${M1} --off --output ${M3} --auto
-fi ; fi' > ${E}sddm/scripts/Xsetup
-echo -e "${A}DISPLAY ENABLED${B}"; ${F}sddm
-echo -e "${A}NETWORK CONNECT${B}"; ${F}NetworkManager
-echo -e "${A}INSTALLING GRUB${B}"; grub-install \
---target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-sed -i 's/auto/1920x1080,auto/' /etc/default/grub
+fi ; fi' >> /${E}/sddm/scripts/Xsetup ; echo -e \
+"${A}DISPLAY MANAGER${B}" ; ${F} sddm ; echo -e \
+"${A}NETWORK${B}"; ${F} NetworkManager; echo -e \
+"${A}GRUB${B}"; grub-install --target=x86_64-efi\
+ --efi-directory=/boot --bootloader-id=GRUB
+sed -i 's/auto/1920x1080,auto/' /etc/default/grub 
 grub-mkconfig -o /boot/grub/grub.cfg ; curl -sSL \
 https://raw.githubusercontent.com/djSharcode/Mona\
 /main/installMo.sh > $X/installMO.sh; rm /etc/T
 cd /; chown root:root /home; chmod 755 /home
 runuser --login $U --session-command "sh ~/installMO.sh"
-# echo -e "${A}BLUETHOOTH ENABLED${B}" ; ${F} bluetooth
-#if [[ "$( pacman -Qd | grep -Ec tpl )" == [1-9] ]] ; systemctl enable tlp && systemctl enable acpid ; fi
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
