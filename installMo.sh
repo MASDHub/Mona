9c567243c92fed7cdeeb66d5fa69efca0fb562f5
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-28 06:32:14 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -9,10 +9,9 @@ E='<separator></separator>' ; Q='docks.ini'
 F='.config/plank/dock1/launchers'
 G='net/launchpad/plank/docks'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-mkdir ~/.config 
-cp -a etc/xdg/openbox/ ~/.config/
-git clone \
+mkdir ~/.config ; git clone \
 https://aur.archlinux.org/pikaur.git
+cp -a /etc/xdg/openbox/ ~/.config/ 
 cd pikaur; makepkg -sri; pikaur -S \
 picom-git oranchelo-icon-theme-git \
 jbxkb ttf-ms-fonts timeshift; sed -\
@@ -20,11 +19,12 @@ i 's/g = no/g = yes/' ~/${Z}; sed -\
 i 's/g = no/g = yes/' ~/${Z}; sed -\
 i '5,$d' ~/${A}/menu.xml ; sed -i -\
 e '5,120 s/8/12/' -e '131 s/4/2/' -\
+e 's/kfmclient openProfile/rofi/' -\
+e 's/filemanagement/-show drun/g' -\
 e 's/sans/Fira Sans Condensed Book/' -\
 e 's/W-e/0x85/' -e 's/A-space/0x85/' -\
 e 's/Konqueror/m/' -e '5,99 s/9/13/' -\
-e 's/Clearlooks/Bear2/' -e \
-'s/kfmclient openProfile filemanagement/rofi -show drun/' ~/${A}/rc.xml
+e 's/Clearlooks/Bear2/' "~/${A}/rc.xml"
 echo -e "<menu id="'"root-menu"'" \
 label="'"Openbox 3"'"> \n${B}Files\
 ${C}nemo${D}\n${B}Search${C}rofi -\
@@ -45,25 +45,26 @@ echo -e "lxqt-policykit &\n\npicom \
 right --tint 0x716966 --height 40 -\
 -transparent true &\n\nnm-applet &\n
 volumeicon &\n(sleep 5 && sh ~/m.sh)\
- &" > ~/$A/autostart
-echo -e 'dconf dump /'"$G"'/ > ~/'"$Q"'
-'"$S"' sed -i '"'s/bottom/right/'"' ~/'"$Q"'
-'"$S"' cat ~/'"$Q"' | dconf load /'"$G"'/
-'"$S"' echo -e "'"[PlankDockItemPreferences]
-Launcher=file:///usr/share/applications/nemo\
-.desktop"'"\ > ~/'"$F"'/nemo.dockitem
+ &" >> "~/${A}/autostart" ; echo -e \
+'dconf dump /'"${G}"'/ > ~/'"${Q}"' \ 
+'"$S"' sed -i '"'s/bottom/right/'"' \
+~/'"${Q}"'\n'"${S}"' cat ~/'"${Q}"' \
+| dconf load /'"${G}"'/   \n'"${S}"'\
+echo -e "'"[PlankDockItemPreferences]
+Launcher=file:///usr/share/applications\
+/nemo.desktop"'" > ~/'"$F"'/nemo.dockitem
 '"$S"' rm ~/'"$F"'/geeqie.dockitem
 '"$S"' rm ~/'"$F"'/vlc-1.dockitem
 '"$S"' rm ~/'"$F"'/vlc.dockitem
-'"$S"' sed -i '"'12,"'$d'"'"'\
- ~/.config/openbox/autostart
-'"$S"' pkill volumeicon 
-'"$S"' sed -i -e '"'s/5/1/'"' \
--e '"'s/l=false/l=true/'"' -e \
-'"'s/b_slider=false/b_slider=true/'"'\
-  ~/.config/volumeicon/volumeicon
-'"$S"' volumeicon ; sleep 10 && \
-rm ~/'"$Q"' && rm ~/m.sh' > ~/m.sh 
-su root -c "chmod 755 /$(cat /etc/U)/m.sh && rm /etc/U && rm -- $0"
-echo -e '\e[1;31mDone!\nTo Finish Type: "'"reboot"'"\e[0m'
+'"$S"' sed -i '"'12,"'$d'"'"' 
+~/.config/openbox/autostart\n'"$S"' \
+pkill volumeicon\n'"$S"'\nsed -i -e \
+'"'s/5/1/'"' -e '"'s/l=false/l=true/'"' \
+-e '"'s/b_slider=false/b_slider=true/'"'\
+~/.config/volumeicon/volumeicon\n'"$S"' \
+volumeicon ; sleep 10 && rm ~/'"$Q"' && \
+rm ~/m.sh' > ~/m.sh
+su root -c "chmod 755 /$(cat /etc/U)/m.sh \
+&& rm /etc/U && rm -- $0" ; echo -e '\e\
+[1;31mDone!\nTo Finish Type: "'"reboot"'"\e[0m'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
