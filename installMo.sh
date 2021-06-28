179adfdee09069d2679ae17d55ca0a11bc732e82
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-06-28 05:49:50 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -43,9 +43,9 @@ echo -e "lxqt-policykit &\n\npicom \
 --experimental-backends &\n\nplank &
 \ntrayer --monitor primary --align \
 right --tint 0x716966 --height 40 -\
--transparent true &\n\n(nm-applet) &
-\n(volumeicon) &\n\n(sleep 5 && sh \
-~/mona.sh) &" > ~/$A/autostart
+-transparent true &\n\nnm-applet &\n
+volumeicon &\n(sleep 5 && sh ~/m.sh)\
+ &" > ~/$A/autostart
 echo -e 'dconf dump /'"$G"'/ > ~/'"$Q"'
 '"$S"' sed -i '"'s/bottom/right/'"' ~/'"$Q"'
 '"$S"' cat ~/'"$Q"' | dconf load /'"$G"'/
@@ -55,7 +55,7 @@ Launcher=file:///usr/share/applications/nemo\
 '"$S"' rm ~/'"$F"'/geeqie.dockitem
 '"$S"' rm ~/'"$F"'/vlc-1.dockitem
 '"$S"' rm ~/'"$F"'/vlc.dockitem
-'"$S"' sed -i '"'13,"'$d'"'"'\
+'"$S"' sed -i '"'12,"'$d'"'"'\
  ~/.config/openbox/autostart
 '"$S"' pkill volumeicon 
 '"$S"' sed -i -e '"'s/5/1/'"' \
@@ -63,7 +63,7 @@ Launcher=file:///usr/share/applications/nemo\
 '"'s/b_slider=false/b_slider=true/'"'\
   ~/.config/volumeicon/volumeicon
 '"$S"' volumeicon ; sleep 10 && \
-rm ~/'"$Q"' && rm ~/mona.sh' > ~/mona.sh 
-su root -c "chmod 755 /$(cat /etc/U)/mona.sh && rm /etc/U && rm -- $0"
+rm ~/'"$Q"' && rm ~/m.sh' > ~/m.sh 
+su root -c "chmod 755 /$(cat /etc/U)/m.sh && rm /etc/U && rm -- $0"
 echo -e '\e[1;31mDone!\nTo Finish Type: "'"reboot"'"\e[0m'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
