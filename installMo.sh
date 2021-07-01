50cd96d52cc27484fd1bcbde1e47374b201f69dd
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-02 00:30:23 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -euo pipefail
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='.config/openbox' ; H='sleep 2 && '
+A='.config/openbox'; H='sleep 2 && '
 B='<item label="' ; I=' ~/docks.ini'
 C='"><action name="Execute"><command>'
 D='</command></action></item>'
