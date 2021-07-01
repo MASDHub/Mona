07695bfe155bf96443132f82c810a7b5834f9906
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-01 23:39:33 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -7,10 +7,10 @@ setfont ter-124b; head -n 8 -- $0 | tail -n 5
    #       ↻     ⊲  Ⅱ  ⊳     ↺            #
 #          VOLUME: ▁▂▃▄▅▆▇ 100%         #
 E='mount -o noatime,compress=zstd,subvol=@'
-F='btrfs su cr @' ; G='timedatectl set-'
-H="$(lscpu | egrep -o 'AMD|Intel' | sort -u)"
+F='btrfs su cr @' ; G='timedatectl set'
+H="$(lscpu| egrep -o 'AMD|Intel'| sort -u)"
 I='/etc/mkinitcpio.conf' ; J='/etc/pacman.'
-T="$(curl -s https://ipapi.co/timezone)"
+T="$(curl -s https://ipapi.co/timezone) "
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k | pacman-key --populate ; head -n 5 -\
 - "$0" | tail -n 1 ; lsblk -d -o NAME,SIZE -\
@@ -29,7 +29,7 @@ ${E}home ${C} /mnt/home; lsblk -n -e 7,11
 if [ "${H}" == Intel ] ; then H1='i915 ' && \
 H2='intel-ucode ' ; fi ; if [ "${H}" == AMD ]
 then H1='amdgpu ' && H2='amd-ucode'; fi
-${G}timezone ${T} && ${G}ntp true  ; sed -i \
+${G}-timezone ${T}&& ${G}-ntp true ; sed -i \
 "s/ULES=()/ULES=(${H1}btrfs)/" ${I}; sed -i \
 's/#Co/Co/' ${J}conf; reflector -p https -c \
 "$(curl -s https://ipapi.co/country)" -a 12 \
