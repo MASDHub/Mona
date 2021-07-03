e8ec889b2132df37078ac3c1a8307a232280c6c8
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-03 23:47:19 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -euo pipefail
-setfont ter-124b; head -n 8 -- $0|tail -n 4
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-    #     Mozart - Moonlight Sonata    #
- #         0:35 ━❍──────── -5:32       #
-   #         ↻     ⊲  Ⅱ  ⊳     ↺       #
-#         VOLUME: ▁▂▃▄▅▆▇ 100%       #
-E='mount -o noatime,compress=zstd,subvol=@'
+setfont ter-124b;head -n 8 -- $0|tail -n 4
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    #     Mozart - Moonlight Sonata   #
+ #         0:35 ━❍──────── -5:32      #
+   #         ↻     ⊲  Ⅱ  ⊳     ↺      #
+#         VOLUME: ▁▂▃▄▅▆▇ 100%      #
+E='mount -o noatime,compress=zstd,subvol='
 F='btrfs su cr @' ; G='timedatectl set-'
-H="$( lscpu|egrep -o 'AMD|Intel'|sort -u )"
-I='/etc/mkinitcpio.conf' ; J='/etc/pacman.'
-T=" $(curl -sSL https://ipapi.co/timezone)"
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+H="$( lscpu|egrep -o 'AMD|Intel'|sort -u)"
+I='/etc/mkinitcpio.conf'; J='/etc/pacman.'
+T="$(curl -sSL https://ipapi.co/timezone)"
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k|pacman-key --populate;printf '%50s
 '|tr ' ' -; lsblk -do NAME,SIZE -e 7,11 |
 egrep --color '|NAME'; read -p $'\e[1;31m
@@ -22,14 +22,14 @@ do lsblk -e 7,11 -o NAME,SIZE&&read -p $'
 B="$( ls /dev/*|egrep "^/dev/${A}p?1$" )"
 C="$( ls /dev/*|egrep "^/dev/${A}p?2$" )"
 mkfs.vfat ${B} ;mkfs.btrfs -fq ${C}
-mount ${C} /mnt;cd /mnt; ${F} ;${F}home
-cd; umount/mnt ;${E} ${C} /mnt;mkdir /\
-mnt/{boot,home};${E}home ${C} /mnt/home
+mount ${C} /mnt;cd /mnt ; ${F} ;${F}home
+cd; umount/mnt ;${E}@ ${C} /mnt;mkdir /\
+mnt/{boot,home};${E}@home ${C} /mnt/home
 mount ${B} /mnt/boot;lsblk -ne 7,11
 if [ "${H}" == AMD ];then H1='amdgpu '&&
 H2='amd-ucode'; elif [ "${H}" == Intel ]
 then H1='i915 ' && H2='intel-ucode' ; fi
-${G}timezone${T}&& ${G}ntp true
+${G}timezone $T && ${G}ntp true
 sed -i "s/ULES=()/ULES=(${H1}btrfs)/" ${I}
 sed -i 's/#Co/Co/' ${J}conf;reflector -c \
 "$(curl -s https://ipapi.co/country)" -p \
@@ -50,11 +50,11 @@ firefox volumeicon screengrab galculator \
 nemo-fileroller git pkg-config xlockmore \
 otf-fira-sans galculator libpulse arandr \
 gvfs-afc xorg-xinit geeqie conky-manager \
-clipgrab ${H2};cp ${I} /mnt${I};curl -sL \
-https://raw.githubusercontent.com/djsharc\
-ode/Mona/main/install.sh -o /mnt/etc/inst\
-all.sh; cp ${J}conf /mnt${J}conf
+clipgrab ${H2}; cp ${J}conf /mnt${J}conf \
+curl -L https://raw.githubusercontent.com\
+/djsharcode/Mona/main/install.sh -o /mnt/\
+etc/install.sh;cp ${I} /mnt${I}
 echo "zoneinfo/$T" >/mnt/etc/T
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh /etc/install.sh
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
