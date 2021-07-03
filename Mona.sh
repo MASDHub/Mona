afd68768ef4cdceba0f0b81d2f3f2c8bc848a6dc
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-04 02:40:41 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
-set -euo pipefail
-setfont ter-124b;head -n 8 -- $0|tail -n 4
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-    #     Mozart - Moonlight Sonata   #
- #         0:35 ━❍──────── -5:32      #
-   #         ↻     ⊲  Ⅱ  ⊳     ↺      #
-#         VOLUME: ▁▂▃▄▅▆▇ 100%      #
-E='mount -o noatime,compress=zstd,subvol='
+set -euo pipefail;setfont ter-124b
+head -n 8 -- $0|tail -n 4
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    #     Mozart - Moonlight Sonata  #
+ #         0:35 ━❍──────── -5:32    #
+   #         ↻     ⊲  Ⅱ  ⊳     ↺     #
+#         VOLUME: ▁▂▃▄▅▆▇ 100%     #
+E='mount -o noatime,compress=zstd,subvol'
 F='btrfs su cr @' ; G='timedatectl set-'
 H="$(lscpu|egrep -o 'AMD|Intel'|sort -u)"
-I='/etc/mkinitcpio.conf'; J='/etc/pacman.'
-T="$(curl -sSL https://ipapi.co/timezone)"
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-gpg -k|pacman-key --populate;printf '%50s
-'|tr ' ' -; lsblk -do NAME,SIZE -e 7,11 |
-egrep --color '|NAME'; read -p $'\e[1;31m
-Enter Disk Name\e[0m: ' A; until sgdisk \
-/dev/$A -Z -n 1::+512M -t 1:EF00 -n -p
-do lsblk -e 7,11 -o NAME,SIZE&&read -p $'
-\e[1;31mRe-try\e[0m: ' A ; done
-B="$(ls /dev/* | egrep "^/dev/${A}p?1$")"
-C="$(ls /dev/* | egrep "^/dev/${A}p?2$")"
+I='/etc/mkinitcpio.conf';J='/etc/pacman.'
+T="$(curl -sL https://ipapi.co/timezone)"
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+gpg -k|pacman-key --populate; printf \
+'%50s'|tr ' ' -;lsblk -do NAME,SIZE -\
+e 7,11|egrep --color '|NAME';read -p \
+$'\e[1;31mEnter Disk Name\e[0m:  ' A
+until Z="/dev/$A"&&sgdisk ${Z} -Z -n \
+1::+512M -t 1:EF00 -n -p;do lsblk -e \
+7,11 -o NAME,SIZE&&read -p $'\e[1;31m
+Retry\e[0m: ' A; done
+B="$(ls /dev/* | egrep "^${Z}p?1$") "
+C="$(ls /dev/* | egrep "^${Z}p?2$") "
 mkfs.vfat ${B} ; mkfs.btrfs -fq ${C}
-mount ${C} /mnt; cd /mnt
-${F} ; ${F}home; cd ; umount/mnt 
-${E}@ ${C} /mnt; mkdir /mnt/{boot,home}
-mount ${B} /mnt/boot; ${E}@home \
+mount ${C}/mnt ; cd /mnt
+${F}; ${F}home ; cd ; umount/mnt 
+${E}=@ ${C}/mnt; mkdir /mnt/{boot,home}
+mount ${B}/mnt/boot; ${E}=@home \
 ${C}/mnt/home; if [ "${H}" == Intel ]
 then H2='intel-ucode'&&H1='i915 '; fi
 lsblk -ne 7,11 ; if [ "${H}" == AMD ]
@@ -58,4 +59,4 @@ etc/install.sh;cp ${I} /mnt${I}
 echo "zoneinfo/$T" >/mnt/etc/T
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh /etc/install.sh
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
