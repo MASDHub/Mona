e7fcad797b0df4d9072eeccfb71334c0df45ab3e
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-04 23:18:24 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-set -euo pipefail;setfont ter-124b
-head -n 8 -- $0|tail -n 4
+set -euo pipefail
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #     Mozart - Moonlight Sonata    #
 #      0:35 ━❍──────── -5:32     #
 #       ↻     ⊲  Ⅱ  ⊳     ↺       #
 #       VOLUME: ▁▂▃▄▅▆▇ 100%    #
-E='noatime,compress=zstd,subvol=@'
-F='btrfs su cr @';G='/etc/pacman.'
-H="$(lscpu|egrep 'AMD|Intel')"
-I='curl -s https://ipapi.co/'
-J='/etc/mkinitcpio.conf'
-K='timedatectl set-'
+E='o noatime,compress=zstd,subvol=@'
+F='btrfs su cr @' ; G='/etc/pacman.'
+I='curl -s https:/';setfont ter-124b
+H="$( lscpu | grep -E 'AMD|Intel' )"
+K='timedatectl set';head -n 8 -- $0|
+tail -n 4 ; J='/etc/mkinitcpio.conf'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k|pacman-key --populate; printf '
 %50s' |tr ' ' -;lsblk -do NAME,SIZE -\
@@ -24,15 +23,15 @@ Retry\e[0m: ' A; done
 B="$(ls /dev/*|egrep "^${Z}p?1$") "
 C="$(ls /dev/*|egrep "^${Z}p?2$") "
 mkfs.vfat ${B};mkfs.btrfs -fq${C}
-mount ${C}/mnt;cd /mnt ;${F};${F}home
-cd ;umount/mnt;mount -o ${E};${C}/mnt
+mount ${C}/mnt;cd /mnt;${F};${F}home
+cd ;umount/mnt; mount -${E};${C}/mnt
 mkdir /mnt/{boot,home};mount ${B}/mnt\
-/boot; mount -o ${E}home ${C}/mnt/home
+/boot; mount -${E}home ${C}/mnt/home
 if [[ -n ${H} ]];then if [ $H == AMD ]
 then S='amd-ucode ' && Q='amdgpu '
-else S='intel-ucode'&& Q='i915 ';fi
-fi;${K}timezone "$(${I}timezone)"&&
-${K}ntp true;lsblk -ne 7,11; sed \
+else S='intel-ucode'&& Q='i915 ' ;fi
+fi;${K}-timezone "$(${I}timezone)"&&
+${K}-ntp true; lsblk -ne 7,11; sed \
 -i 's/#Co/Co/' ${G}conf ; sed -i "
 s/ULES=()/ULES=(${Q}btrfs)/" $J
 reflector -p https -c $(${I}country) \
