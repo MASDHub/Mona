1ebfeedb48f9f92fdb35df0dc60f14921de77b63
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-04 03:50:15 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -euo pipefail;setfont ter-124b
 head -n 8 -- $0|tail -n 4
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
     #     Mozart - Moonlight Sonata  #
  #         0:35 ━❍──────── -5:32    #
    #         ↻     ⊲  Ⅱ  ⊳     ↺     #
 #         VOLUME: ▁▂▃▄▅▆▇ 100%     #
-E='mount -o noatime,compress=zstd,subvol'
-F='btrfs su cr @' ; G='timedatectl set-'
-H="$(lscpu|egrep -o 'AMD|Intel'|sort -u)"
-I='/etc/mkinitcpio.conf';J='/etc/pacman.'
+E='mount -o noatime,compress=zstd,subvo'
+F='btrfs su cr @';G='timedatectl set-'
+I='etc/mkinitcpio.conf';J='/etc/pacman.'
+H="$(lscpu|egrep -o 'AMD|Intel')"
 T="$(curl -s https://ipapi.co/timezone)"
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k|pacman-key --populate; printf '
 %50s' |tr ' ' -;lsblk -do NAME,SIZE -\
 e 7,11|egrep --color '|NAME';read -p \
@@ -24,15 +24,15 @@ B="$(ls /dev/*|egrep "^${Z}p?1$") "
 C="$(ls /dev/*|egrep "^${Z}p?2$") "
 mkfs.vfat ${B};mkfs.btrfs -fq ${C}
 mount ${C}/mnt;cd /mnt;${F}
-${F}home ; cd ;umount/mnt;${E}=@
-${C}/mnt ;mkdir /mnt/{boot,home}
-mount ${B}/mnt/boot;${E}=@home \
+${F}home ; cd ;umount/mnt;${E}l=@
+${C}/mnt ; mkdir /mnt/{boot,home}
+mount ${B}/mnt/boot;${E}l=@home \
 ${C}/mnt/home ; lsblk -ne 7,11 ; if 
 [ -n ${H} ];then if [ ${H} == AMD ]
 then H1='amdgpu ' && H2='amd-ucode'
 else H2='intel-ucode'&&H1='i915 ';fi
 fi ; ${G}timezone ${T}&&${G}ntp true
-sed -i "s/ULES=()/ULES=(${H1}btrfs)/" $I
+sed -i "s/ULES=()/ULES=(${H1}btrfs)/" /$I
 sed -i 's/#Co/Co/' ${J}conf;reflector -c \
 "$(curl -s https://ipapi.co/country)" -p \
 https --sort rate --save ${J}d/mirrorlist||
@@ -55,7 +55,7 @@ gvfs-afc xorg-xinit geeqie conky-manager \
 clipgrab ${H2}; cp ${J}conf /mnt${J}conf \
 curl -L https://raw.githubusercontent.com\
 /djsharcode/Mona/main/install.sh -o /mnt/\
-etc/install.sh;cp ${I} /mnt${I}
+etc/install.sh;cp /$I /mnt/$I
 echo "zoneinfo/$T" >/mnt/etc/T
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh /etc/install.sh
