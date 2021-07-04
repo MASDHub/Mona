fb1057b39f440518273b04bf1dd480ff047a14b0
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-04 04:02:11 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -29,9 +29,9 @@ ${C}/mnt ; mkdir /mnt/{boot,home}
 mount ${B}/mnt/boot;${E}l=@home \
 ${C}/mnt/home ; lsblk -ne 7,11 ; if
 [ -n ${H} ];then if [ ${H} == AMD ]
-then H2='amd-ucode'&&H1='amdgpu '
-else H2='intel-ucode'&&H1='i915 ';fi
-fi;${G}-timezone ${T}&&${G}-ntp true
+then H2='amd-ucode ' &&H1='amdgpu '
+else H2='intel-ucode'&&H1='i915 '; fi
+fi; ${G}-timezone ${T}&&${G}-ntp true
 sed -i "s/ULES=()/ULES=(${H1}btrfs)/" /$I
 sed -i 's/#Co/Co/' ${J}conf;reflector -c \
 "$(curl -s https://ipapi.co/country)" -p \
