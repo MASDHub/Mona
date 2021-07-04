c02fe8b8d2e749c726696bb683e49f70834b6925
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-04 03:10:22 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -10,7 +10,7 @@ E='mount -o noatime,compress=zstd,subvol'
 F='btrfs su cr @' ; G='timedatectl set-'
 H="$(lscpu|egrep -o 'AMD|Intel'|sort -u)"
 I='/etc/mkinitcpio.conf';J='/etc/pacman.'
-T="$(curl -sL https://ipapi.co/timezone)"
+T="$(curl -s https://ipapi.co/timezone)"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k|pacman-key --populate ; printf \
 '%50s'|tr ' ' -; lsblk -do NAME,SIZE -\
