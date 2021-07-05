1f9c61bad15e1b98f345aa5e7e2f05197b99c271
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-06 00:03:46 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -euo pipefail
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-A='.config/openbox'; H='sleep 2 && '
+A='.config/openbox' ; H='sleep 2 && '
 B='<item label="' ; I='~/docks.ini'
 C='"><action name="Execute"><command>'
 D='</command></action></item>'
@@ -9,15 +9,14 @@ E='<separator></separator>'
 F='~/.config/plank/dock1/launchers/'
 G='/net/launchpad/plank/docks/'
 Z='.config/pikaur.conf' #~~~~~~~~~~#
-mkdir ~/.config ; git clone \
+mkdir ~/.config;cp -a /etc/xdg/openbox\
+/ ~/.config/ ; git clone \
 https://aur.archlinux.org/pikaur.git
-cp -a /etc/xdg/openbox/ ~/.config/ 
-cd pikaur/ ; makepkg -fsri ; pikaur \
--S picom-git timeshift ttf-ms-fonts \
-oranchelo-icon-theme-git #qxkb
-sed -i 's/g = no/g = yes/' ~/$Z ; se\
-d -i 's/g = no/g = yes/' ~/$Z ; sed \
--i '5,$d' ~/${A}/menu.xml ; sed -i \
+cd pikaur/; makepkg -fsri; pikaur -S \
+picom-git timeshift ttf-ms-fonts \
+oranchelo-icon-theme-git ; sed -i '
+s/g = no/g = yes/' .config/pikaur.conf
+sed -i '5,$d' $A/menu.xml ; sed -i \
 -e '5,120 s/8/12/' -e '131 s/4/2/' \
 -e 's/W-e/0x85/' -e '5,99 s/9/13/' \
 -e 's/kfmclient openProfile/rofi/' \
