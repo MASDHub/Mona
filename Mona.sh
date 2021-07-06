327344cc2a654bbe8db28e2804c834efcb45c1aa
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-06 19:34:00 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -6,35 +6,34 @@ set -euo pipefail
 #       ↻     ⊲  Ⅱ  ⊳     ↺        #
 #    VOLUME: ▁▂▃▄▅▆▇ 100%       #
 E=' -o noatime,compress=zstd,subvol=@'
-F='btrfs su cr @' ; G='/etc/pacman.'
-I='curl -s https://ipapi.c';setfont t\
-er-124b;H="$(lscpu|egrep -o 'AMD|Intel
-'|sort -u)" ; J='/etc/mkinitcpio.conf'
-K='timedatectl set-';head -n 7 -- $0 |
-tail -n 4 #~~~~~~~~~~~~~~~~~~~~~~~~~~#
+F='btrfs su cr @';G='/etc/pacman.';H="
+$(lscpu|egrep -o 'AMD|Intel'|sort -u)"
+I='curl -s https://ipapi.co';setfont \
+ter-124b;head -n 7 -- $0|tail -n 4;J='
+/etc/mkinitcpio.conf';K='timedatectl '
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 gpg -k |pacman-key --populate;printf '
-%50s\n'|tr ' ' -;lsblk -do NAME,SIZE \
--e 7,11|grep --color [A-Z];read -p$'\e
-[1;31mInstall Disk\e[0m: ' A;until Z=\
-"/dev/$A"&&sgdisk $Z -Z -n 1::+512M -\
-t 1:EF00 -n -p;do lsblk -e 7,11 -do N\
-AME,SIZE|grep --color [A-Z]&&read -p$'
-\e[1;31mRetry\e[0m: ' A ; done
-B="$(ls /dev/* | egrep "^${Z}p?1$") "
-C="$(ls /dev/* | egrep "^${Z}p?2$") "
-mkfs.vfat ${B} ; mkfs.btrfs -fq ${C}
-mount ${C}/mnt ; cd /mnt;${F};${F}home
-cd;umount /mnt ; mount ${E} ${C}/mnt
-mkdir /mnt/{boot,home};mount${B}/mnt\
-/boot ; mount ${E}home ${C}/mnt/home
-lsblk -e 7,11 ; if [[ $H == AMD ]]
+%50s\n'|tr ' ' -;until lsblk -e 7,11 \
+-do NAME,SIZE|egrep --color 'G|NAME'&&
+read -p$'\e[31mEnter Installation-Disk
+Name\e[0m: ' A&&Z="/dev/$A"&& sgdisk \
+$Z -Z -n 1::+512M -t 1:EF00 -n -p;do :
+done;lsblk -e 7,11|egrep --color '|/*'
+B="$(ls /dev/*|egrep "^${Z}p?1$") "
+C="$(ls /dev/*|egrep "^${Z}p?2$") "
+mkfs.vfat ${B};mkfs.btrfs -fq ${C}
+mount ${C}/mnt;cd /mnt;${F};${F}home
+cd;umount /mnt;mount${E} ${C}/mnt
+mkdir /mnt/{boot,home};mount ${B}/\
+mnt/boot;mount${E}home ${C}/mnt/home
+; if [[ $H == AMD ]]
 then S='amd-ucode ' && Q='amdgpu '
 else S='intel-ucode'&& Q='i915 '
-fi;${K}timezone $(${I}o/timezone)&&
-${K}ntp true ; sed -i 's/#Co/Co/' \
+fi;${K}set-timezone $(${I}/timezone)&&
+${K}set-ntp true ; sed -i 's/#Co/Co/' \
 ${G}conf ; sed -i "s/ULES=(/ULES=(\
 ${Q}btrfs/" $J;reflector -p https \
---sort rate -c "$(${I}o/country)" \
+--sort rate -c "$(${I}/country)" \
 -a 12 -f 2 --save ${G}d/mirrorlist ||
 reflector -p https --score 10 --sort \
 rate -a 8 -f 2 --save ${G}d/mirrorlist
@@ -59,7 +58,7 @@ libreoffice-still ${S} ; cp ${G}conf \
 /mnt${G}conf;curl -sL https://raw.git\
 hubusercontent.com/djsharcode/Mona/ma\
 in/install.sh -o /mnt/mtc/install.sh
-echo zoneinfo/$(${I}o/timezone) \
+echo zoneinfo/$(${I}/timezone) \
 >/mnt/etc/T;cp ${J} /mnt${J}
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sh /etc/instal.sh
