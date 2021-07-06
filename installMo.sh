6dc4cb95e45bd815fd745c51427408da08943ce7
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-06 06:14:00 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -23,30 +23,29 @@ sed -i '5,$d' $A/menu.xml ; sed -i \
 -e 's/filemanagement/-show drun/g' \
 -e 's/Clearlooks/Bear2/' -e 's/sans\
 /Fira Sans Condensed Book/' -e 's/A\
--space/0x85/' $A/rc.xml
-echo -e "<menu id="'"root-menu"'" \
-label="'"Openbox 3"'"> \n${B}Files\
-${C}nemo${D}\n${B}Search${C}rofi -\
-show drun ${D}\n${B}Web${C}firefox\
-${D}\n${B}Terminal${C}alacritty${D}
-${B}Text${C}geany${D}\n${B}Calcula\
-tor${C}galculator${D}${E}\n${B}Ref\
-resh${C}openbox --reconfigure ${D}\
-${E}${B}Lock Screen${C}xlock -mode\
-atlantis -echokeys -echokey '*' -i\
-nfo +description "'"Password : "'"\
-${D}\n${B}Reboot${C}reboot${D}${B}\
-Power Off${C}poweroff${D}\n</menu>\
-</openbox_menu>" >> $A/menu.xml
-echo -e "lxqt-policykit &\n\npicom \
---experimental-backends &\n\nplank &
-\ntrayer --monitor primary --align \
-right --tint 0x716966 --height 40 -\
--transparent true &\n\nxpad &\n\n(s\
-leep 2&&nm-applet) &\n\n(sleep 3&&v\
-olumeicon) &\n\n(sleep 4&&sh m.sh) &
-"> $A/autostart
-echo -e 'dconf dump '"$G"'>'"$I"''"
+-space/0x85/' ${A}/rc.xml; echo -e "
+<menu id="'"root-menu"'" label="'" \
+Openbox 3"'">\n${B}Files${C}nemo${D}
+${B}Search${C}rofi -show drun${D}$B
+Web${C}Firefox${D}\n${B}Terminal${C}
+alacritty${D}\n${B}Text${C}geany${D}
+${B}Calculator${C}galculator${D}${E}
+${B}Refresh${C}openbox --reconfigure
+${D}${E}${B}Lock Screen${C}xlock -m\
+ode atlantis -echokeys -echokey '*
+' -info +description "'"Password: "'
+"${D}\n${B}Reboot${C}reboot${D}${B}\
+Power Off${C}poweroff${D}\n</menu><\
+/openbox_menu>">>$A/menu.xml;echo -\
+e "lxqt-policykit &\n\npicom --expe\
+rimental-backends &\n\nplank &\n\nt\
+rayer --monitor primary --align rig\
+ht --tint 0x716966 --height 40 --tr\
+ansparent true &\n\nxpad &\n\n(slee\
+p 2&&nm-applet) &\n\n(sleep 3&&volu\
+meicon) &\n\n(sleep 4&&sh m.sh) &">\
+$A/autostart;echo -e 'dconf dump '"
+$G"'>'"$I"''"
 $H"'sed -i '"'s/bottom/right/'"'\'"
 $I"';'"$H"'cat'"$I"'|dconf load \'"
 $G"';'"$H"'echo -e "'"[PlankDockIt\
