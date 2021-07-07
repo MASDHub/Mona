6873958da3a1fe1a8adc97db23a5668a8098de21
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-07 04:56:40 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -30,8 +30,8 @@ $H == Intel ]];then S='intel-ucode'&&
 Q='i915 ';elif [[ $H == AMD ]];then \
 Q='amdgpu '&&S='amd-ucode';fi; sed -\
 i "0,/()/s//(${Q}btrfs)/" $J;sed -i '
-s/#Co/Co/' ${G}conf ; ${K}-timezone "
-$(${I}/timezone)"&&${K}-ntp true
+s/#Co/Co/' ${G}conf ; ${K}-timezone \
+"$(${I}/timezone)"&&${K}-ntp true
 reflector -p https -c "$($I/country)
 " -a 8 -f 2 --sort rate --save ${G}d\
 /mirrorlist||reflector --score 5 -p \
