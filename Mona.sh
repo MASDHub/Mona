a2a99150ab623ce1507ef5bb35c823a7ce7fa9ae
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-07 06:16:37 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -17,10 +17,10 @@ read -p$'\e[1;31mEnter Installation -
 Disk\e[0m: ' A&&Z="/dev/$A"&&sgdisk \
 $Z -Z -n 1::+512M -t 1:EF00 -n 2;do :
 done;B="$(ls /dev/*|egrep "^${Z}p?1$"
-) ";C="$(ls /dev/*|egrep "^${Z}p?2$")
- ";mkfs.vfat -c $B;mkfs.btrfs -fq $C
-mount ${C} /mnt; cd /mnt;${F}home;$F
-cd; umount /mnt;mount ${E} ${C}/mnt
+) "; C="$(ls /dev/*|egrep "^${Z}p?2$"
+) ";mkfs.vfat -c $B;mkfs.btrfs -fq $C
+mount ${C} /mnt ; cd /mnt;${F}home;$F
+cd; umount /mnt ; mount ${E} ${C}/mnt
 mkdir /mnt/{boot,home};mount $B/mnt/\
 boot;mount ${E}home $C/mnt/home;if [[
 $H == Intel ]];then S='intel-ucode'&&
