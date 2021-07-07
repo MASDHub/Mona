1b812364d7f3385638279d1dfe6c671847960101
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-07 18:06:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,15 +59,14 @@ normal;fi;fi' >$C/sddm/scripts/Xsetup
 echo -e "${A}CONNECTING NETWORKS${B}
 ";${F}NetworkManager ; echo -e "${A}
 DISPLAY MANAGER ENABLED${B}";${F}sddm
-echo -e "${A}GRUB INSTALL${B}";grub-\
-install --target=x86_64-efi --efi-di\
-rectory=/boot --bootloader-id=GRUB
-grub-mkconfig > /boot/grub/grub.cfg
-curl -s htt\
-ps://raw.githubusercontent.com/djSha\
-rcode/Mona/main/installMo.sh> /home/\
-$U/installMO.sh;cd /;chown root:root\ 
-/home;chmod 755 /home;runuser --logi\
-n $U --session-command "sh ~/install\
-MO.sh"
+echo -e "${A}GRUB INSTALL${B}"; grub\
+-install --target=x86_64-efi --efi-d\
+irectory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+curl -sSL https://raw.githubusercont
+ent.com/djSharcode/Mona/main/install\
+Mo.sh >> /home/$U/installMO.sh ; cd /
+chown root:root /home;chmod 755 /home
+runuser --login $U --session-command "
+sh ~/installMO.sh"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
