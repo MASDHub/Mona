35a280b34b6e295a093b10fdbc586fd1c6568b5b
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-07 04:24:16 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -26,11 +26,11 @@ mount ${C} /mnt;cd /mnt;${F}home;${F}
 cd; umount /mnt;mount ${E} ${C} /mnt
 mkdir /mnt/{boot,home};mount $B/mnt/\
 boot;mount ${E}home $C/mnt/home;if [[
-$H = Intel ]]then S='intel-ucode'&&Q='
-i915 ';elif [[ $H = AMD ]];then Q='
-amdgpu '&&S='amd-ucode'&&;fi;sed -i "
-0,/()/s//(${Q}btrfs)/" ${J}; sed -i '
-s/#Co/Co/' ${G}conf;${K}-timezone "
+$H = Intel ]];then S='intel-ucode'&&
+Q='i915 ';elif [[ $H = AMD ]];then \
+Q='amdgpu '&&S='amd-ucode'; fi ;sed \
+-i "0,/()/s//(${Q}btrfs)/" $J;sed \ 
+-i 's/#Co/Co/' ${G}conf;${K}-timezone "
 $(${I}timezone)"&&${K}-ntp true
 reflector -p https -a 6 -c \
 "$(${I}country)" --sort rate --save \
