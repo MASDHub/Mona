8c75f7e28b7658dae6f68b889ff41fa2db01c3ed
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-08 00:44:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ Enter User's Password${A}"&&passwd $U
 do :;done;P="${U}pc";echo -e "127.0.\
 0.1${S}localhost\n::1${Z}127.0.1.1$S\
 ${P}.localdomain ${P}" >>  /etc/hosts
-ln -sf ${E}/$(cat /etc/TZ) /etc/loca\
+ln -sf /usr/$(cat /etc/TZ) /etc/loca\
 ltime;echo "LANG=${F}TF-8">/etc/loca\
 le.conf;echo "/home/${U}/">/etc/U;hw\
 clock --systohcs;echo "$P">/etc/host\
