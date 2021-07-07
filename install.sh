3a921d46c91872b7aa9a0e603ea2536ebfce1bdf
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-07 17:45:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,9 +51,11 @@ N='"$O"'o '"'HDMI-1|HDMI1'"')" \nO='\
 "$O"'o '"'eDP1|eDP-1'"')"  \nP='"$O"\
 'c '"'HDMI|eDP'"')"\nif [ -r ${M} ]&&
 $(grep -q xrandr ${M});then $M;else \
-if [[ "${P}" -ge "2" ]];then xrandr \
---output $O --off --output $N --auto
-fi;fi' >${C}/sddm/scripts/Xsetup
+if [[ ${P} -ge 2 ]]; then xrandr --o\
+utput $O --mode 1920x1080 --pos 0x0 \
+--rotate normal --output $N --primar\
+y --mode auto --pos 1920x0 --rotate \
+normal;fi;fi' >${C}/sddm/scripts/Xsetup
 echo -e "${A}DISPLAY ON${B}"${F}sddm
 echo -e "${A}NETWORKS CONNECTED${B}"
 ${F}NetworkManager; echo -e "${A}GRUB
