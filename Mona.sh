96b9d18f307023338da12ec12654ed352135a712
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-07 04:27:18 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -28,8 +28,8 @@ mkdir /mnt/{boot,home};mount $B/mnt/\
 boot;mount ${E}home $C/mnt/home;if [[
 $H == Intel ]];then S='intel-ucode'&&
 Q='i915 ';elif [[ $H == AMD ]];then \
-Q='amdgpu '&&S='amd-ucode'; fi; sed \
--i "0,/()/s//(${Q}btrfs)/" $J;sed -\ 
+Q='amdgpu '&&S='amd-ucode';fi; sed -\
+i "0,/()/s//(${Q}btrfs)/" $J;sed -\ 
 i 's/#Co/Co/' ${G}conf;${K}-timezone "
 $(${I}timezone)"&&${K}-ntp true
 reflector -p https -a 6 -c \
