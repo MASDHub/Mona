5dc093a5c5a56a88732e700f57678d9dca448d5f
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-09 16:00:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,11 +15,11 @@ set -euo pipefail
  #  '''::===..-'   =  --.  `
 S='       ';Z='             localhost
 ';B='\e[1;31m';A='\e[0m';head -15 $0|
-tail -13;C='Fira Sans Condensed Book'
-E='/usr/share/gtk';G='/Cantarell';H='
+tail -13;C='/Fira Sans Condensed Book
+';E=' /usr/share/gtk';G='/Cantarell/'
+D='Adwaita/Oranchelo';F='en_US.U';H='
 openbox-session';J='systemctl enable
-';F='en_US.U';D='/Adwaita/Oranchelo'
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 until printf "${B}Enter Root Password
 :${A}"&&passwd;do :;done;until print\
 f "${B}User Name: ${A}"&&read -r R &&
@@ -40,30 +40,31 @@ xinit/xinitrc;echo "$H">~$U/.xinitrc
 sed -i '0,/# %/ s/# %/ %/' /etc/sudo\
 ers;sed -i "s/#${F}/${F}/" /etc/loca\
 le.gen;locale-gen;hwclock --systohc
-sed -i -e "s$G/$C/" -e "2 s/$D/" -e "
-s$D-Beka/" $E-2.0/gtkrc;sed -i -e "s\
-$G/$C/" -e "2 s$D-Beka/" -e "3 s$D/"\
- $E-3.0/settings.ini;echo -e "${B}GRUB${A}";grub-install \
---target=x86_64-efi --efi-directory=\
-/boot --bootloader-id=GRUB;grub-mkco\
-nfig >/boot/grub/grub.cfg; 
-echo -e "${B}NETWORK${A}";${G}Networ\
-kManager;echo -e "${B}DISPLAY ON${A}"
-${J}sddm;I='"$(xrandr|egrep -';echo \
--e 'N='"$I"'o '"'HDMI-1|HDMI1'"')" 
-M=$(find /home/*/.screenlayout/*.sh)
-O='"$I"'o '"'eDP1|eDP-1'"')"\nP='"$I\
-"'c '"'HDMI|eDP'"')"\nif [ -r ${M} ]\
-&&$(grep -q xrandr $M);then $M;else \
-if [[ ${P} -ge 2 ]]; then xrandr --o\
-utput $O --mode 1920x1080 --pos 0x0 \
---rotate normal --output $N --primar\
-y --mode auto --pos 1920x0 --rotate \
-normal;fi;fi'>/usr/share/sddm/script\
-s/Xsetup;curl -sL https://raw.github\
-usercontent.com/djSharcode/Mona/main\
-/installMo.sh > ~$U/installMO.sh;cd / 
-chown root:root /home;chmod 755 /home
-runuser --login $U --session-command "
+sed -i -e "2 s/$D/" -e "s/$D-Beka/" \
+-e "s${G}$C/"$E-2.0/gtkrc;sed -i -e "
+3 s/$D/" -e "$G$C/" -e "2 s/$D-Beka/"
+$E3.0/settings.ini;echo -e "${B}GRUB
+${A}";grub-install --target=x86_64-e\
+fi --efi-directory=/boot --bootloade\
+r-id=GRUB;grub-mkconfig >/boot/grub/\
+grub.cfg;echo -e "${B}DISPLAY ENABLE
+${A}";$Jsddm;echo -e "${B}NETWORK${A}
+";$JNetworkManager;I='$(xrandr|egrep'
+echo -e 'P="'"$I"'-c '"'HDMI|eDP'"')"
+N="'"$I"'-o '"'HDMI-1|HDMI1'"')"\nM=\
+"$(find /home/*/.screenlayout/*.sh)"
+O='"$I"'-o '"'eDP1|eDP-1'"')"\nP='"$I
+"'-c '"'HDMI|eDP'"')"\nif [ -r $M ]&&
+$(grep -q xrandr ${M});then $M;else \
+if [[ $P -ge 2 ]];then xrandr --outp\
+ut $O --mode 1920x1080 --pos 0x0 --r\
+otate normal --output $N --primary -\
+-mode auto --pos 1920x0 --rotate nor\
+mal;fi;fi'>/usr/share/sddm/scripts/X\
+setup;curl -sL https://raw.githubuse\
+rcontent.com/djSharcode/Mona/main/in\
+stallMo.sh>~$U/installMO.sh;cd /;cho\
+wn root:root /home;chmod 755 /home;r\
+unuser --login $U --session-command "
 sh ~/installMO.sh"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
