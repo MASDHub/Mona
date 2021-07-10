5c0f92669c1e5ff89fd7e22a6fe82892d1b2361c
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-10 19:27:43 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 E='-o noatime,compress=zstd,subvol=@'
-F='btrfs su cr';H='/etc/pacman.';J=$(
+F='btrfs su cr ';H='/etc/pacman';J=$(
 lscpu |egrep -o 'AMD|Intel' |sort -u)
 K='/etc/mkinitcpio.conf';setfont ter\
 -124n;I='curl -sSL https://ipapi.co/'
@@ -14,8 +14,8 @@ $B -Z -n 1::+512M -t 1:EF00 -n 2;do :
 done;C="$(ls /dev/*|egrep "^${B}p?1$"
 ) "; D="$(ls /dev/*|egrep "^${B}p?2$"
 ) ";mkfs.vfat -c $C;mkfs.btrfs -fq $D
-mount ${D} /mnt;cd /mnt;$F @home;$F @
-cd ; umount /mnt;mount ${E} ${D}/mnt
+mount $D /mnt;cd /mnt;${F}@home;${F}@
+cd;umount /mnt;mount ${E} ${D} /mnt
 mkdir /mnt/{boot,home};mount $C/mnt/\
 boot;mount ${E}home $D/mnt/home;if [[
 $J == Intel ]];then M='intel-ucode'&&
@@ -23,13 +23,13 @@ L='i915 ';elif [[ $J == AMD ]];then \
 L='amdgpu '&&M='amd-ucode';fi;lsblk \
 -pe 7,11|egrep --color /....;sed -i "
 0,/()/s//(${L}btrfs)/" $K;sed -i 's/\
-#Co/Co/' ${H}conf;${G}timezone $(${I\
-}timezone)&&${G}ntp true;reflector -\
+#Co/Co/' $H.conf;${G}timezone $(${I}\
+timezone) &&${G}ntp true;reflector -\
 p https -a 8 --sort rate -c $(${I}co\
-untry) -f 2 --save ${H}d/mirrorlist||
+untry) -f 2 --save $H.d/mirrorlist ||
 reflector -p https --score 5 --sort \
-rate --save ${H}d/mirrorlist;pacstra\
-p -i /mnt base vim grub xorg arandr \
+rate --save $H.d/mirrorlist;pacstrap\
+ -i /mnt base vim grub xorg arandr \
 base-devel linux pipewire alacritty \
 efibootmgr xorg-xinit linux-headers \
 plank linux-firmware lxqt-policykit \
@@ -48,6 +48,6 @@ curl -sSL https://raw.githubusercont\
 ent.com/djsharcode/Mona/main/install\
 .sh>/mnt/etc/instal.sh;cp $K /mnt${K}
 echo share/zoneinfo/$(${I}timezone)>\
-/mnt/etc/TM;cp ${H}conf /mnt${H}conf
+/mnt/etc/TM;cp $H.conf /mnt$H.conf
 genfstab -U /mnt>>/mnt/etc/fstab;arc\
 h-chroot /mnt sh /etc/install.sh
