07be967e9f4d2213ae3630b648f72f2900f4b089
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-10 19:32:07 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -3,7 +3,7 @@ E='-o noatime,compress=zstd,subvol=@'
 F='btrfs su cr ';H='/etc/pacman';J=$(
 lscpu |egrep -o 'AMD|Intel' |sort -u)
 K='/etc/mkinitcpio.conf';setfont ter\
--124n;I='curl -sSL https://ipapi.co/'
+-124n;I='curl -sSL https://ipapi.co'
 G='timedatectl set' #~~~~~~~~~~~~~~~#
 gpg -k|pacman-key --populate;printf '
 %46s\n'|tr ' ' -;until lsblk -do NAM\
@@ -23,10 +23,10 @@ L='i915 ';elif [[ $J == AMD ]];then \
 L='amdgpu '&&M='amd-ucode';fi;lsblk \
 -pe 7,11|egrep --color /....;sed -i "
 0,/()/s//(${L}btrfs)/" $K;sed -i 's/\
-#Co/Co/' $H.conf;$G-timezone $(${I}t\
-imezone)&&$G-ntp true;reflector - p \
-https -a 8 --sort rate -c $(${I}co\
-untry) -f 2 --save $H.d/mirrorlist ||
+#Co/Co/' $H.conf;$G-timezone $($I/tim\
+ezone)&&$G-ntp true;reflector - p \
+https --sort rate -c $($I/country) \
+-f 2 --save $H.d/mirrorlist ||
 reflector -p https --score 5 --sort \
 rate --save $H.d/mirrorlist;pacstrap\
  -i /mnt base vim grub xorg arandr \
