4151ac2f5f6f7f4a2bf940d22c0943372c2f0469
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-10 18:41:26 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -24,9 +24,9 @@ L='amdgpu '&&M='amd-ucode';fi;lsblk \
 -pe 7,11|egrep --color /....;sed -i "
 0,/()/s//(${L}btrfs)/" $K;sed -i 's/\
 #Co/Co/' ${H}conf;${G}timezone $(${I\
-}timezone)&&${G}ntp true;reflector \
-ector -p https -a 12 --sort rate -c \
-$(${I}country) --save ${H}d/mirrorli\
+}timezone)&&${G}ntp true;reflector -\
+p https -a 12 --sort rate -c $(${I}\
+country) --save ${H}d/mirrorli\
 st||reflector --score 5 -p https -a \
 8 --sort rate --save ${H}d/mirrorlist
 pacstrap -i /mnt base vim grub xorg \
