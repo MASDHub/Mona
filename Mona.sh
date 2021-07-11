65e53b459ecaecb7c4a3467844294348e9d375e2
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-12 02:06:42 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -26,8 +26,8 @@ sed -i "0,/(/s//(${L}btrfs/" $K;refl\
 ector -p https --sort rate -a 5 --sc\
 ore 5 --save $H.d/mirrorlist;sed -i '
 s/#Co/Co/' $H.conf;pacstrap -i /mnt \
-base linux-headers vim linux-firmware \
-linux  lxqt-policykit \
+base linux-headers linux-firmware \
+linux nitrogen lxqt-policykit vim \
 base-devel xterm pipewire alacritty \
 efibootmgr xorg-xinit pipewire-jack \
 xorg arandr plank grub nemo-preview \
