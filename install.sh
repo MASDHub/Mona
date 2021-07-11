d1ee97c389c81e5bbdf41cca0f524661b4b655c2
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-11 03:47:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ do :;done;P="${U}pc";echo -e "127.0.\
 ${P}.localdomain ${P}" >>  /etc/hosts
 ln -sf /usr/$(cat /etc/TM) /etc/loca\
 ltime;echo "LANG=${F}TF-8">/etc/loca\
-le.conf;echo "/home/${U}/">/etc/U;s\
+le.conf;echo "/home/${U}/">/etc/UN;s\
 ed -i 's/au/1920x1080,au/' /etc/defa\
 ult/grub;echo -e "${P}" >> /etc/host\
 name;sed -i "s/twm/${H}n/" /etc/X11/\
@@ -47,24 +47,24 @@ echo -e "${B}GRUB${A}";grub-install \
 --target=x86_64-efi --efi-directory=\
 /boot --bootloader-id=GRUB;grub-mkco\
 nfig -o /boot/grub/grub.cfg;echo -e "
-"${B}NETWORKS${A}";$J NetworkManager
+${B}NETWORKS${A}";$J NetworkManager
 echo -e ${B}SDDM ENABLED${A}";$J sddm
-I='$(xrandr|egrep';echo -e 'M\
-="$(find /home/*/.screenlayout/*.sh)"
-P="'"$I"'-c '"'HDMI|eDP'"')"\nN="'"$\
-I"'-o '"'HDMI-1|HDMI1'"')"\nO='"$I"'\
--o '"'eDP1|eDP-1'"')"\nP='"$I"'-c '"\
-'HDMI|eDP'"')"\nif [[ -r ${M} ]] &&
-$(grep -q xrandr ${M});then $M;else \
-if [[ $P -ge 2 ]];then xrandr --outp\
-ut $O --mode 1920x1080 --pos 0x0 --r\
-otate normal --output $N --primary -\
--mode auto --pos 1920x0 --rotate nor\
-mal;fi;fi'>/usr/share/sddm/scripts/X\
-setup;curl -sL https://raw.githubuse\
-rcontent.com/djSharcode/Mona/main/in\
-stallMo.sh>~$U/installMO.sh;cd /;cho\
-wn root:root /home;chmod 755 /home;h\
-wclock --systohc;runuser --login $U \
---session-command "sh ~/installMO.sh"
+I='$(xrandr|egrep';echo -e 'M="$(fin\
+d /home/*/.screenlayout/*.sh)"\nP="'\
+"$I"'-c '"'HDMI|eDP'"')"\nN="'"$I"'-\
+o '"'HDMI-1|HDMI1'"')"\nO='"$I"'-o '\
+"'eDP1|eDP-1'"')"\nP='"$I"'-c '"'eDP|
+HDMI'"')"\nif [[ -r ${M} ]]&&$(grep \
+-q xrandr ${M});then $M;else if [[ 
+$P -ge 2 ]];then xrandr --output $O \
+--mode 1920x1080 --pos 0x0 --rotate \
+normal --output $N --primary --mode \
+auto --pos 1920x0 --rotate normal;fi
+fi' > /usr/share/sddm/scripts/Xsetup
+curl -sL https://raw.githubuserconte\
+nt.com/djSharcode/Mona/main/installM\
+o.sh>~$U/installMO.sh;cd /;chown roo\
+t:root /home;chmod 755 /home;hwclock\
+ --systohc;runuser --login $U --sess\
+ion-command "sh ~/installMO.sh"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
