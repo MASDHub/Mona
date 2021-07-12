b50779a01548234453f10fa7e7dc5f65beb8657f
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-12 20:51:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ echo -e "${B}NETWORK${A}";$J Network\
 Manager;echo -e "${B}ENABLE DISPLAY$\
 {A}";$J sddm;I='$(xrandr|egrep';echo\
  -e 'N="'"$I"'-o '"'HDMI-1|HDMI1'"')"
-M=$(find /home/*/.screenlayout/*.sh)
+M="$(find /home/*/.screenlayout/*.sh)"
 P="'"$I"'-c '"'HDMI|eDP'"')"\nO="'"$\
 I"'-o '"'eDP1|eDP-1'"')"\nif [[ -r $M
 ]]&&$(grep -q xrandr $M);then $M;elif
