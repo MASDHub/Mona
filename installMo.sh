21f7f71b519eb929400572b00a7dd3283b43c015
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-13 18:49:36 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 C='"><actionname="Execute"><command>'
 G=' /net/launchpad/plank/docks/ ';H='
-sleep 2 && ';D='</command></action>'\
+;sleep 2 &&';D='</command></action>'\
 '</item><item label="';I=' docks.ini'
 E='separator>';A=.config/openbox;F=.\
 'config/plank/docks1/launcher/' #~~~#
@@ -32,29 +32,27 @@ geany'$D'Calculator'$C'galculator'$D\
 '$D'Reboot'$C'reboot'$D'Power-Off'$C\
 'poweroff</command></action></item>'\
 '</menu></openbox_menu>'>>$A/menu.xml 
-echo -e "lxqt-policykit &\n
-picom --experimental-backends &\n\np\
-lank &\n\ntrayer --transparent true \
---align right --height 40 --monitor \
-primary --tint 0x716966 &\n\nxpad &\n
-(sleep 2 && volumeicon) &\n\n(sleep \
-2 && nm-applet) &\n\n(sleep 2 && sh \
-m.sh) &">~/$A/autostart;echo -e 'dco\
-nf dump '"$G"'>'"$I"''"$H"'sed -i '"'
-s/bottom/right/'"' '"$I"''"$H"'cat '"
-$I"'|dconf load '"$G"''"$H"'echo -e "
-'"[PlankDockItemPreferences]\nLaunch\
-er=file:///usr/share/applications/ne\
-mo.desktop"'">'"$F"'nemo.dockitem'"$H
-"'rm '"$F"'{geeqie.dockitem,vlc-1.do\
-ckitem}'"$H"'sed -i '"'15,"'$d'"'"' '
-"$A"'/autostart'"$H"'pkill volumeicon
-'"$H"'sed -i -e '"'13,16 s/fals/tru/'
-"' -e '"'s/xterm -e "'alsamixer'"/pa\
-vucontrol/'"' .config/volumeicon/volu\
-meicon'"$H"'volumeicon'"$H"'sleep 19&&
-rm'"$I"'&&rm $0'>m.sh ; su root -c "
-chmod 755 $(cat /etc/U)m.sh";echo -e \
-'\e[1;31mDone,Type: reboot\e[0m';rm $0
-rm /etc/U
+echo -e "lxqt-policykit &\n\npicom -\
+-experimental-backends &\n\nplank &\n
+trayer --transparent true --monitor \
+primary --height 40 --tint 0x716966 \
+--align right &\n\nxpad &\n\n(sleep \
+2 && volumeicon) &\n\n(sleep 2 && nm\
+-applet) &\n\n(sleep 2 && sh na.sh) &
+">$A/autostart;echo -e 'dconf dump'$\
+G'>'$I';'$H'sed -i "s/bottom/right/"\
+''"$I"''"$H"'cat'"$I"'|dconf\
+load'"$G"''"$H"'echo -e "[PlankDockItemPreferences]\nLauncher=file:///\
+usr/share/applications/nemo.desktop"\
+>'"$F"'nemo.dockitem'"$H"'rm '"$F"\
+'{geeqie.dockitem,vlc-1.dockitem}'"$\
+H"'sed -i '"'15,"'$d'"'"' '"$A"'/aut\
+ostart'"$H"'pkill volumeicon'"$H"'se\
+d -i -e "13,16 s/fals/tru/" -e "s/xt\
+erm -e "alsamixer"/pavucontrol/" .config/volumeicon/volumeicon'\
+"$H"'volumeicon'"$H"'sleep 19&&rm'"$\
+I"'&&rm $0'>na.sh;su root -c "chmod \
+755 $(cat /etc/U)na.sh";echo -e '\e[1
+;31mDone,Type: reboot\e[0m';rm {/etc\
+/U,$0}
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
