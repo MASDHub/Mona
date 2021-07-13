c32245c192b6cf4a814e2d85b9fb7a7aebf80b9b
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-13 07:55:37 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -4,9 +4,10 @@ set -euo pipefail
 C='"><actionname="Execute"><command>'
 G=' /net/launchpad/plank/docks/ ';D='
 </command></action></item><item lab'\
-'el="';E='<separator></separator>';H='
-sleep 2&&'
-F='.config/plank/docks1/launcher/';;A='.config/openbox';I='docks.ini'
+'el="';E='<separator></separator>';F\
+='.config/plank/docks1/launcher/';A='
+.config/openbox';I='docks.ini';H='sleep 2&&
+'
 
 ' #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 cp -r /etc/xdg/openbox/ $A;git clone\
