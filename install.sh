030a35dcba07040aabc4b5520808ae1ed81b5798
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-13 06:09:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,12 +47,12 @@ sddm;echo -e "$B*NETWORK$A";$J Netwo\
 rkManager;echo -e "$B*GRUB$A";grub-i\
 nstall --target=x86_64-efi --efi-dir\
 ectory=/boot --bootloader-id=GRUB;gr\
-ub-mkconfig>/boot/grub/grub.cfg;I=$'\
-'(xrandr|egrep -';echo -e 'M="$(fin'\
+ub-mkconfig>/boot/grub/grub.cfg;I='$
+(xrandr|egrep -';echo -e 'M="$(fin'\
 'd/home/*/\.screenlayout/*.sh)"\nN="\
 '"$I"'o '"'HDMI-1|HDMI1'"')"\nO="'"$\
-I"'o '"'eDP1|eDP-1'"')"\nP="'"$I"'c '\
-"'HDMI|eDP'"')"\nif [[ -r ${M} ]] &&
+I"'o '"'eDP1|eDP-1'"')"\nP='"$I"'c '\
+"'HDMI|eDP'"')\nif [[ -r ${M} ]] &&
 $(grep -q xrandr $M);then $M;fi;if [[
 $P -ge 2 ]];then xrandr --output $O \
 --mode 1920x1080 --pos 0x0 --rotate \
