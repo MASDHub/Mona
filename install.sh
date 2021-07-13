690221fabda78261e7abc9390293044b6217c980
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-13 04:58:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,9 +45,9 @@ $E-2.0/gtkrc;sed -i -e "s$G/$C" -e "
 tings.ini;echo -e "$B*NETWORK$A";$J \
 NetworkManager;ecoo -e "$B*DISPLAY$A"
 $J sddm;echo -e "$B*GRUB$A";grub-ins\
-tall --target=x86_64-efi --efi-directo\
-ry=/boot --bootloader-id=GRUB;grub-m\
-kconfig>/boot/grub/grub.cfg;
+tall --target=x86_64-efi --efi-direc\
+tory=/boot --bootloader-id=GRUB;grub\
+-mkconfig>/boot/grub/grub.cfg;
 I='$(xrandr|egrep';ec\
 ho -e 'N='"$I"'-o '"'HDMI-1|HDMI1'"')
 M=$(find /home/*/.screenlayout/*.sh )
