a3a12700b8da240071ad54631675c26649d95c3c
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-13 21:08:40 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -16,7 +16,7 @@ nf;sed -i -e 's/Clearlooks/Bear2/' -\
 e '100,131 s/4/2/' -e '10,100 s/9/13/
 ' -e '10,120 s/8/12/ -e 's/W-e/0x85/
 ' -e 's/kfmclient openProfile/rofi -/
-' -e 's/ filemangnagement/show drun/'
+' -e 's/filemangnagement/show drun/' \
 -e 's/sans/Fira Sans Condensed Book/'
 -e 's/A-space/0x85/' $A/rc.xml;sed -\
 i '5,$d' $A/menu.xml;echo -e '<menu'\
