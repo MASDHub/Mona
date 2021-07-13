3e386b4bac53fa2963d6620253a090922ab2c2cc
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-13 06:11:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,9 +47,9 @@ sddm;echo -e "$B*NETWORK$A";$J Netwo\
 rkManager;echo -e "$B*GRUB$A";grub-i\
 nstall --target=x86_64-efi --efi-dir\
 ectory=/boot --bootloader-id=GRUB;gr\
-ub-mkconfig>/boot/grub/grub.cfg;I=$('
-(xrandr|egrep -';echo -e 'M="$(fin'\
-'d/home/*/\.screenlayout/*.sh)"\nN="\
+ub-mkconfig>/boot/grub/grub.cfg;I=$'(
+xrandr|egrep -';echo -e 'M="$(find'\
+'/home/*/\.screenlayout/*.sh)"\nN="\
 '"$I"'o '"'HDMI-1|HDMI1'"')"\nO="'"$\
 I"'o '"'eDP1|eDP-1'"')"\nP='"$I"'c '\
 "'HDMI|eDP'"')\nif [[ -r ${M} ]] &&
