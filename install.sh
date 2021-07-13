811796c36118e87efdd217f6b7373a22014edb5e
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-13 06:02:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,9 +48,9 @@ rkManager;echo -e "$B*GRUB$A";grub-i\
 nstall --target=x86_64-efi --efi-dir\
 ectory=/boot --bootloader-id=GRUB;gr\
 ub-mkconfig>/boot/grub/grub.cfg;I=$'\
-'(xrandr|egrep -';echo -e 'N="'"$I"\
-'o '"'HDMI-1|HDMI1'"')"\nM="$(find '\
-'/home/*/\.screenlayout/*.sh)"\nO='"\
+'(xrandr|egrep -';echo -e 'M="$(fin'\
+'d/home/*/\.screenlayout/*.sh)"\nN="'"$I"\
+'o '"'HDMI-1|HDMI1'"')"\n\nO='"\
 $I"'o '"'eDP1|eDP-1'"')\nP='"$I"'c '\
 "'HDMI|eDP'"')\nif [[ -r ${M} ]] && \
 $(grep -q xrandr $M);then $M;fi;if [[
