aaaf70bb19770c74f454c9f9b09b2cf9ce61371d
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-13 06:26:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,8 +51,8 @@ ub-mkconfig > /boot/grub/grub.cfg;I=\
 '$(xrandr|egrep -';echo -e 'M=$(fin'\
 'd/home/*/\.screenlayout/*.sh)\nN="'\
 "$I"'o '"'HDMI-1|HDMI1'"')"\nO="'"$I\
-"'o '"'eDP1|eDP-1'"')"\nP='"$I"'c '\
-"'HDMI|eDP'"')\nif [[ -r ${M} ]] &&
+"'o '"'eDP1|eDP-1'"')"\nP="'"$I"'c '\
+"'HDMI|eDP'"')"\nif [[ -r ${M} ]] &&
 $(grep -q xrandr $M);then $M;fi;if [[
 $P -ge 2 ]];then xrandr --output $O \
 --mode 1920x1080 --pos 0x0 --rotate \
