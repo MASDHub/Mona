b94f013c7c94ebe6814cb5968963b1d35d1f2e30
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-15 00:58:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,34 +21,34 @@ tail -13;E='Fira Sans Condensed Book/
 H='openbox-session';I='usr/share/gtk'
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 until printf $A"Enter User Name:$B"&&
-read R&&U="${R,,}"&&[ ${#U} -gt 4 ]&&
-[[ "$U" =~ ^[a-z]*$ ]];do :;done;use\
-radd -m -G wheel $U;until printf $A"\
-User Password"$B&&passwd $U;do :;done
-until printf $A"Enter Root Password \
-"$B&&passwd;do :;done;echo -e "127.0\
-.0.1${S}localhost\n::1${Z}127.0.1.1$\
-S${U}c.localdomain ${U}c"> /etc/hosts
-sed -i '0,/# %/ s/# %/ %/' /etc/sudo\
-ers;ln -sf "$(cat /etc/T)" /etc/loca\
-ltime;echo LANG=${C}TF-8 > /etc/loca\
-le.conf;sed -i "s/#$C/$C/" /etc/loca\
-le.gen;echo "/home/$U/n" > /etc/U;s\
+read R&&U=${R,,}&&[ ${#U} -gt 4 ]&&[[
+"$U" =~ ^[a-z]*$ ]];do :;done;userad\
+d -m -G wheel $U;P="`eval echo ~$U`/"
+until printf $A"Enter User's Password
+"$B&&passwd $U;do :;done;until print\
+f "Enter Root $A(Admin)${B}Password"\
+&&passwd;do :;done;echo -e "127.0.0.\
+1${S}localhost\n::1${Z}127.0.1.1${S}\
+$U-pc.localdomain $U-pc" > /etc/hosts
+echo $H>$P.xinitrc;echo $P>/etc/U;s\
 ed -i 's/au/1920x1080,au/' /etc/defa\
 ult/grub;sed -i "s/tw/$H/" /etc/X11/\
-xinit/xinitrc;echo ${U}c > /etc/host\
-name;echo $H>/home/${U}/.xinitrc;hwc\
-lock --systohc;locale-gen;sed -i -e "
-s$F/$E" -e "2 s$D/" -e "s$D-Beka/" /\
-$I-2.0/gtkrc;sed -i -e "s$F/$E" -e "
-3 s$D/" -e "2 s$D-Beka/" /$I-3.0/set\
-tings.ini;echo -e $A"SDDM"$B;$G sddm
-echo -e $A"NETWORK"$B;$G NetworkMana\
-ger;echo -e $A"GRUB"$B;grub-install \
---target=x86_64-efi --efi-directory=\
-/boot --bootloader-id=GRUB;grub-mkco\
-nfig>/boot/grub/grub.cfg;I='"$(xrandr
-|egrep -';echo -e 'N='$I'o '"'HDMI-1|
+xinit/xinitrc;echo ${U}-pc>/etc/host\
+name;ln -sf $(cat /etc/T ) /etc/loca\
+ltime;echo LANG=${C}TF-8 > /etc/loca\
+le.conf;sed -i "s/#$C/$C/" /etc/loca\
+le.gen;hwclock --systohc;locale-gen
+sed -i '0,/# %/ s/# %/ %/' /etc/sudo\
+ers;sed -i -e "3 s$D/" -e "s$F/$E" -\
+e "2 s$D-Beka/" /$I-3.0/settings.ini
+sed -i -e "s$F/$E" -e "2 s$D/" -e "s\
+$D-Beka/" /$I-2.0/gtkrc;echo -e $A"S\
+DDM"$B;$G sddm;echo -e $A"NETWORK"$B
+$G NetworkManager;echo -e $A"GRUB"$B
+grub-install --target=x86_64-efi --ef\
+i-directory=/boot --bootloader-id=GRUB
+grub-mkconfig>/boot/grub/grub.cfg;I='"$(
+xrandr|egrep -';echo -e 'N='$I'o '"'HDMI-1|
 HDMI1'"')";O='$I'o '"'eDP1|eDP-1'"')"
 M="$(find /home/*/.screenlayout/*.sh)
 ";P='$I'c '"'HD|eD'"')";if [ -r $M ]\
