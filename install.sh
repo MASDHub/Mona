808906e1bf19c6003ef440c65eb08be0c85c2898
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-14 21:53:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,9 +49,9 @@ ger;echo -e $A"GRUB"$B;grub-install \
 /boot --bootloader-id=GRUB;grub-mkco\
 nfig>/boot/grub/grub.cfg;I='"$(xrandr
 |egrep -';echo -e 'M="$(find/home/*'\
-'/\.screenlayout/*.sh)"\nN='$I'o '"\
-'HDMI-1|HDMI1'"')"\nO='$I'o '"'eDP1|
-eDP-1'"')"\nP='$I'c '"'HDMI |'eDP'"\
+'/\.screenlayout/*.sh)" \nN='$I'o '"\
+'HDMI-1|HDMI1'"')"\nO='$I'o '"'eDP-1|
+eDP1'"')"\nP='$I'c '"'HDMI |'eDP'"\
 ')";if $(grep -q xrandr $M)&&[[ -r $M 
 ]];then $M;fi;if [[ $P -ge 2 ]]
 then xrandr --output $N --mode auto \
