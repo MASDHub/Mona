222a782fedfb8f5c71d6ef761316f36508eb8132
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-14 21:43:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,10 +48,10 @@ Manager;echo -e $A"GRUB"$B;grub-inst\
 all --target=x86_64-efi --efi-direct\
 ory=/boot --bootloader-id=GRUB;grub-\
 mkconfig>/boot/grub/grub.cfg;I='"$(
-xrandr|egrep -';echo -e 'M=$(fi'\
-'nd/home/*/\.screenlayout/*.sh)\nN='\
-$I'o '"'HDMI-1|HDMI1'"')"\nO='$I'o '\
-"'eDP1|eDP-1'"')"\nP='$I'c '"'HDMI|'\
+xrandr|egrep -';echo -e 'M="$(find/'\
+'home/*/\.screenlayout/*.sh)"\nN='$I\
+'o '"'HDMI-1|HDMI1'"')"\nO='$I'o '"\
+'eDP1|eDP-1'"')"\nP='$I'c '"'HDMI|'\
 'eDP'"')";if $(grep -q xrandr $M)&&[[
 -r $M ]];then $M;fi;if [[ $P -ge 2 ]]
 then xrandr --output $N --mode auto \
