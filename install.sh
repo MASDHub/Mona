8b88e1ce994b40c90b39053fa898a54b296865cc
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-15 02:55:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,20 +30,20 @@ until printf $A"Enter User's Password
 "&&passwd;do :;done;echo -e "127.0.0\
 .1${S}localhost\n::1${Z}127.0.1.1$S$\
 {U}-pc.localdomain $U-pc" >/etc/hosts
-echo $H>$P.xinitrc;echo $P>/etc/U;s\
-ed -i 's/au/1920x1080,au/' /etc/defa\
-ult/grub;sed -i "s/tw/$H/" /etc/X11/\
-xinit/xinitrc;echo ${U}-pc>/etc/host\
-name;ln -sf $(cat /etc/T ) /etc/loca\
+sed -i '0,/# %/ s/# %/ %/' /etc/sudo\
+ers;ln -sf "$(cat /etc/T)" /etc/loca\
 ltime;echo LANG=${C}TF-8 > /etc/loca\
 le.conf;sed -i "s/#$C/$C/" /etc/loca\
-le.gen;hwclock --systohc;locale-gen
-sed -i '0,/# %/ s/# %/ %/' /etc/sudo\
-ers;sed -i -e "3 s$D/" -e "s$F/$E" -\
-e "2 s$D-Beka/" /$I-3.0/settings.ini
-sed -i -e "s$F/$E" -e "2 s$D/" -e "s\
-$D-Beka/" /$I-2.0/gtkrc;echo -e $A"S\
-DDM"$B;$G sddm;echo -e $A"NETWORKS$B"
+le.gen ;sed -i "s/twm/$H/" /etc/X11/\
+xinit/xinitrc;echo $H>$P.xinitrc;sed\
+ -i 's/au/1024x768x24,au/' /etc/defa\
+ult/grub;echo "${U}-pc" >> /etc/host\
+name;locale-gen;hwclock --systohc;se\
+d -i -e "2 s$D-Beka/"  -e "s$F/$E" -\
+e "3 s$D/"/$I-3.0/settings.ini;sed -\
+i -e "s$D-Beka/" -e "2 s$D/" -e "s$F\
+/$E" /$I-2.0/gtkrc;echo -e $A"SDDM"$B
+$G sddm;echo -e $A"NETWORKS$B"
 $G NetworkManager;echo -e $A"GRUB2$B"
 grub-install --target=x86_64-efi --e\
 fi-directory=/boot --bootloader-id=G\
