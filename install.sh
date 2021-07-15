45a027904147b165f14fa5b096f9af8981bcabe4
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-15 03:00:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ $G sddm;echo -e $A"NETWORKS$B";$G Ne\
 tworkManager;echo -e $A"GRUB2$B";gru\
 b-install --target=x86_64-efi --efi-\
 directory=/boot --bootloader-id=GRUB
-grub-mkconfig>/boot/grub/grub.cfg
+grub-mkconfig -o /boot/grub/grub.cfg
 I='"$(xrandr|egrep -';echo -e 'N='$I\
 'o '"'HDMI-1|HDMI1'"')"\nO='$I'o '"'\
 eDP1|eDP-1'"')"\nP='$I'c '"'HD|eD'"')
