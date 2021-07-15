ba41edfb23d9aad04f3c78c2ee69a8eaeb4a3589
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-15 03:36:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,8 +48,8 @@ I-3.0/settings.ini;echo -e $A"NETWORK
 install --target=x86_64-efi --efi-di\
 rectory=/boot --bootloader-id=GRUB;g\
 rub-mkconfig>/boot/grub/grub.cfg;I='"
-$(xrandr|egrep -';echo -e 'M="$find'\
-' /home/*/.screenlayout/*.sh"\nN='$I\
+$(xrandr|egrep -';echo -e 'M=`find '\
+'/home/*/.screenlayout/*.sh"\nN='$I\
 'o '"'HDMI-1|HDMI1'"')"\nP='$I'c '"\
 'HD|eD'"')";O='$I'o '"'eDP1|eDP-1'"\
 ')";if[ -r $M ]&&`grep -q xrandr $M`
