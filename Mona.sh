3fc393360288911499454382052a9b46557c3b83
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-15 05:52:06 +0300

    Update Mona.sh

--- Mona.sh
+++ Mona.sh
@@ -40,10 +40,10 @@ galculator pkg-config rofi clipgrab \
 nitrogen pipewire conky-manager vim \
 gvfs-afc obconf-qt pavucontrol htop \
 pipewire-alsa alsa-utils volumeicon \
-geany-plugins gvfs-mtp vlc $M;cp $H.\
-conf /mnt$H.conf;cp $K /mnt$K;echo /\
-usr/share/zoneinfo/$I>/mnt/etc/T;gen\
-fstab -U /mnt>>/mnt/etc/fstab;arch-c\
-hroot /mnt sh -c "`curl -sSL https:/\
-/raw.githubusercontent.com/djsharcod\
-e/Mona/main/install.sh`"
+geany-plugins gvfs-mtp vlc $M;curl -\
+s https://raw.githubusercontent.com/\
+djsharcode/Mona/main/install.sh>/mnt\
+/etc/.sh;cp $H.conf /mnt$H.conf;cp $\
+K /mnt$K;echo /usr/share/zoneinfo/$I\
+>/mnt/etc/T;genfstab -U /mnt>>/mnt/e\
+tc/fstab;arch-chroot /mnt sh /etc/.sh
