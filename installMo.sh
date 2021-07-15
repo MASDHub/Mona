90e95ffe5a31d4f93b0d81c0e2e1b6d49f76dd87
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-16 01:00:09 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -27,8 +27,8 @@ $C'rofi -show drun'$D''$Z'Web'$C'fi'\
 'refox'$D''$Z'Terminal'$C'alacritty'\
 $D''$Z'Text'$C'geany'$D''$Z'Calcula'\
 'tor'$C'galculator'$D''$Z''E'Refres'\
-\h'$C'openbox --reconfigure'$D''E''$Z\
-'Lock Screen'$C'xlock +description -mode \
+\h'$C'openbox --reconfigure'$D''E''$\
+Z'Lock Screen'$C'xlock +description -mode \
 atlantis -echokeys -echokey "*" -inf\
 o "Enter Password:"'$D''$Z'Reboot'$C\
 'reboot'$D''$Z'Power-Off'$C'powerof'\
