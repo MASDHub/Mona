5b7227547d935d8eb517025b261ca9489df13ff5
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-15 03:44:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,11 +48,11 @@ I-3.0/settings.ini;echo -e $A"NETWORK
 install --target=x86_64-efi --efi-di\
 rectory=/boot --bootloader-id=GRUB;g\
 rub-mkconfig>/boot/grub/grub.cfg;I='`
-xrandr|egrep -';echo -e 'M=`find '\
-'/home/*/.screenlayout/*.sh"\nN='$I\
-'o '"'HDMI-1|HDMI1'"')"\nP='$I'c '"\
-'HD|eD'"')";O='$I'o '"'eDP1|eDP-1'"\
-')";if[ -r $M ]&&`grep -q xrandr $M`
+xrandr|egrep -';echo -e 'N='$I'o '\
+"'HDMI-1|HDMI1'"'`\nO='$I'o '"'eDP1|
+eDP-1'"'`\nP='$I'c '"'HD|eD'"'`\nM'\
+'=`find /home/*/.screenlayout/*.sh`;if
+[ -r $M ]&&`grep -q xrandr $M`
 then $M;elif [ $P -ge 2 ];then xran\
 dr --output $N --pos 1920x0 --mode \
 auto --rotate normal -primary --out\
