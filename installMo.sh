274639093ec5bfd0d14f041e105372f1323c4ad4
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-16 01:14:17 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -29,10 +29,10 @@ $D''$Z'Text'$C'geany'$D''$Z'Calcula'\
 'tor'$C'galculator'$D''$Z''E'Refres'\
 'h'$C'openbox --reconfigure'$D''E''$\
 Z'Lock Screen'$C'xlock -mode atlant'\
-'is -echokeys -info "Enter Password:
-" +description -echokey "*" '$D''$Z'Reboot'$C\
-'reboot'$D''$Z'Power-Off'$C'powerof'\
-'f'$D'</menu></openbox_menu>'>>~/$A/\
+'is -echokeys -info "Enter Password :
+" +description -echokey "*"'$D''$Z''\
+'Reboot'$C'reboot'$D''$Z'Power-Off'$C\
+'poweroff'$D'</menu></openbox_menu>'>>~/$A/\
 menu.xml;echo -e "lxqt-policykit &\n
 picom --experimental-backends &\n\np\
 lank &\n\ntrayer --height 40 --tint \
