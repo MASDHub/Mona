1f720ad71f874543fbbe21dfe6469cd2f2a96cd6
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-23 20:18:47 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -20,38 +20,38 @@ ikaur.conf;sed -i -e 's/A-space/0x85/
  -e 's/filemanagement/-show drun/' -\
 e 's/Clearlooks/Bear2/' -e 's/sans/F\
 ira Sans Condensed Book/' ~/$A/rc.xml
-sed -i '5,$d' ~/$A/menu.xml;echo -e '
-<menu id="root-menu" label="Openbox'\
-' 3">'$B'File'$C'nemo'$I''$B'Search'\
-$C'rofi -show drun'$I''$B'Web'$C'fi'\
-'refox'$I''$B'Terminal'$C'alacritty'\
-$I''$B'Text'$C'geany'$I''$B'Calcula'\
-'tor'$C'galculator'$I''$E''$B'Refre'\
-'sh'$C'openbox --reconfigure'$I''$E'
-'$B'Lock Screen'$C'xlock -echokey "*\
-" +description -info "Enter Password\
-" -mode atlantis -echokeys'$I''$B'R'\
-'eboot'$C'reboot'$I''$B'Power-Off'$C'
-poweroff'$I'</menu></openbox_menu>'>\
->~/$A/menu.xml;echo -e "lxqt-policyk\
-it &\n\npicom --experimental-backend\
-s &\n\ntrayer --height 40 --monitor \
-primary --tint 0x716966 --transparen\
-t true --align right &\n\n(sleep 2&&\
-volumeicon) &\n\nplank &\n\n(sleep 2\
-&&nm-applet) &\n\n(sleep 2&&sh .sh) &
-">~/$A/autostart;echo -e 'echo "[Pla\
-nkDockItemPreferences]\nLauncher=fil\
-e:///usr/share/applications/nemo.des\
-ktop"> ~'$G'nemo.dockitem'$F' rm ~'$\
-G'{geeqie.dockitem,vlc-1.dockitem}'$\
-F'sed -i '"'13,"'$d'"'"' ~/'$A'/auto\
-start'$F'dconf dump'$D'>'$H''$F'sed \
--i "s/bottom/right/"'$H''$F'cat'$H' |
-dconf load'$D''$F'pkill volumeicon'$\
-F'sed -i -e "13,16 s/fals/tru/" -e "
-s/xterm -e '"'alsamixer'"'/pavucontr\
-ol/" .config/volumeicon/volumeicon&&
-volumeicon&&sleep 19&&rm'$H'$0'>~/.sh
-echo -e '\e[1;31mTo Finish Type:\e[0m
+sed -i '5,$d' ~/$A/menu.xml;echo -e "
+<menu id='"'root-menu'"' label='"'Op\
+enbox 3'"'>"$B"File"$C"nemo$I"$B"Sea\
+rch"$C"rofi -show drun$I"$B"Web"$C"f\
+irefox$I"$B"Terminal"$C"alacritty$I"
+$B"Text"$C"geany$I"$B"Calculator"$C"
+galculator$I"$E""$B"Refresh"$C"openb\
+ox --reconfigure$I"$E""$B"Lock Screen
+"$C"xlock -echokey '*' +description \
+-info 'Enter Password' -mode atlanti\
+s -echokeys$I"$B"Reboot"$C"reboot"$I"
+"$B"Power-Off"$C"poweroff"$I"</menu>\
+</openbox_menu>">~/$A/menu.xml;echo \
+-e "lxqt-policykit &\n\npicom --expe\
+rimental-backends &\n\ntrayer --tint\
+ 0x716966 --height 40 --monitor prim\
+ary --transparent true --align right\
+ &\n\n(sleep 2&&volumeicon) &\n\npla\
+nk &\n\n(sleep 2&&nm-applet) &\n\n(s\
+leep 2&&sh .sh) &">~/$A/autostart;ec\
+ho -e 'echo "[PlankDockItemPreferen'\
+'ces]\nLauncher=file:///usr/share/a'\
+'pplications/nemo.desktop"> ~'$G'ne'\
+'mo.dockitem'$F' rm ~'$G'{geeqie.do'\
+'ckitem,vlc-1.dockitem}'$F'sed -i '"\
+'13,"'$d'"'"' ~/'$A'/autostart'$F'd'\
+'conf dump'$D'>'$H''$F'sed -i "s/bo'\
+'ttom/right/"'$H''$F'cat'$H' |dconf'\
+'load'$D''$F'pkill volumeicon'$F'se'\
+d -i -e "13,16 s/fals/tru/" -e "s/x'\
+term -e '"'alsamixer'"'/pavucontrol/
+" .config/volumeicon/volumeicon&&vol\
+umeicon&&sleep 19&&rm'$H'$0'>~/.sh:e\
+cho -e '\e[1;31mTo Finish Type:\e[0m
 reboot'
