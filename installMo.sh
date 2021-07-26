c6bd54034b8a89806479d76787bfeee997f4eeee
djSharcode <84243246+djSharcode@users.noreply.github.com>
2021-07-26 23:39:25 +0300

    Update installMo.sh

--- installMo.sh
+++ installMo.sh
@@ -16,11 +16,10 @@ penbox/ ~/.config;sed -i 's/g = no/g\
  = yes/' ~/.config/pikaur.conf;sed -\
 i -e '9,99 s/9/13/' -e '9,120 s/8/12/
 ' -e 's/W-e/0x85/' -e '100,131 s/4/2/
-' -e 's/filemanagement/-show drun/' -\
-' -e 's/sans/Fira Sans Condensed Book/
--e 's/A-space/0x85/'
+' -e 's/filemanagement/-show drun/' \
+-e 's/sans/Fira Sans Condensed Book/
 ' -e 's/kfmclient openProfile/rofi/'\
-e 's/Clearlooks/Bear2/' ~/$A/rc.xml
+-e 's/A-space/0x85/' -e 's/Clearlooks/Bear2/' ~/$A/rc.xml
 sed -i -e '6,$d' -e 's/apps-accessories/root/' -e 's/Accessories/Openbox 3/' ~/$A/menu.xml
 ;echo -e "
 <menu id='"'root-menu'"' label='"'Op\
